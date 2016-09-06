f802dad4b7571716a695c6f60cc4224ba1100c1d
hugo <hfernandez@neuquen.gov.ar>
2016-09-06 01:10:28 -0300

    prueba integracion slack

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
+import {
+  NgModule
+} from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  ReactiveFormsModule
+} from '@angular/forms';
 
-import { AppComponent }  from './app.component';
-import {Establecimiento} from './establecimiento/establecimiento.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  Establecimiento
+} from './establecimiento/establecimiento.component';
 
 @NgModule({
-  imports: [ BrowserModule, ReactiveFormsModule ],
-  declarations: [ AppComponent, Establecimiento ],
-  bootstrap: [ AppComponent ]
+  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent, Establecimiento],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
