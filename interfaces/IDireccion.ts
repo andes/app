74a0bb9098e00f745292152b7dd9115418cc9f1b
silviroa <silviroa@gmail.com>
2016-10-03 10:54:23 -0300

    Agregar Interface Direccion

---
+++ interfaces/IDireccion.ts
@@ -0,0 +1,11 @@
+import { IUbicacion } from './IUbicacion';
+
+export interface IDireccion{
+    valor: String,
+    codigoPostal: String,
+    ubicacion: IUbicacion,
+    ranking: Number,
+    geoReferencia: [Number],
+    ultimaActualizacion: Date,
+    activo: Boolean
+}
---
+++ interfaces/IUbicacion.ts
@@ -0,0 +1,18 @@
+export interface IUbicacion{
+    barrio: {
+        id: String,
+        nombre: String
+    },
+    localidad: {
+        id: String,
+        nombre: String
+    },
+    provincia: {
+        id: String,
+        nombre: String
+    },
+    pais: {
+        id: String,
+        nombre: String
+    }
+}
