36953c469f97882e792690c2a3d7c1c7d60fe5bc
Luis Parada <luisparadawagner@gmail.com>
2016-11-17 23:04:55 -0300

    Se agrega listado de agendas

--- src/app/components/turnos/buscar-agendas.component.ts
+++ src/app/components/turnos/buscar-agendas.component.ts
@@ -3,6 +3,8 @@ import { PlexService } from 'andes-plex/src/lib/core/service';
 import { PrestacionService } from './../../services/turnos/prestacion.service';
 import { ProfesionalService } from './../../services/profesional.service';
 import { EspacioFisicoService } from './../../services/turnos/espacio-fisico.service';
+import { PlantillaService } from './../../services/turnos/plantilla.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'buscar-agendas',
@@ -11,13 +13,16 @@ import { EspacioFisicoService } from './../../services/turnos/espacio-fisico.ser
 
 export class BuscarAgendasComponent implements OnInit {
     constructor(public plex: PlexService, public servicioPrestacion: PrestacionService, public serviceProfesional: ProfesionalService,
-        public serviceEspacioFisico: EspacioFisicoService) { }
+        public serviceEspacioFisico: EspacioFisicoService, public servicePlantilla: PlantillaService, protected router: Router) { }
 
     public modelo: any = {};
     public prestaciones: any = [];
+    public agendas: any = [];
 
     ngOnInit() {
         this.modelo = { nombre: "", descripcion: "" };
+
+        this.loadAgendas();
     }
 
     loadPrestaciones(event) {
@@ -30,5 +35,21 @@ export class BuscarAgendasComponent implements OnInit {
 
     loadEspaciosFisicos(event) {
         this.serviceEspacioFisico.get().subscribe(event.callback);
+    }
+
+    loadAgendas() {
+        this.servicePlantilla.get()
+            .subscribe(
+            agendas => this.agendas = agendas, //Bind to view
+            err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+    }
+
+    editarAgenda() {
+        this.router.navigate(['/plantillas']);
+        return false;
     }
 }
