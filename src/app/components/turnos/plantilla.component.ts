9ba26fbbd1cef9bea64054ca2913cac948554f92
kro <celeste.carolina.s@gmail.com>
2016-11-29 11:45:48 -0300

    corrijo bugs en plantilla

--- src/app/components/turnos/plantilla.component.ts
+++ src/app/components/turnos/plantilla.component.ts
@@ -39,37 +39,17 @@ export class PlantillaComponent {
         this.bloqueActivo = -1;
     }
 
-    compararBloques(fecha1, fecha2): number {
-        if (fecha1 && fecha2) {
-            return fecha1.horaInicio.getTime() - fecha2.horaInicio.getTime();
-        }
-        else
-            return 0;
-    }
-
-    compararFechas(fecha1: Date, fecha2: Date): number {
-        if (fecha1 && fecha2)
-            return fecha1.getTime() - fecha2.getTime();
-        else
-            return 0;
-    }
-
     cargarPlantilla(agenda: IPlantilla[]) {
         this.modelo = agenda;
         this.calculosInicio();
         this.modelo.bloques.sort(this.compararBloques);
-        // this.ServicioPlantilla.getById(id).subscribe(resultado => {
-        //     { debugger; this.modelo = resultado };
-        //     this.calculosInicio();
-        //     this.modelo.bloques.sort(this.compararBloques);
-        // });
     }
 
+    
     cargar() {
         this.showBuscarAgendas = true;
         this.showPlantilla = false;
     }
-
 
     loadPrestaciones(event) {
         this.servicioPrestacion.get().subscribe(event.callback);
@@ -87,16 +67,27 @@ export class PlantillaComponent {
         this.modelo.fecha = this.modelo.horaInicio;
         let bloques = this.modelo.bloques;
         bloques.forEach((bloque, index) => {
+            bloque.indice = index;
             bloque.horaInicio = new Date(bloque.horaInicio);
             bloque.horaFin = new Date(bloque.horaFin);
             var inicio = bloque.horaInicio;
             var fin = bloque.horaFin;
-            bloque.accesoDirectoDelDiaPorc = Math.floor((bloque.accesoDirectoDelDia * 100) / bloque.cantidadTurnos);
-            bloque.accesoDirectoProgramadoPorc = Math.floor((bloque.accesoDirectoProgramado * 100) / bloque.cantidadTurnos);
-            bloque.reservadoProgramadoPorc = Math.floor((bloque.reservadoProgramado * 100) / bloque.cantidadTurnos);
-            bloque.reservadoProfesionalPorc = Math.floor((bloque.reservadoProfesional * 100) / bloque.cantidadTurnos);
-            let duracion = this.calcularDuracion(bloque.horaInicio, bloque.horaFin, bloque.cantidadTurnos);
-            bloque.duracionTurno = Math.floor(duracion);
+            if (bloque.cantidadTurnos){
+                bloque.accesoDirectoDelDia? bloque.accesoDirectoDelDiaPorc = Math.floor((bloque.accesoDirectoDelDia * 100) / bloque.cantidadTurnos):bloque.accesoDirectoDelDiaPorc=0;
+                bloque.accesoDirectoProgramado? bloque.accesoDirectoProgramadoPorc = Math.floor((bloque.accesoDirectoProgramado * 100) / bloque.cantidadTurnos):bloque.accesoDirectoProgramadoPorc = 0;
+                bloque.reservadoProgramado? bloque.reservadoProgramadoPorc = Math.floor((bloque.reservadoProgramado * 100) / bloque.cantidadTurnos):bloque.reservadoProgramadoPorc = 0;
+                bloque.reservadoProfesional? bloque.reservadoProfesionalPorc = Math.floor((bloque.reservadoProfesional * 100) / bloque.cantidadTurnos):bloque.reservadoProfesionalPorc = 0;
+                let duracion = this.calcularDuracion(bloque.horaInicio, bloque.horaFin, bloque.cantidadTurnos);
+                bloque.duracionTurno = Math.floor(duracion);
+            }
+            else{
+                bloque.accesoDirectoDelDiaPorc = 0;
+                bloque.accesoDirectoProgramadoPorc = 0;
+                bloque.reservadoProgramadoPorc = 0;
+                bloque.reservadoProfesionalPorc = 0;
+                bloque.duracionTurno = 0
+            }
+
             bloque.titulo = inicio.getHours() + ":" + (inicio.getMinutes() < 10 ? '0' : '') + inicio.getMinutes() + "-" +
                 fin.getHours() + ":" + (fin.getMinutes() < 10 ? '0' : '') + fin.getMinutes();
         });
@@ -109,27 +100,52 @@ export class PlantillaComponent {
     }
 
     addBloque() {
+        let longitud = this.modelo.bloques.length;
         this.modelo.bloques.push({
+            indice: longitud,
             "descripcion": "Nuevo Bloque",
             "titulo": "",
             "cantidadTurnos": null,
             "horaInicio": null,
             "horaFin": null,
             "duracionTurno": null,
+            "cantidadSimultaneos": null,
             "accesoDirectoDelDia": 0, "accesoDirectoDelDiaPorc": 0,
             "accesoDirectoProgramado": 0, "accesoDirectoProgramadoPorc": 0,
             "reservadoProgramado": 0, "reservadoProgramadoPorc": 0,
             "reservadoProfesional": 0, "reservadoProfesionalPorc": 0
         });
-        this.activarBloque(this.modelo.bloques.length - 1);
+        this.activarBloque(longitud);
     }
 
     deleteBloque(indice: number) {
-        if (confirm('Confirme que desea eliminar el bloque')) {
+        if (this.plex.confirm('Confirma que desea eliminar el bloque?')) {
             this.modelo.bloques.splice(indice, 1);
             this.bloqueActivo = -1;
             this.validarTodo();
         }
+    }
+
+    compararBloques(fecha1, fecha2): number {
+        let indiceAux: Number;
+        if (fecha1 && fecha2) {
+            console.log("fecha1.horaInicio  "+fecha1.horaInicio+"fecha2.horaInicio  "+fecha2.horaInicio+ " resu "+(fecha1.horaInicio.getTime() - fecha2.horaInicio.getTime()));
+            if(fecha1.horaInicio.getTime() - fecha2.horaInicio.getTime() > 0){
+                indiceAux = fecha1.indice;
+                fecha1.indice = fecha2.indice;
+                fecha2.indice = indiceAux;
+            }
+            return fecha1.horaInicio.getTime() - fecha2.horaInicio.getTime();
+        }
+        else
+            return 0;
+    }
+
+    compararFechas(fecha1: Date, fecha2: Date): number {
+        if (fecha1 && fecha2)
+            return fecha1.getTime() - fecha2.getTime();
+        else
+            return 0;
     }
 
     cambioFecha() {
@@ -150,15 +166,16 @@ export class PlantillaComponent {
         this.validarTodo();
     }
 
-    cambioHoraBloques(){
-        this.fecha = this.modelo.fecha? new Date(this.modelo.fecha): new Date();
+    cambioHoraBloques(texto:String) {
+        this.fecha = this.modelo.fecha ? new Date(this.modelo.fecha) : new Date();
         var inicio = this.combinarFechas(this.fecha, this.elementoActivo.horaInicio);
         var fin = this.combinarFechas(this.fecha, this.elementoActivo.horaFin);
-
+        this.elementoActivo.horaInicio =inicio;
         if (inicio && fin) {
             this.elementoActivo.titulo = inicio.getHours() + ":" + (inicio.getMinutes() < 10 ? '0' : '') + inicio.getMinutes() + "-" +
                 fin.getHours() + ":" + (fin.getMinutes() < 10 ? '0' : '') + fin.getMinutes();
-            this.modelo.bloques.sort(this.compararBloques);
+                
+            
             let duracion = this.calcularDuracion(inicio, fin, this.elementoActivo.cantidadTurnos);
             if (duracion) {
                 this.elementoActivo.duracionTurno = Math.floor(duracion);
@@ -167,6 +184,12 @@ export class PlantillaComponent {
             }
             this.validarTodo();
         }
+        if (texto == "inicio")
+            this.modelo.bloques.sort(this.compararBloques);
+        
+        this.bloqueActivo = this.elementoActivo.indice;
+        this.activarBloque(this.elementoActivo.indice);
+        
     }
 
     cambiaCantidadTurnos() {
@@ -189,51 +212,39 @@ export class PlantillaComponent {
         }
     }
 
-    cambiaAccesoDirectoDelDia() {
-        this.elementoActivo.accesoDirectoDelDiaPorc = Math.floor((this.elementoActivo.accesoDirectoDelDia * 100) / this.elementoActivo.cantidadTurnos);
-        //this.validarPorcentajes(this.elementoActivo);
+    cambiaCantTipo(cual:String){
+        switch(cual) {
+            case "accesoDirectoDelDia":
+                this.elementoActivo.accesoDirectoDelDiaPorc = Math.floor((this.elementoActivo.accesoDirectoDelDia * 100) / this.elementoActivo.cantidadTurnos);
+                break;
+            case "accesoDirectoProgramado":
+                this.elementoActivo.accesoDirectoProgramadoPorc = Math.floor((this.elementoActivo.accesoDirectoProgramado * 100) / this.elementoActivo.cantidadTurnos);
+                break;
+            case "reservadoProgramado":
+                this.elementoActivo.reservadoProgramadoPorc = Math.floor((this.elementoActivo.reservadoProgramado * 100) / this.elementoActivo.cantidadTurnos);
+                break;
+            case "reservadoProfesional":
+                this.elementoActivo.reservadoProfesionalPorc = Math.floor((this.elementoActivo.reservadoProfesional * 100) / this.elementoActivo.cantidadTurnos);
+                break;
+        }
         this.validarTodo();
     }
 
-    cambiaAccesoDirectoDelDiaPorc() {
-        this.elementoActivo.accesoDirectoDelDia = Math.floor((this.elementoActivo.accesoDirectoDelDiaPorc * this.elementoActivo.cantidadTurnos) / 100);
-        //this.validarPorcentajes(this.elementoActivo);
-        this.validarTodo();
-    }
-
-    cambiaAccesoDirectoProgramado() {
-        this.elementoActivo.accesoDirectoProgramadoPorc = Math.floor((this.elementoActivo.accesoDirectoProgramado * 100) / this.elementoActivo.cantidadTurnos);
-        //this.validarPorcentajes(this.elementoActivo);
-        this.validarTodo();
-    }
-
-    cambiaAccesoDirectoProgramadoPorc() {
-        this.elementoActivo.accesoDirectoProgramado = Math.floor((this.elementoActivo.accesoDirectoProgramadoPorc * this.elementoActivo.cantidadTurnos) / 100);
-        //this.validarPorcentajes(this.elementoActivo);
-        this.validarTodo();
-    }
-
-    cambiaReservadoProgramado() {
-        this.elementoActivo.reservadoProgramadoPorc = Math.floor((this.elementoActivo.reservadoProgramado * 100) / this.elementoActivo.cantidadTurnos);
-        //this.validarPorcentajes(this.elementoActivo);
-        this.validarTodo();
-    }
-
-    cambiaReservadoProgramadoPorc() {
-        this.elementoActivo.reservadoProgramado = Math.floor((this.elementoActivo.reservadoProgramadoPorc * this.elementoActivo.cantidadTurnos) / 100);
-        //this.validarPorcentajes(this.elementoActivo);
-        this.validarTodo();
-    }
-
-    cambiaReservadoProfesional() {
-        this.elementoActivo.reservadoProfesionalPorc = Math.floor((this.elementoActivo.reservadoProfesional * 100) / this.elementoActivo.cantidadTurnos);
-        //this.validarPorcentajes(this.elementoActivo);
-        this.validarTodo();
-    }
-
-    cambiaReservadoProfesionalPorc() {
-        this.elementoActivo.reservadoProfesional = Math.floor((this.elementoActivo.reservadoProfesionalPorc * this.elementoActivo.cantidadTurnos) / 100);
-        //this.validarPorcentajes(this.elementoActivo);
+    cambiaPorcentajeTipo(cual:String){
+        switch(cual) {
+            case "accesoDirectoDelDia":
+                this.elementoActivo.accesoDirectoDelDia = Math.floor((this.elementoActivo.accesoDirectoDelDiaPorc * this.elementoActivo.cantidadTurnos) / 100);
+                break;
+            case "accesoDirectoProgramado":
+                this.elementoActivo.accesoDirectoProgramado = Math.floor((this.elementoActivo.accesoDirectoProgramadoPorc * this.elementoActivo.cantidadTurnos) / 100);
+                break;
+            case "reservadoProgramado":
+                this.elementoActivo.reservadoProgramado = Math.floor((this.elementoActivo.reservadoProgramadoPorc * this.elementoActivo.cantidadTurnos) / 100);
+                break;
+            case "reservadoProfesional":
+                this.elementoActivo.reservadoProfesional = Math.floor((this.elementoActivo.reservadoProfesionalPorc * this.elementoActivo.cantidadTurnos) / 100);
+                break;
+        }
         this.validarTodo();
     }
 
@@ -269,47 +280,53 @@ export class PlantillaComponent {
     }
 
     verificarCantidades() {
-        this.cambiaAccesoDirectoDelDiaPorc();
-        this.cambiaAccesoDirectoProgramadoPorc();
-        this.cambiaReservadoProgramadoPorc();
-        this.cambiaReservadoProfesionalPorc();
+        this.cambiaPorcentajeTipo('accesoDirectoDelDia');
+        this.cambiaPorcentajeTipo('accesoDirectoProgramado');
+        this.cambiaPorcentajeTipo('reservadoProgramado');
+        this.cambiaPorcentajeTipo('reservadoProfesional');
     }
 
     validarTodo() {
-        // var desde = this.modelo.horaInicio;
-        // var hasta = this.modelo.horaFin;
         var alerta: string;
         var indice: number;
+        var fechaaux = this.modelo.fecha;
         let bloques = this.modelo.bloques;
         this.alertas = [];
         bloques.forEach((bloque, index) => {
-            var inicio = this.combinarFechas(this.modelo.fecha, bloque.horaInicio);
-            var fin = this.combinarFechas(this.modelo.fecha, bloque.horaFin);
+            
+            var inicio = this.combinarFechas(fechaaux, bloque.horaInicio);
+            var fin = this.combinarFechas(fechaaux, bloque.horaFin);
+            
             if (this.compararFechas(this.modelo.horaInicio, inicio) > 0 || this.compararFechas(this.modelo.horaFin, fin) < 0) {
-                alerta = "Bloque " + (index + 1) + ": Está fuera de los límites de la agenda";
+                alerta = "Bloque " + (bloque.indice+1) + ": Está fuera de los límites de la agenda";
                 indice = this.alertas.indexOf(alerta);
                 this.alertas.push(alerta);
             }
 
             if ((bloque.accesoDirectoDelDia + bloque.accesoDirectoProgramado + bloque.reservadoProgramado + bloque.reservadoProfesional) > bloque.cantidadTurnos) {
-                alerta = "Bloque " + (index + 1) + ": La cantidad de turnos asignados es mayor a la cantidad disponible";
+                alerta = "Bloque " + (bloque.indice+1) + ": La cantidad de turnos asignados es mayor a la cantidad disponible";
                 this.alertas.push(alerta);
             }
 
             if (this.compararFechas(inicio, fin) > 0) {
-                alerta = "Bloque " + (index + 1) + ": La hora de inicio es mayor a la hora de fin";
+                alerta = "Bloque " + (bloque.indice+1) + ": La hora de inicio es mayor a la hora de fin";
                 this.alertas.push(alerta);
             }
         });
+
     }
 
     combinarFechas(fecha1, fecha2) {
         if (fecha1 && fecha2) {
             let horas: number;
             let minutes: number;
+            let auxiliar: Date;
+
+            auxiliar = new Date(fecha1);
             horas = fecha2.getHours();
             minutes = fecha2.getMinutes();
-            return new Date(fecha1.setHours(horas, minutes));
+            auxiliar.setHours(horas, minutes);
+            return auxiliar;
         }
         else
             return null;
