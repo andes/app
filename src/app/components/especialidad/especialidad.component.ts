ac2747449a67d98e93543ee3277ddb974187fa33
NicoDino <nicodinolfo@gmail.com>
2017-01-19 10:03:13 -0300

    scroll en especialidad

--- src/app/components/especialidad/especialidad.component.ts
+++ src/app/components/especialidad/especialidad.component.ts
@@ -7,6 +7,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Plex } from 'andes-plex/src/lib/core/service';
 import { PlexValidator } from 'andes-plex/src/lib/core/validator.service';
 
+const limit = 2;
+
 @Component({
     selector: 'especialidades',
     templateUrl: 'especialidad.html'
@@ -17,6 +19,9 @@ export class EspecialidadComponent implements OnInit {
     datos: IEspecialidad[];
     searchForm: FormGroup;
     seleccion: IEspecialidad;
+    skip: number = 0;
+    loader: boolean = false;
+    value: any;
 
     constructor(private formBuilder: FormBuilder, public plex: Plex, private especialidadService: EspecialidadService) { }
 
@@ -29,16 +34,24 @@ export class EspecialidadComponent implements OnInit {
         });
         //Genera la busqueda con el evento change.
         this.searchForm.valueChanges.debounceTime(200).subscribe((value) => {
-            let codSisa = value.codigoSisa ? value.codigoSisa : ""
-            this.loadDatos({"codigoSisa": codSisa, "nombre": value.nombre});
+            this.value = value;
+            this.skip = 0;
+            this.loadDatos(false);
         })
-
         this.loadDatos();
     }
 
-    loadDatos(parametros = {}){
+    loadDatos(concatenar: boolean = false) {
+        let parametros = {
+            "codigoSisa": this.value && this.value.codigoSisa,
+            "nombre": this.value && this.value.nombre, "skip": this.skip, "limit": limit
+        };
+
         this.especialidadService.get(parametros).subscribe(
-            datos => this.datos = datos) //Bind to view
+            datos => {
+                this.datos = concatenar ? this.datos.concat(datos) : datos;
+                this.loader = false;
+            }) //Bind to view
     }
 
     onReturn(objEspecialidad: IEspecialidad): void {
@@ -53,16 +66,22 @@ export class EspecialidadComponent implements OnInit {
         this.seleccion = objEspecialidad;
     }
 
-      activate(objEspecialidad: IEspecialidad) {
+    activate(objEspecialidad: IEspecialidad) {
         if (objEspecialidad.habilitado) {
 
-           this.especialidadService.disable(objEspecialidad)
-            .subscribe(datos => this.loadDatos()) //Bind to view
+            this.especialidadService.disable(objEspecialidad)
+                .subscribe(datos => this.loadDatos()) //Bind to view
         }
         else {
-             this.especialidadService.enable(objEspecialidad)
-            .subscribe(datos => this.loadDatos()) //Bind to view
+            this.especialidadService.enable(objEspecialidad)
+                .subscribe(datos => this.loadDatos()) //Bind to view
         }
+    }
+
+    nextPage() {
+        this.skip += limit;
+        this.loadDatos(true);
+        this.loader = true;
     }
 
 }
