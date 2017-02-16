1d4521417523a0229b3d34dd8fcfb0ff6e1a2223
fersastre <fersastre23@gmail.com>
2017-02-16 08:18:38 -0300

    Rup-AtomosYmoleculas

--- src/app/components/rup/temperatura.component.ts
+++ src/app/components/rup/temperatura.component.ts
@@ -6,6 +6,7 @@ import { Component, Output, Input, EventEmitter } from '@angular/core';
     templateUrl: 'temperatura.html'
 })
 export class TemperaturaComponent {
+    @Input('datosIngreso') datosIngreso: any;
     @Input('paciente') paciente: IPaciente;
     @Input('tipoPrestacion') prestacion: any;
     @Input('required') required: Boolean;
@@ -15,6 +16,11 @@ export class TemperaturaComponent {
     temperatura: Number = null;
     mensaje: String = null;
 
+    ngOnInit() {
+        if (this.datosIngreso) {
+            this.temperatura = this.datosIngreso;
+        }
+    }
     devolverValores() {
         this.evtData.emit(this.temperatura);
 
