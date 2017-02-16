cd86e02dca0e6ffa38ab8805be6bdad5c99af2e4
kro <celeste.carolina.s@gmail.com>
2017-02-16 12:43:26 -0300

    modifico panel espacio fisico

--- src/app/components/turnos/configuracion/espacio-fisico/panel-espacio.component.ts
+++ src/app/components/turnos/configuracion/espacio-fisico/panel-espacio.component.ts
@@ -3,6 +3,8 @@ import { Plex } from 'andes-plex/src/lib/core/service';
 import { Component, OnInit, Input } from '@angular/core';
 import { AgendaService } from './../../../../services/turnos/agenda.service';
 import { EspacioFisicoService } from './../../../../services/turnos/espacio-fisico.service';
+import * as moment from 'moment';
+moment.locale('en');
 
 @Component({
     selector: 'app-panel-espacio',
@@ -10,22 +12,97 @@ import { EspacioFisicoService } from './../../../../services/turnos/espacio-fisi
 })
 
 export class PanelEspacioComponent implements OnInit {
-    private _agenda: any;
-    @Input('agenda')
-    set agenda(value: any) {
-        this._agenda = value;
-    }
-    get agenda(): any {
-        return this._agenda;
-    }
+    // private _agenda: any;
+    public unidad = 60; // unidad horaria minima (en minutos)
+    public fechaActual = new Date();
+    public inicio: any;
+    public fin: any;
+    public horarios: any[] = [];
+    public consultorios: any[] = [];
+    public listaAgenda: any[] = [];
+    public listaConsultorios: any[] = ['consultorio 1 ', 'consultorio 2'];
+    // @Input('agenda')
+    // set agenda(value: any) {
+    //     this._agenda = value;
+    // }
+    // get agenda(): any {
+    //     return this._agenda;
+    // }
     private espacios: IEspacioFisico[];
+    public agenda = {
+        inicio: moment(new Date(this.fechaActual.setHours(9, 0, 0, 0))),
+        fin: moment(new Date(this.fechaActual.setHours(12, 0, 0, 0))),
+        medico: 'Murakami',
+        consultorio: 'consultorio 2'
+    };
+    public filas = 2;
     constructor(private serviceAgenda: AgendaService, private serviceEspacio: EspacioFisicoService, public plex: Plex) { }
 
     ngOnInit() {
         this.loadEspacios();
-    }
+        this.inicio = new Date(this.fechaActual.setHours(8, 0, 0, 0));
+        let inicioM = moment(this.inicio);
+        this.fin = new Date(this.fechaActual.setHours(13, 0, 0, 0));
+        let diferencia = (this.fin.getTime() - this.inicio.getTime()) / 60000;
+        let cantidadBloques = diferencia / this.unidad;
+
+        // for (let j = 0; j < cantidadBloques; j++) {
+        //     let lista = [];
+        //     for (let i = 0; i < this.listaConsultorios.length; i++) {
+        //         if (inicioM.isBetween(this.agenda.inicio, this.agenda.fin, null, '[)') &&
+        //             this.agenda.consultorio === this.listaConsultorios[i]) {
+        //             lista.push(this.agenda.medico);
+        //         } else {
+        //             lista.push('');
+        //         }
+        //     }
+        //     let elemento = {
+        //         hora: inicioM.format('hh:mm'),
+        //         lista: lista
+        //     }
+        //     this.horarios.push(elemento);
+        //     inicioM.add(this.unidad, 'm');
+        // }
+        for (let j = 0; j < this.listaConsultorios.length; j++) {
+            let lista = [];
+            for (let i = 0; i < cantidadBloques; i++) {
+                if (inicioM.isBetween(this.agenda.inicio, this.agenda.fin, null, '[)') &&
+                    this.agenda.consultorio === this.listaConsultorios[j]) {
+                    lista.push(this.agenda.medico);
+                } else {
+                    lista.push('');
+                }
+            }
+            let elemento = {
+                hora: inicioM.format('hh:mm'),
+                lista: lista
+            }
+            // this.horarios.push(elemento);
+            this.consultorios.push(elemento);
+            inicioM.add(this.unidad, 'm');
+        }
+        console.log('lista', this.consultorios);
+    };
 
     loadEspacios() {
         this.serviceEspacio.get({}).subscribe(espacios => { this.espacios = espacios; console.log('espacios ', this.espacios); });
     }
+
+    combinarFechas(fecha1, fecha2) {
+        if (fecha1 && fecha2) {
+            let horas: number;
+            let minutes: number;
+            let auxiliar: Date;
+
+            auxiliar = new Date(fecha1);
+            horas = fecha2.getHours();
+            minutes = fecha2.getMinutes();
+            // Date.setHours(hour, min, sec, millisec)
+            auxiliar.setHours(horas, minutes, 0, 0);
+            return auxiliar;
+        } else {
+            return null;
+        }
+    }
+
 }
