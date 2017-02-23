9638865f0bec6d54d1a5d5876adcab56b2190b84
fersastre <fersastre23@gmail.com>
2017-02-23 10:38:30 -0300

    Arreglo-Error

--- src/app/interfaces/IPaciente.ts
+++ src/app/interfaces/IPaciente.ts
@@ -23,6 +23,7 @@ export interface IPaciente {
     genero: Genero;
     fechaNacimiento: Date; // Fecha Nacimiento
     edad: Number;
+    edadReal: {valor: Number, unidad: String};
     fechaFallecimiento: Date;
     direccion: [{
         valor: String;
