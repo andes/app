5e91081b41cbcb586151af07e1c21be73a0c208c
Leandro Lambertucci <llambertucci@hospitalneuquen.org.ar>
2017-03-06 13:52:46 -0300

    Vivienda Situacion Socio Economica

--- src/app/components/rup/viviendaSituacionSocioEconomica.component.ts
+++ src/app/components/rup/viviendaSituacionSocioEconomica.component.ts
@@ -26,7 +26,27 @@ export class ViviendaSituacionSocioEconomicaComponent implements OnInit {
     };
 
 
-    ngOnInit() {          
+    ngOnInit() {
+
+            this.tipoPrestacion={
+    "id" : "58b986feb64acd0989b9f53f",
+    "key" : "viviendaSituacionSocioEconomica",
+    "nombre" : "Situación Socioeconómica del Familiar",
+    "autonoma" : false,
+    "activo" : true,
+    "ejecucion" : [ 
+        "58bd644ea99b5e0646e8f42e", 
+        "58b8278bb64acd0989b9f53e", 
+        "58b6d618b64acd0989b9f538", 
+        "58b8183bb64acd0989b9f53d"
+    ],
+    "componente" : {
+        "ruta" : "rup/viviendaSituacionSocioEconomica.component.ts",
+        "nombre" : "ViviendaSituacionSocioEconomicaComponent"
+    },
+    "turneable" : false
+}
+
         this.servicioTipoPrestacion.getById(this.tipoPrestacion.id).subscribe(tipoPrestacion => {
             this.tipoPrestacion = tipoPrestacion;
         });
