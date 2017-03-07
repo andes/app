975d0948ee226df0b7af99ff8096c1af4e120e0d
fersastre <fersastre23@gmail.com>
2017-03-07 13:58:32 -0300

    Rup-hallazgoParto

--- src/app/components/rup/hallazgoRelacionadoParto.component.ts
+++ src/app/components/rup/hallazgoRelacionadoParto.component.ts
@@ -7,17 +7,14 @@ import { Component, Output, Input, EventEmitter, OnInit } from '@angular/core';
     templateUrl: 'hallazgoRelacionadoParto.html'
 })
 export class HallazgoRelacionadoPartoComponent implements OnInit {
-    //@Input('paciente') paciente: any;
-    paciente: any; // será un IPaciente
-    //@Input('tipoPrestacion') tipoPrestacion: any;
-    tipoPrestacion: any;
+    @Input('paciente') paciente: any;
+    @Input('tipoPrestacion') tipoPrestacion: any;
     @Input('required') required: Boolean;
     @Input('datosIngreso') datosIngreso: any;
 
     @Output() evtData: EventEmitter<Number> = new EventEmitter<Number>();
 
     data: any = {
-        //valor: '',
         mensaje: {
             class: "",
             texto: ""
@@ -31,30 +28,6 @@ export class HallazgoRelacionadoPartoComponent implements OnInit {
         { id: 'Parto por Cesárea', nombre: 'Parto por Cesárea' },
     ];
     ngOnInit() {
-        this.paciente = {
-            "id": "57ebacce69fe79a598e6281d",
-            "documento": "29410428",
-            "activo": true,
-            "estado": "validado",
-            "nombre": "Carolina",
-            "apellido": "Celeste",
-            "sexo": "femenino",
-            "genero": "femenino",
-            "fechaNacimiento": "02/11/1993",
-            "estadoCivil": "soltera"
-        };
-        this.tipoPrestacion = {
-            "id": "58b6b6b2b64acd0989b9f536",
-            "key": "hallazgoRelacionaParto",
-            "nombre": "Edad gestacional Fetal",
-            "autonoma": true,
-            "activo": true,
-            "componente": {
-                "ruta": "rup/edadGestacionalFetal/edadGestacionalFetal.component.ts",
-                "nombre": "edadGestacionalFetalComponent"
-            },
-            "turneable": false
-        };
         this.data[this.tipoPrestacion.key] = (this.datosIngreso) ? this.datosIngreso : {
             valor: null,
             fecha: null
