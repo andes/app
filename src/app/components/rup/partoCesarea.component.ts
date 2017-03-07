0973be6035f00746c1fd2ca3305214a881a81666
fersastre <fersastre23@gmail.com>
2017-03-07 12:37:34 -0300

    Rup-PartoCesarea

--- src/app/components/rup/partoCesarea.component.ts
+++ src/app/components/rup/partoCesarea.component.ts
@@ -8,48 +8,21 @@ import { Component, Output, Input, EventEmitter, OnInit } from '@angular/core';
 
 export class PartoCesareaComponent implements OnInit {
 
-  //@Input('paciente') paciente: any;
-     paciente: any; // será un IPaciente
-    //@Input('tipoPrestacion') tipoPrestacion: any;
-    tipoPrestacion: any;
-    @Input('required') required: Boolean;
-    @Input('datosIngreso') datosIngreso: any;
 
-    @Output() evtData: EventEmitter<Number> = new EventEmitter<Number>();
+  @Input('paciente') paciente: any;
+  @Input('tipoPrestacion') tipoPrestacion: any;
+  @Input('required') required: Boolean;
+  @Input('datosIngreso') datosIngreso: any;
+  @Output() evtData: EventEmitter<any> = new EventEmitter<any>();
 
-    data: any = {
-        //valor: '',
-        mensaje: {
-            class: "",
-            texto: ""
-        },
-    };
+  data: any = {
+    mensaje: {
+      class: "",
+      texto: "",
+    },
+  };
 
-    ngOnInit() {
-        this.paciente = {
-            "id": "57ebacce69fe79a598e6281d",
-            "documento": "29410428",
-            "activo": true,
-            "estado": "validado",
-            "nombre": "Carolina",
-            "apellido": "Celeste",
-            "sexo": "femenino",
-            "genero": "femenino",
-            "fechaNacimiento": "02/11/1993",
-            "estadoCivil": "soltera"
-        };
-        this.tipoPrestacion = {
-            "id": "58b6cd6eb64acd0989b9f537",
-            "key": "scoreApgar",
-            "nombre": "Score Apgar",
-            "autonoma": true,
-            "activo": true,
-            "componente": {
-                "ruta": "rup/scoreApgar.component.ts",
-                "nombre": "scoreApgarComponent"
-            },
-            "turneable": false
-        };
+  ngOnInit() {
     this.data[this.tipoPrestacion.key] = (this.datosIngreso) ? this.datosIngreso : false;
   }
 
