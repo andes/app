2b96857eb70f70ee54ce8c1d362fd1a8b3354f08
NicoDino <nicodinolfo@gmail.com>
2017-03-20 11:04:40 -0300

    cambio put por patch en turnoService

--- src/app/services/turnos/turno.service.ts
+++ src/app/services/turnos/turno.service.ts
@@ -6,13 +6,13 @@ import { Server } from '@andes/shared';
 
 @Injectable()
 export class TurnoService {
-    private turnoUrl = AppSettings.API_ENDPOINT + '/modules/turnos/turno';  // URL to web api
+    private turnoUrl = AppSettings.API_ENDPOINT + '/modules/turnos';  // URL to web api
 
     constructor(private server: Server, private http: Http) { }
     save(turno: any): Observable<any> {
         if (turno.idAgenda) {
-            // return this.server.put(this.turnoUrl + '/' + turno.idAgenda, turno);
-            return this.server.patch(this.turnoUrl + '/' + turno.idAgenda, turno);
+            //return this.server.put(this.turnoUrl + '/' + turno.idAgenda, turno);
+            return this.server.patch(this.turnoUrl + '/agenda/' + turno.idAgenda + '/turno/' + turno.idTurno , turno);
         }
     }
 }
