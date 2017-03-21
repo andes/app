d09398f6d7625461a1dafe33055619b08eaa27a9
Leandro Lambertucci <llambertucci@hospitalneuquen.org.ar>
2017-03-21 13:57:18 -0300

    Validaciones

--- src/app/components/rup/atomos/talla.component.ts
+++ src/app/components/rup/atomos/talla.component.ts
@@ -30,13 +30,11 @@ export class TallaComponent implements OnInit {
 
     devolverValores() {
 
-        debugger;
-        if (!this.data[this.tipoPrestacion.key]){
+        if (this.data[this.tipoPrestacion.key] === '') {
             this.data = {};
-        } else{
-            this.mensaje = this.getMensajes();
-            this.evtData.emit(this.data);
-            }
+        }
+        this.mensaje = this.getMensajes();
+        this.evtData.emit(this.data);
     }
 
     getMensajes() {
