470cf0073fc3e31d598d113f6e86c21f4958bbb1
hugo <hfernandez@neuquen.gov.ar>
2017-04-03 20:04:08 -0300

    pipe patient

--- src/app/components/paciente/paciente-create-update.component.ts
+++ src/app/components/paciente/paciente-create-update.component.ts
@@ -74,6 +74,9 @@ import {
 import {
   MapsComponent
 } from './../../utils/mapsComponent';
+import {
+  patientPipe
+} from './../../utils/patientPipe';
 
 @Component({
   selector: 'paciente-create-update',
@@ -522,7 +525,6 @@ export class PacienteCreateUpdateComponent implements OnInit {
         operacionPac.subscribe(result => {
 
           this.plex.alert('Los datos se actualizaron correctamente');
-          //alert('Los datos se actualizaron correctamente');
           this.data.emit(result);
         });
         //   }
@@ -536,11 +538,9 @@ export class PacienteCreateUpdateComponent implements OnInit {
 
           if (result) {
             this.plex.alert('Los datos se actualizaron correctamente');
-            //alert('Los datos se actualizaron correctamente');
             this.data.emit(result);
           } else {
             this.plex.alert('ERROR: Ocurrio un problema al actualizar los datos');
-            // alert('ERROR: Ocurrio un problema al actualizar los datos');
           }
         });
         //   }
@@ -548,7 +548,6 @@ export class PacienteCreateUpdateComponent implements OnInit {
       }
     } else {
       this.plex.alert('Debe completar los datos obligatorios');
-      //alert('Debe completar los datos obligatorios');
     }
   }
 
