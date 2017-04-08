1c70b2642d0f0e62d9ccd76e10e4a511c9d41b68
hugo <hfernandez@neuquen.gov.ar>
2017-04-08 00:02:16 -0300

    fullName Pipe and realAge Pipe

--- src/app/utils/patientPipe.ts
+++ src/app/utils/patientPipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
-@Pipe({name: 'patient'})
-export class patientPipe implements PipeTransform {
+@Pipe({name: 'fullName'})
+export class patientFullNamePipe implements PipeTransform {
   transform(value: any, args: string[]): any {
     if (value.alias) {
         return value.apellido + ' ' + value.alias;
@@ -11,3 +12,36 @@ export class patientPipe implements PipeTransform {
     }
   }
 }
+@Pipe({name: 'realAge'})
+export class patientRealAgePipe implements PipeTransform {
+  transform(value: any, args: string[]): any {
+
+    let edad: any;
+    let fechaNac: any;
+    let fechaActual: Date = new Date();
+    let fechaAct: any;
+    let difAnios: any;
+    let difDias: any;
+    let difMeses: any;
+    let difHs: any;
+    let difD: any;
+
+    fechaNac = moment(value.fechaNacimiento, 'YYYY-MM-DD HH:mm:ss');
+    fechaAct = moment(fechaActual, 'YYYY-MM-DD HH:mm:ss');
+    difDias = fechaAct.diff(fechaNac, 'd'); // Diferencia en días
+    difAnios = Math.floor(difDias / 365.25);
+    difMeses = Math.floor(difDias / 30.4375);
+    difHs = fechaAct.diff(fechaNac, 'h'); // Diferencia en horas
+
+    if (difAnios !== 0) { edad = { valor: difAnios, unidad: 'Años' } }
+    else
+        if (difMeses !== 0) { edad = { valor: difMeses, unidad: 'Meses' } }
+        else
+            if (difDias !== 0) { edad = { valor: difDias, unidad: 'Dias' } }
+            else
+                if (difHs !== 0) { edad = { valor: difHs, unidad: 'Horas' } }
+    return (edad.valor + ' ' + edad.unidad); 
+
+  }
+}
+
