d2d24a48af994ab031505d12336d8c2fdedbde51
manouman <manurbano.dev@gmail.com>
2017-05-24 12:32:01 -0300

    Punto inicio sin snomed

--- src/app/components/rup/ejecucion/puntoInicio.component.ts
+++ src/app/components/rup/ejecucion/puntoInicio.component.ts
@@ -38,19 +38,6 @@ export class PuntoInicioComponent implements OnInit {
         public servicioAgenda: AgendaService, public auth: Auth,
         private router: Router, private route: ActivatedRoute) {}
 
-        public conceptos = [];
-        conceptoSnomed($e) {
-
-            console.log($e);
-        }
-        arrastrandoProblema(a) {
-            console.log(a);
-        }
-        conceptoDropeado($e) {
-            this.conceptos.push($e.dragData);
-            console.log($e);
-        }
-
     ngOnInit() {
         this.breadcrumbs = this.route.routeConfig.path;
         console.log('pantalla:', this.breadcrumbs);
