7582d31c4549fc58b2117b2c71e0f79ff9be72f6
kro <celeste.carolina.s@gmail.com>
2017-06-12 09:25:54 -0300

    control existencia financiador

--- src/app/components/paciente/paciente-create-update.component.ts
+++ src/app/components/paciente/paciente-create-update.component.ts
@@ -402,10 +402,12 @@ export class PacienteCreateUpdateComponent implements OnInit {
         elem.tipo = ((typeof elem.tipo === 'string') ? elem.tipo : (Object(elem.tipo).id));
         return elem;
       });
-      pacienteGuardar.financiador.map((elem: any) => {
-        delete elem.entidad.$order;
-        return elem;
-      });
+      if (pacienteGuardar.financiador) {
+        pacienteGuardar.financiador.map((elem: any) => {
+          delete elem.entidad.$order;
+          return elem;
+        });
+      }
 
       // Luego aquí habría que validar pacientes de otras prov. y paises (Por ahora solo NQN)
       pacienteGuardar.direccion[0].ubicacion.pais = this.paisArgentina;
