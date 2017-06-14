9784449848d2269ef1e8fe6052592dbb2baa9d38
hugo <hfernandez@neuquen.gov.ar>
2017-06-14 14:11:04 -0300

    fix load pacientes

--- src/app/pipes/profesional.pipe.ts
+++ src/app/pipes/profesional.pipe.ts
@@ -4,6 +4,9 @@ import * as moment from 'moment';
 @Pipe({ name: 'profesional' })
 export class ProfesionalPipe implements PipeTransform {
     transform(value: any, args?: string[]): any {
-        return value.apellido + ', ' + value.nombre;
+        if (value) {
+            return value.apellido + ', ' + value.nombre;
+        }
+        
     }
 }
