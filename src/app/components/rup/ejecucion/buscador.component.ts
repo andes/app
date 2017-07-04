1aab0c423db242da3276684f0c03fad0a0eb6453
fersastre <fersastre23@gmail.com>
2017-07-04 08:49:25 -0300

    Rup-ComponenteBuscador

---
+++ src/app/components/rup/ejecucion/buscador.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Output, Input, EventEmitter, AfterViewInit, HostBinding, ViewEncapsulation } from '@angular/core';
+import { TipoPrestacionService } from './../../../services/tipoPrestacion.service';
+
+
+@Component({
+    selector: 'rup-buscador',
+    templateUrl: 'buscador.html',
+
+})
+
+export class BuscadorComponent implements OnInit {
+    public searchPlanes: String = '';
+    //Lista de planes.
+    public listaPlanes: any[] = [];
+     // concepto snomed seleccionado del buscador a ejecutar
+    public conceptoSnomedSeleccionado: any;
+
+    // array de resultados a guardar devueltos por RUP
+    public data: any[] = [];
+
+    //Variable a pasar al buscador de Snomed.. Indica el tipo de busqueda
+    public tipoBusqueda: string = 'problemas'; //Por defecto trae los problemas
+    public showPlanes: boolean = false;
+    public ejecucion: any[] = [];
+
+
+    constructor(public servicioTipoPrestacion: TipoPrestacionService) {
+    }
+
+    ngOnInit() { }
+
+
+
+    // Buscador de planes
+    buscar() {
+        if (this.searchPlanes !== null) {
+            this.servicioTipoPrestacion.get({ term: this.searchPlanes }).subscribe(tiposPrestacion => {
+                this.listaPlanes = tiposPrestacion;
+            });
+        } else {
+            this.listaPlanes = [];
+        }
+    }
+
+    //Recibe el parametro y lo setea para realizar la busqueda en Snomed
+    filtroBuscadorSnomed(tipoBusqueda) {
+        this.showPlanes = false;// Oculta el buscador de planes
+        console.log(tipoBusqueda);
+        this.tipoBusqueda = tipoBusqueda;
+    }
+    //Muestra el buscador de planes
+    busquedaPlanes() {
+        this.tipoBusqueda = 'planes';
+        this.showPlanes = true;
+    }
+}
