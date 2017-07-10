6ce994d53c6303a8ed094d9a4d67093f789fa02d
NicoDino <nicodinolfo@gmail.com>
2017-07-10 11:34:12 -0300

    fix temporal (parche) en textFilterPipe

--- src/app/pipes/textFilter.pipe.ts
+++ src/app/pipes/textFilter.pipe.ts
@@ -10,11 +10,13 @@ export class TextFilterPipe implements PipeTransform {
         if (!items || !value || value.length === 0) {
             return items;
         }
+        // value = value.trim();
         debugger;
-        value = value.trim();
+        // PARCHE HASTA CONTAR CON INTERFAZ DE PERMISOS
+        items.forEach((item: any) => item.usuario = item.usuario.toString());
         return items.filter((item: any) =>
 
-            ((item.nombre) ? (item.usuario.trim().toUpperCase().search(value.toUpperCase()) > -1) : '') ||
+            ((item.usuario) ? (item.usuario.trim().toUpperCase().search(value.toUpperCase()) > -1) : '') ||
             ((item.documento) ? (item.documento.trim().toUpperCase().search(value.toUpperCase()) > -1) : '') ||
             ((item.nombre) ? (item.nombre.trim().toUpperCase().search(value.toUpperCase()) > -1) : '') ||
             ((item.apellido) ? (item.apellido.trim().toUpperCase().search(value.toUpperCase()) > -1) : '')
