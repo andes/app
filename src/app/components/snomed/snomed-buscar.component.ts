32327f2dd9826c16d133142facfbc8934c956e62
manouman <manurbano.dev@gmail.com>
2017-08-10 15:05:37 -0300

    hot fix buscador snomed

--- src/app/components/snomed/snomed-buscar.component.ts
+++ src/app/components/snomed/snomed-buscar.component.ts
@@ -197,12 +197,12 @@ export class SnomedBuscarComponent implements OnInit, OnChanges {
                     this.loading = false;
                     this.resultados = problemas;
 
-                    if (this.tipoBusqueda === 'procedimientos') {
-                        // Filtrar de los resultado las prestaciones turneables
-                        this.resultados = this.resultados.filter(concepto => {
-                            return this.cachePrestacionesTurneables.findIndex(c => c.conceptId === concepto.conceptId) <= -1;
-                        });
-                    }
+                    // if (this.tipoBusqueda === 'procedimientos') {
+                    //     // Filtrar de los resultado las prestaciones turneables
+                    //     this.resultados = this.resultados.filter(concepto => {
+                    //         return this.cachePrestacionesTurneables.findIndex(c => c.conceptId === concepto.conceptId) <= -1;
+                    //     });
+                    // }
 
                 }, err => {
                     this.loading = false;
