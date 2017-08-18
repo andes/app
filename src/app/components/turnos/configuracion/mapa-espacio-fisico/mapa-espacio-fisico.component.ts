4a059049e60f3818971c961fdbaa6d0e3a4e25ec
Mariano Botta <marianoabotta@gmail.com>
2017-08-18 09:06:49 -0300

    Mapa de espacios fisicos

--- src/app/components/turnos/configuracion/mapa-espacio-fisico/mapa-espacio-fisico.component.ts
+++ src/app/components/turnos/configuracion/mapa-espacio-fisico/mapa-espacio-fisico.component.ts
@@ -7,6 +7,7 @@ import { IEspacioFisico } from './../../../../interfaces/turnos/IEspacioFisico';
 import { IAgenda } from './../../../../interfaces/turnos/IAgenda';
 import { EspacioFisicoService } from './../../../../services/turnos/espacio-fisico.service';
 import { OrganizacionService } from './../../../../services/organizacion.service';
+import { AgendaService } from './../../../../services/turnos/agenda.service';
 
 import * as moment from 'moment';
 
@@ -17,44 +18,89 @@ import * as moment from 'moment';
 })
 export class MapaEspacioFisicoComponent implements OnInit, OnChanges {
     @Input() espacioTable: IEspacioFisico[] = [];
-    @Input() agendasTable: IAgenda[] = [];
+    @Input() opciones: any;
+    @Input() agendaSeleccionada: IAgenda = null;
 
-    @Input() start: Date;
-    @Input() end: Date;
-    @Input() unit: String;
+    @Output() onEspacioClick = new EventEmitter<IEspacioFisico>();
+
+    @Input() agendasTable: IAgenda[] = null;
+
+    private start: any;
+    private end: any;
+    private unit: String;
 
     private _start: any;
     private _end: any;
+    private _unit: any;
 
     private headers = [];
     private matrix: any;
 
     constructor(
-        public plex: Plex) { }
+        public plex: Plex,
+        public servicioAgenda: AgendaService) { }
+
 
     ngOnInit() {
-        this.calcHeaders();
-        this.calcRows();
-        this.calcItems();
+        this.refreshScreen();
     }
 
-    ngOnChanges(a) {
-        this.calcHeaders();
-        this.calcRows();
-        this.calcItems();
+    ngOnChanges() {
+        this.refreshScreen();
     }
+
+    aproximar(date, cotaInferior) {
+        let m = date.get('minutes');
+        let remaider = m % this._unit;
+        if (remaider !== 0) {
+            if (cotaInferior) {
+                date.subtract(remaider, 'minutes');
+            } else {
+                date.add(this._unit - remaider, 'minutes');
+            }
+        }
+        return date;
+    }
+
+    refreshScreen() {
+        if (!this.opciones) {
+            this.start = moment(this.agendaSeleccionada.horaInicio).startOf('hour').set('hour', 7);
+            this.end = moment(this.agendaSeleccionada.horaFin).startOf('hour').set('hour', 20);
+            this.unit = '10';
+        } else {
+            this.start = moment(this.opciones.start);
+            this.end = moment(this.opciones.end);
+            this.unit = this.unit;
+        }
+        if (this.unit !== 'day') {
+            this._unit = Number(this.unit);
+        }
+        this.servicioAgenda.get({ fechaDesde: this.start, fechaHasta: this.end }).subscribe((agendas) => {
+            this.agendasTable = agendas;
+            this.calcHeaders();
+            this.calcRows();
+            this.calcItems();
+        });
+    }
+
 
     calcItems() {
         this.agendasTable.forEach(agenda => {
-            if (agenda.espacioFisico) {
-                let _id = agenda.espacioFisico.id;
-                let temp = this.matrix.find(item => item.id === _id);
-                if (temp) {
-                    temp.items.push({
-                        id: agenda.id,
-                        start: moment(agenda.horaInicio),
-                        end: moment(agenda.horaFin)
-                    });
+            let start_time = moment(agenda.horaInicio);
+            let end_time = moment(agenda.horaFin);
+            if (start_time >= this._start && end_time <= this._end) {
+                if (agenda.espacioFisico) {
+                    let _id = agenda.espacioFisico.id;
+                    let temp = this.matrix.find(item => item.id === _id);
+                    if (temp) {
+                        temp.items.push({
+                            id: agenda.id,
+                            titulo: agenda.tipoPrestaciones[0].term,
+                            descripcion: agenda.profesionales.length ? agenda.profesionales[0].nombre : '',
+                            start: this.aproximar(start_time, false),
+                            end: this.aproximar(end_time, true)
+                        });
+                    }
                 }
             }
         });
@@ -64,42 +110,84 @@ export class MapaEspacioFisicoComponent implements OnInit, OnChanges {
 
             let temp = this._start.clone();
             espacio.items.forEach(item => {
-                let span = this.calcFrame(temp, item.start);
-                for (let i = 0; i < span; i++) {
-                    espacio._items.push({
-                        colspan: 1
-                    });
-                }
+
+                this.iterarLibres(espacio._items, temp, item.start);
 
                 item.colspan = this.calcFrame(item.start, item.end);
                 espacio._items.push(item);
 
                 temp = item.end.clone();
             });
-
-            let span = this.calcFrame(temp, this._end);
-            for (let i = 0; i < span + 1; i++) {
-                espacio._items.push({
-                    colspan: 1
-                });
-            }
+            this.iterarLibres(espacio._items, temp, this._end);
 
         });
     }
 
+    iterarLibres(items, start, end) {
+        let ini = this.aproximar(moment(this.agendaSeleccionada.horaInicio), false);
+        let fin = this.aproximar(moment(this.agendaSeleccionada.horaFin), true);
+        let span = this.calcFrame(start, end);
+        let unit = parseInt(this.unit.toString(), 0);
+        for (let i = 0; i < span; i++) {
+            let it: any = {
+                colspan: 1,
+                time: start.clone().add(unit * i, 'minutes')
+            };
+            it.disponible = it.time >= ini && it.time.add(unit, 'minutes') <= fin;
+            items.push(it);
+        }
+    }
+
     calcRows() {
-        this.matrix = [];
+        let matrix = [];
         if (this.espacioTable) {
             this.espacioTable.forEach(espacio => {
-                this.matrix.push({
+                matrix.push({
                     id: espacio.id,
                     name: espacio.nombre,
+                    descripcion: espacio.edificio.descripcion,
                     _items: [],
                     items: []
                 });
             });
         }
+        this.matrix = matrix;
     }
+
+
+
+    calcHeaders() {
+        let headers = [];
+        this._start = moment(this.start);
+        this._end = moment(this.end);
+        let temp;
+        switch (this.unit) {
+            case 'day':
+                this._start.startOf('day');
+                this._end.endOf('day');
+                temp = this._start.clone();
+                while (temp < this._end) {
+                    headers.push({ date: temp, text: this._start.format('DD'), colspan: 1 });
+                    temp = temp.add(1, 'days');
+                }
+                break;
+            default:
+                this._start.startOf('hour');
+                this._end.endOf('hour');
+                temp = this._start.clone();
+                while (temp < this._end) {
+                    headers.push({ date: temp, text: temp.format('HH:mm'), colspan: 60 / this._unit });
+                    temp = temp.add(1, 'hour');
+                    // temp = temp.add(unit, 'minutes');
+                }
+                break;
+        }
+
+
+        this.headers = headers;
+
+    }
+
 
     calcFrame(start, end) {
         switch (this.unit) {
@@ -115,46 +203,9 @@ export class MapaEspacioFisicoComponent implements OnInit, OnChanges {
         }
     }
 
-    calcHeaders() {
-        this.headers = [];
-        this._start = moment(this.start);
-        this._end = moment(this.end);
-        this.headers.push({ colspan: 1, text: 'Espacios físicos' });
-        let temp;
-        switch (this.unit) {
-            case 'day':
-                this._start = this._start.startOf('day');
-                this._end = this._end.endOf('day');
-                temp = this._start.clone();
-                while (temp < this._end) {
-                    this.headers.push({ date: temp, text: this._start.format('DD'), colspan: 1 });
-                    temp = temp.add(1, 'days');
-                }
-                break;
-            case 'hour':
-                this._start = this._start.startOf('hour');
-                this._end = this._end.endOf('hour');
-                temp = this._start.clone();
-                while (temp < this._end) {
-                    this.headers.push({ date: temp, text: temp.format('HH:mm'), colspan: 1 });
-                    temp = temp.add(1, 'hour');
-                }
-                break;
-            default:
-                let unit = parseInt(this.unit.toString(), 0);
-                this._start = this._start.startOf('hour');
-                this._end = this._end.endOf('hour');
-                temp = this._start.clone();
-                while (temp < this._end) {
-                    this.headers.push({ date: temp, text: temp.format('HH:mm'), colspan: 60 / unit });
-                    temp = temp.add(1, 'hour');
-                    // temp = temp.add(unit, 'minutes');
-                }
-                break;
-        }
-
+    onClick(espacio) {
+        this.onEspacioClick.emit(this.espacioTable.find(item => item.id === espacio.id));
     }
-
 
 
 
