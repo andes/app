b15a2b9909e6cb473844a50b3ad1598fc77c38eb
hugo <hfernandez@neuquen.gov.ar>
2017-08-18 18:50:58 -0300

    permisos acceso a modulos

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -10,10 +10,50 @@ import { Wizard } from './../../classes/wizard.class';
 })
 export class InicioComponent implements AfterViewInit {
     @HostBinding('class.plex-layout') layout = true;
+    public turnos = '';
+    public mpi = '';
+    public rup = '';
+    public denied = false;
+    public accessList: any = [];
 
     constructor(private plex: Plex, public auth: Auth) { }
 
     ngAfterViewInit() {
+ 
+        let accessList = [];
+        let menuList = [];
+
+        // Cargo el array de permisos
+        if (this.auth.getPermissions('turnos:?').length > 0) {
+            this.turnos = 'turnos';
+            accessList.push({label: 'CITAS: Agendas & Turnos', icon: 'calendar', route: '/citas/gestor_agendas'});
+        }
+        if (this.auth.getPermissions('mpi:?').length > 0) {
+            this.mpi = 'mpi';
+            accessList.push({label: 'MPI: Indice Maestro de Pacientes', icon: 'account-multiple-outline', route: '/mpi'});
+        }
+
+        if (this.auth.getPermissions('rup:?').length > 0) {
+            this.rup = 'rup';
+            accessList.push({label: 'RUP: Registro Universal de Prestaciones', icon: 'contacts', route: '/rup'});
+        }
+
+        if (accessList.length <= 0) {
+            this.denied = true;
+        }
+        menuList.push({ label: 'Página principal', icon: 'home', route: '/inicio' });
+        accessList.forEach((permiso) => {
+            menuList.push(permiso);
+        });
+        menuList.push({ divider: true });
+        menuList.push({ label: 'Cerrar Sesión', icon: 'logout', route: '/login' });
+
+        // Actualizamos la lista de menú
+        this.plex.updateMenu(menuList);
+
+
+        // Por ahora desactivamos el wizard!
+
         // let wizard = new Wizard('turnos');
         // wizard.addStep('Bienvenido al módulo de Agendas & Turnos', 'Este asistente lo ayudará a empezar a trabajar');
         // wizard.addStep('Crear la agenda', 'El primer paso es crear una agenda a través del Gestor de Agendas');
