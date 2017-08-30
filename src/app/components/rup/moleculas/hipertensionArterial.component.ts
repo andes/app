3f4f7bba351cdd4c54ffffaca6d8d6c2e989fb43
fersastre <fersastre23@gmail.com>
2017-08-30 09:56:52 -0300

    Atomo de hipertencion y obesidad

---
+++ src/app/components/rup/moleculas/hipertensionArterial.component.ts
@@ -0,0 +1,11 @@
+import { Molecula } from './../core/molecula.component';
+import { Component, OnInit, Output, Input, EventEmitter, AfterViewInit } from '@angular/core';
+
+@Component({
+    selector: 'rup-hipertensionArterial',
+    templateUrl: 'hipertensionArterial.html'
+})
+
+export class HipertensionArterialComponent  extends Molecula {
+// tslint:disable-next-line:eofline
+}
