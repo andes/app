c16a3c194c12ae9c6add8c8ba98b0fabed892478
fersastre <fersastre23@gmail.com>
2017-09-19 15:11:05 -0300

    alertas en frecuencia respiratoria, peso y saturacion de oxigeno

--- src/app/modules/rup/components/elementos/saturacionOxigeno.component.ts
+++ src/app/modules/rup/components/elementos/saturacionOxigeno.component.ts
@@ -16,37 +16,40 @@ export class SaturacionOxigenoComponent extends RUPComponent implements OnInit {
                 this.emitChange(false);
             }
         });
+        if (this.registro.valor) {
+            this.mensaje = this.getMensajes();
+        }
     }
-    //     getMensajes() {
-    //     let saturacionOxigeno = this.data[this.elementoRUP.key];
-    //     let edadEnMeses;
+        getMensajes() {
+        let saturacionOxigeno = this.registro.valor;
+        let edadEnMeses;
 
-    //     // Calculo Edad en Meses
-    //     let edadMeses: any = null;
-    //     let fechaNac: any;
-    //     let fechaActual: Date = new Date();
-    //     let fechaAct: any;
-    //     let difDias: any;
-    //     let difMeses: any;
-    //     fechaNac = moment(this.paciente.fechaNacimiento, 'YYYY-MM-DD HH:mm:ss');
-    //     fechaAct = moment(fechaActual, 'YYYY-MM-DD HH:mm:ss');
-    //     difDias = fechaAct.diff(fechaNac, 'd');     // Diferencia en días
-    //     edadEnMeses = Math.trunc(difDias / 30.4375); // Diferencia en Meses
+        // Calculo Edad en Meses
+        let edadMeses: any = null;
+        let fechaNac: any;
+        let fechaActual: Date = new Date();
+        let fechaAct: any;
+        let difDias: any;
+        let difMeses: any;
+        fechaNac = moment(this.paciente.fechaNacimiento, 'YYYY-MM-DD HH:mm:ss');
+        fechaAct = moment(fechaActual, 'YYYY-MM-DD HH:mm:ss');
+        difDias = fechaAct.diff(fechaNac, 'd');     // Diferencia en días
+        edadEnMeses = Math.trunc(difDias / 30.4375); // Diferencia en Meses
 
-    //     let mensaje: any = {
-    //         texto: '',
-    //         class: 'danger'
-    //     };
+        let mensaje: any = {
+            texto: '',
+            class: 'danger'
+        };
 
-    //     if (saturacionOxigeno) {
-    //         // agregar validaciones aca en base al paciente y el tipo de prestacion
-    //         if (saturacionOxigeno >= 90 && saturacionOxigeno <= 94) {
-    //             mensaje.texto = 'Hipoxemia';
-    //         }
-    //         if (saturacionOxigeno <= 94) {
-    //             mensaje.texto = 'Hipoxemia Severa';
-    //         }
-    //     }
-    //     return mensaje;
-    // }
+        if (saturacionOxigeno) {
+            // agregar validaciones aca en base al paciente y el tipo de prestacion
+            if (saturacionOxigeno >= 90 && saturacionOxigeno <= 94) {
+                mensaje.texto = 'Hipoxemia';
+            }
+            if (saturacionOxigeno <= 94) {
+                mensaje.texto = 'Hipoxemia Severa';
+            }
+        }
+        return mensaje;
+    }
 }
