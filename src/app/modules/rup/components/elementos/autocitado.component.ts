b52c5c4bf7b907a591e288f727eb538b6351b3c1
silviroa <silviroa@gmail.com>
2017-11-01 13:28:28 -0300

    ver la prestación seleccionada de autocitación en la validación

--- src/app/modules/rup/components/elementos/autocitado.component.ts
+++ src/app/modules/rup/components/elementos/autocitado.component.ts
@@ -13,13 +13,15 @@ export class AutocitadoComponent extends RUPComponent implements OnInit {
     public darTurnoEmit = new EventEmitter<any>();
 
     ngOnInit() {
-        this.registro.valor = (this.registro.valor) ? this.registro.valor : {};
-        this.registro.valor['autocitado'] = true;
-
-        // vamos a buscar los tipos de prestaciones en las agendas donde este incluido el profesional
-
+        // Buscamos los tipos de prestación que sean turneables para los que el tenga permisos
+        // (OBS: a futuro un profesional puede tener permisos para mas prestaciones que no sean turneables)
         this.servicioTipoPrestacion.get({ id: this.auth.getPermissions('rup:tipoPrestacion:?') }).subscribe(data => {
             this.tiposPrestacion = data;
+            if (!this.registro.valor) {
+                this.registro.valor = {};
+                this.registro.valor['autocitado'] = true;
+                this.registro.valor['prestacionSolicitada'] = this.tiposPrestacion.find(tp => tp.conceptId === this.prestacion.solicitud.tipoPrestacion.conceptId);
+            }
         });
     }
 
