8688576dad1f8914f62ca0c2c301d7a70b375a05
silviroa <silviroa@gmail.com>
2017-11-02 13:35:47 -0300

    linteo

--- src/app/modules/rup/components/elementos/frecuenciaRespiratoria.component.ts
+++ src/app/modules/rup/components/elementos/frecuenciaRespiratoria.component.ts
@@ -36,30 +36,7 @@ export class FrecuenciaRespiratoriaComponent extends RUPComponent implements OnI
         Sexo = this.paciente.sexo;
         Edad = this.paciente.edad;
         frecuenciaRespiratoria = this.registro.valor;
-        debugger;
         if (frecuenciaRespiratoria) {
-            // Ver validacines para NEO - Ver unidad de la edad
-            // Ver validaciones - Falta definición para lograr identidicarlos
-            // if (this.paciente.edadReal.unidad === 'Días' || this.paciente.edadReal.unidad === 'Meses' || this.paciente.edadReal.unidad === 'Horas') {
-
-            //     // Prematuro -ver el peso del paciente
-            //     if (frecuenciaRespiratoria >= 40 && frecuenciaRespiratoria <= 90) {
-            //         mensaje.texto = 'Paciente Prematuro: Dentro de los parámetros normales';
-            //     } else {
-            //         mensaje.texto = 'Paciente Prematuro: Fuera de los parámetros normales';
-            //     }
-
-            //     // Recien Nacido a término
-            //     if (frecuenciaRespiratoria >= 30 && frecuenciaRespiratoria <= 80) {
-            //         mensaje.texto = 'Paciente Recien Nacido a término: Dentro de los parámetros normales';
-            //     } else {
-            //         mensaje.texto = 'Paciente Recien Nacido a término: Fuera de los parámetros normales';
-            //     }
-            // }
-
-            // Pacientes Años
-            // if (this.paciente.edadReal.unidad === 'Años') {
-
             if (Edad <= 1) {
                 if (frecuenciaRespiratoria >= 20 && frecuenciaRespiratoria <= 40) {
                     mensaje.texto = 'Dentro de los parámetros normales';
