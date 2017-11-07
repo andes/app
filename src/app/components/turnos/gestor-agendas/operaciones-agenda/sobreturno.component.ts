9218651e041bebfba688f27fa9e9ae7509efaee3
kro <celeste.carolina.s@gmail.com>
2017-11-07 09:57:08 -0300

    fix navegación sobreturno gestor

--- src/app/components/turnos/gestor-agendas/operaciones-agenda/sobreturno.component.ts
+++ src/app/components/turnos/gestor-agendas/operaciones-agenda/sobreturno.component.ts
@@ -28,7 +28,7 @@ export class AgregarSobreturnoComponent implements OnInit {
         return this._agenda;
     }
 
-    @Output() volverAlGestor = new EventEmitter<IAgenda>();
+    @Output() volverAlGestor = new EventEmitter<boolean>();
     @Output() selected: EventEmitter<any> = new EventEmitter<any>();
     @Output() escaneado: EventEmitter<any> = new EventEmitter<any>();
 
@@ -182,7 +182,7 @@ export class AgregarSobreturnoComponent implements OnInit {
 
             this.serviceAgenda.patch(this.agenda.id, patch).subscribe(resultado => {
                 this.plex.toast('success', 'Información', 'El sobreturno se guardó correctamente');
-                this.volverAlGestor.emit(resultado);
+                this.volverAlGestor.emit(true);
             });
         } else {
             this.plex.alert('Debe completar los datos requeridos');
@@ -207,6 +207,6 @@ export class AgregarSobreturnoComponent implements OnInit {
     }
 
     cancelar() {
-        this.volverAlGestor.emit();
+        this.volverAlGestor.emit(true);
     }
 }
