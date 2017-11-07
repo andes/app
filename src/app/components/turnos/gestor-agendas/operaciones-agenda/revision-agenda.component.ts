81709d574a34744f163a3186e3f816ae0a64379b
kro <celeste.carolina.s@gmail.com>
2017-11-07 10:50:23 -0300

    agrego "agregar sobreturno" en revision

--- src/app/components/turnos/gestor-agendas/operaciones-agenda/revision-agenda.component.ts
+++ src/app/components/turnos/gestor-agendas/operaciones-agenda/revision-agenda.component.ts
@@ -76,6 +76,7 @@ export class RevisionAgendaComponent implements OnInit {
     @Output() escaneado: EventEmitter<any> = new EventEmitter<any>();
 
     showRevisionAgenda: Boolean = true;
+    showAgregarSobreturno: Boolean = false;
     turnosAsignados = [];
     sobreturnos = [];
     horaInicio: any;
@@ -360,9 +361,32 @@ export class RevisionAgendaComponent implements OnInit {
         }
     }
 
+    agregarSobreturno(){
+        this.showAgregarSobreturno = true;
+        this.showRevisionAgenda = false;
+        this.modoCompleto = false;
+    }
+
+    refresh() {
+        this.serviceAgenda.getById(this._agenda.id).subscribe(agenda => {
+            this._agenda = agenda;
+
+        }, err => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+
     volver() {
         this.volverAlGestor.emit(true);
+    }
 
+    volverRevision() {
+        this.showAgregarSobreturno = false;
+        this.showRevisionAgenda = true;
+        this.modoCompleto = true;
+        this.refresh();
     }
 
 }
