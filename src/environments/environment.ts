cd696b0fc6d802e0e7382927ff1185a51dfb5f0b
Carolina Celeste <celeste.carolina.s@gmail.com>
2017-11-28 11:45:07 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,6 @@
 
 export const environment = {
   production: false,
-  // API: '//localhost:3002/api',
-  API: 'https://d-testing.hospitalneuquen.org.ar/api',
+  API: '//localhost:3002/api',
   APIStatusCheck: false
 };
