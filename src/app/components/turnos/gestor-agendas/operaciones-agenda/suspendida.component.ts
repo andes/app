fd23cc454301929eff35b10967eb2b7290b7c4ea
NicoDino <nicodinolfo@gmail.com>
2018-01-22 11:54:17 -0300

    agrego service agenda en suspendida

--- src/app/components/turnos/gestor-agendas/operaciones-agenda/suspendida.component.ts
+++ src/app/components/turnos/gestor-agendas/operaciones-agenda/suspendida.component.ts
@@ -3,6 +3,7 @@ import { Plex } from '@andes/plex';
 import { IAgenda } from './../../../../interfaces/turnos/IAgenda';
 import { ITurno } from './../../../../interfaces/turnos/ITurno';
 import { EstadosAgenda } from './../../enums';
+import { AgendaService } from '../../../../services/turnos/agenda.service';
 
 
 
@@ -13,10 +14,12 @@ import { EstadosAgenda } from './../../enums';
 
 export class SuspendidaComponent implements OnInit {
 
+
     @Input() agenda: IAgenda;
     @Output() cancelaSuspenderAgenda = new EventEmitter<boolean>();
 
-    constructor(public plex: Plex) { }
+
+    constructor(public plex: Plex, public serviceAgenda: AgendaService) { }
 
     public motivoSuspensionSelect = { select: null };
     public motivoSuspension: { id: number; nombre: string; }[];
@@ -45,6 +48,52 @@ export class SuspendidaComponent implements OnInit {
     suspenderTurno() {
         this.showConfirmar = false;
         this.showData = true;
+        if (this.motivoSuspensionSelect.select.nombre === null) {
+            return;
+        }
+
+        let patch: any;
+        patch = {
+            op: 'suspenderTurno',
+            turnos: this.turnos.map((resultado) => { return resultado.id; }),
+            motivoSuspension: this.motivoSuspensionSelect.select.nombre
+        };
+
+
+        // Patchea los turnosSeleccionados (1 o más)
+        this.serviceAgenda.patch(this.agenda.id, patch).subscribe(
+
+            resultado => {
+                this.agenda = resultado;
+                if (this.turnos.length === 1) {
+                    this.plex.toast('warning', 'El turno seleccionado fue suspendido');
+                } else {
+                    this.plex.toast('warning', 'Los turnos seleccionados fueron suspendidos');
+                }
+
+
+                // // Se envían SMS sólo en Producción
+                // if (environment.production === true) {
+                //     for (let x = 0; x < this.seleccionadosSMS.length; x++) {
+
+                //         let dia = moment(this.seleccionadosSMS[x].horaInicio).format('DD/MM/YYYY');
+                //         let horario = moment(this.seleccionadosSMS[x].horaInicio).format('HH:mm');
+                //         let mensaje = 'Le informamos que su turno del dia ' + dia + ' a las ' + horario + ' horas fue suspendido.';
+                //         this.enviarSMS(this.seleccionadosSMS[x].paciente, mensaje);
+                //     };
+                // } else {
+                //     this.plex.toast('info', 'INFO: SMS no enviado (activo sólo en Producción)');
+                // }
+            },
+            err => {
+                if (err) {
+                    console.log(err);
+                }
+            }
+
+        );
+        this.cancelaSuspenderAgenda.emit(true);
+
     }
 
     cancelar() {
