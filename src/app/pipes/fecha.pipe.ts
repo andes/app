0b00c59a51368d3df4f31c17f1644faf9a92350e
Andrrr <andresin@gmail.com>
2018-02-08 11:41:07 -0300

    Pipe "fecha" opcionalmente mustra también horas, minutos y segundos

--- src/app/pipes/fecha.pipe.ts
+++ src/app/pipes/fecha.pipe.ts
@@ -3,8 +3,14 @@ import * as moment from 'moment';
 
 @Pipe({ name: 'fecha' })
 export class FechaPipe implements PipeTransform {
-    transform(value: any, args: string[]): any {
-        let unaFecha = moment(value).format('DD/MM/YYYY');
-        return (unaFecha);
+    transform(value: any, args: string[], hora = false): any {
+        let fecha: any;
+        console.log(hora);
+        if (hora) {
+            fecha = moment(value).format('DD/MM/YYYY H:m:s');
+        } else {
+            fecha = moment(value).format('DD/MM/YYYY');
+        }
+        return (fecha);
     }
 }
