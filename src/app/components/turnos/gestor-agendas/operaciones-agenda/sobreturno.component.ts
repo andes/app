5d81ea6501b470d46db8e01a8e4a6928def10447
NicoDino <nicodinolfo@gmail.com>
2018-03-08 13:00:44 -0300

    agrego tipo de prestacion a los sobreturnos p/ agendas con 1 solo tipo

--- src/app/components/turnos/gestor-agendas/operaciones-agenda/sobreturno.component.ts
+++ src/app/components/turnos/gestor-agendas/operaciones-agenda/sobreturno.component.ts
@@ -87,6 +87,9 @@ export class AgregarSobreturnoComponent implements OnInit {
         };
         this.showSobreturno = false;
         this.pacientesSearch = true;
+        if (this.agenda.tipoPrestaciones.length == 1) {
+            this.tipoPrestacion = this.agenda.tipoPrestaciones[0];
+        }
     }
 
     afterCreateUpdate(paciente) {
