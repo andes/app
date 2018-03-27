96129ba2f12769ca12d875d6c363b504ff7375d0
fersastre <fersastre23@gmail.com>
2018-03-27 10:20:14 -0300

    elemento rup Informe de epicrisis

---
+++ src/app/modules/rup/components/elementos/informeEpicrisis.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { RUPComponent } from './../core/rup.component';
+
+@Component({
+    selector: 'rup-InformeEpicrisisComponent',
+    templateUrl: 'informeEpicrisis.html'
+})
+export class InformeEpicrisisComponent extends RUPComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+
+}
