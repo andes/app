f62fedfb41db8a8bbf9bf8ac749b699c21700e78
fersastre <fersastre23@gmail.com>
2018-04-11 10:41:42 -0300

    imprimir censo diario en proceso...

--- src/app/modules/rup/components/ejecucion/internacion/censoDiario.component.ts
+++ src/app/modules/rup/components/ejecucion/internacion/censoDiario.component.ts
@@ -7,7 +7,10 @@ import { InternacionService } from '../../../services/internacion.service';
 import { OrganizacionService } from '../../../../../services/organizacion.service';
 import * as moment from 'moment';
 import { forEach } from '@angular/router/src/utils/collection';
-
+import { DocumentosService } from '../../../../../services/documentos.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Slug } from 'ng2-slugify';
+import { saveAs } from 'file-saver';
 
 @Component({
     templateUrl: 'censoDiario.html'
@@ -30,26 +33,16 @@ export class CensoDiarioComponent implements OnInit {
         semanticTag: 'procedimiento'
     };
 
-
-    public resumenCenso = {
-        existencia0: 0,
-        ingresos: 0,
-        pasesDe: 0,
-        egresosAlta: 0,
-        egresosDefuncion: 0,
-        pasesA: 0,
-        existencia24: 0,
-        ingresoEgresoDia: 0,
-        pacientesDia: 0,
-        disponibles24: 0,
-        disponibles0: 0
-    };
+    // Usa el keymap 'default'
+    private slug = new Slug('default');
 
     constructor(private router: Router, private route: ActivatedRoute,
         private plex: Plex, public auth: Auth,
         public camasService: CamasService,
         private organizacionService: OrganizacionService,
-        private servicioInternacion: InternacionService) { }
+        private servicioInternacion: InternacionService,
+        private servicioDocumentos: DocumentosService,
+        private sanitizer: DomSanitizer) { }
 
     ngOnInit() {
 
@@ -65,8 +58,7 @@ export class CensoDiarioComponent implements OnInit {
         };
         this.servicioInternacion.getInfoCenso(params).subscribe((respuesta: any) => {
             console.log(respuesta);
-            this.listadoCenso = respuesta.censoDiario;
-            this.resumenCenso = respuesta.resumen;
+            this.listadoCenso = respuesta;
             // this.completarResumenDiario();
         });
     }
@@ -76,5 +68,66 @@ export class CensoDiarioComponent implements OnInit {
     }
 
 
+
+
+
+
+    descargarCenso() {
+        setTimeout(() => {
+
+            let content = '';
+            // let headerPrestacion: any = document.getElementById('pageHeader').cloneNode(true);
+            // let datosSolicitud: any = document.getElementById('datosSolicitud').cloneNode(true);
+
+            /**
+             * Cada logo va a quedar generado como base64 desde la API:
+             *
+             * <img src="data:image/png;base64,..." class="logoAndes">
+             * <img src="data:image/png;base64,..." class="logotipoAndes">
+             * <img src="data:image/png;base64,..." class="logoPDP">
+             *
+             */
+            let tabla = document.getElementById('tabla');
+
+            // const header =
+            content += tabla.innerHTML;
+
+            console.log(content);
+            // content += header;
+            // content += `
+            // <div class="paciente">
+            //     <b>Paciente:</b> ${this.paciente.apellido}, ${this.paciente.nombre} - 
+            //     ${this.paciente.documento} - ${moment(this.paciente.fechaNacimiento).fromNow(true)}
+            // </div>
+            // `;
+
+            // agregamos prestaciones
+            // let elementosRUP: HTMLCollection = document.getElementsByClassName('rup-card');
+
+            // const total = elementosRUP.length;
+            // for (let i = 0; i < total; i++) {
+            //     content += ' <div class="rup-card">' + elementosRUP[i].innerHTML + '</div>';
+            // }
+
+            // Sanitizar? no se recibe HTML "foráneo", quizá no haga falta
+            // content = this.sanitizer.sanitize(1, content);
+
+            this.servicioDocumentos.descargar(content).subscribe(data => {
+                if (data) {
+                    // Generar descarga como PDF
+                    this.descargarArchivo(data, { type: 'application/pdf' });
+                } else {
+                    // Fallback a impresión normal desde el navegador
+                    window.print();
+                }
+            });
+        });
+    }
+
+    private descargarArchivo(data: any, headers: any): void {
+        let blob = new Blob([data], headers);
+        let nombreArchivo = this.slug.slugify('CENSODIARIO' + '-' + moment().format('DD-MM-YYYY-hmmss')) + '.pdf';
+        saveAs(blob, nombreArchivo);
+    }
 
 }
