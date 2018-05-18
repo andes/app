9c06ca74a9fd0ebf896883184c607b9101d33d40
Lautaro Molina <negro89_nqn@hotmail.com>
2018-05-18 10:21:26 -0300

    rena

--- src/app/components/paciente/paciente-detalle.ts
+++ src/app/components/paciente/paciente-detalle.ts
@@ -21,25 +21,50 @@ export class PacienteDetalleComponent {
      * @memberof PacienteDetalleComponent
      */
     @Input() paciente: IPaciente;
-    renaperFoto;
-    // datos
+    loading = false;
+    deshabilitar = false;
+    inconsistenciaDatos = false;
+    pacientesSimilares = [];
 
     constructor(private pacienteService: PacienteService, private renaperService: RenaperService) { }
 
     renaperVerification(patient) {
         // TODO llamar al servicio de renaper y actualizar: Datos básicos y Foto
         // En caso que el paciente ya esté validado sólo traer la foto!
+        // Cancela la búsqueda anterior
+
+        this.deshabilitar = true;
+        window.document.getElementById("detalleContenedor").style.opacity = '0.3';
+        this.loading = true;
         let sexoRena = (patient.sexo === 'masculino') ? 'M' : 'F';
+
+
         this.renaperService.get({ documento: patient.documento, sexo: sexoRena }).subscribe(resultado => {
+            this.loading = false;
             if (resultado) {
                 let codigo = resultado.codigo;
                 let datos = resultado.datos;
-                this.paciente.foto = resultado.datos.foto;
+                console.log(datos);
+
+                // si esta validado agregamos foto y chequeamos domicilio.
+                if (patient.estado === 'validado') {
+                    patient.foto = resultado.datos.foto;
+                    // (patient.direccion['valor'] == "") ? patient.direccion['valor'] = datos.calle + datos.numero : '';
+                } else {
+                    this.paciente.nombre = resultado.datos.nombres;
+                    this.paciente.apellido = resultado.datos.apellido;
+                    this.paciente.fechaNacimiento = moment(resultado.datos.fechaNacimiento, 'DD/MM/YYYY');
+                    this.paciente.direccion[0]['valor'] = datos.calle + ' ' + datos.numero;
+                    this.paciente.direccion[0]['codigoPostal'] = datos.cpostal;
+                }
+
             } else {
                 // TODO ver el tema de mostrar algún error si no trae nada
                 console.log('entro por algun problema');
             }
 
+            window.document.getElementById("detalleContenedor").style.opacity = '1';
+            console.log('paciente: ', this.paciente);
         });
     }
 }
