9241f5b33406ad9dd88305ceae7224b118a18b1b
matiurrutia <urrutia.mati@gmail.com>
2018-05-29 05:11:29 -0300

    Merge pull request #505 from andes/percentilo-perimetroCefalico-cefalico
    percentilo perimetro cefalico y perimetro cefalico

---
+++ src/app/modules/rup/components/elementos/perimetroCefalico.component.ts
@@ -0,0 +1,22 @@
+import { Component, Output, Input, EventEmitter, OnInit } from '@angular/core';
+import { RUPComponent } from './../core/rup.component';
+import * as moment from 'moment';
+@Component({
+    selector: 'rup-perimetroCefalico',
+    templateUrl: 'perimetroCefalico.html'
+})
+export class PerimetroCefalicoComponent extends RUPComponent implements OnInit {
+    ngOnInit() {
+        if (!this.soloValores) {
+            this.conceptObserverService.observe(this.registro).subscribe((data) => {
+                if (this.registro.valor !== data.valor) {
+                    this.registro.valor = data.valor;
+                    this.emitChange(false);
+                }
+            });
+        }
+        if (this.registro.valor) {
+            this.mensaje = this.getMensajes();
+        }
+    }
+}
