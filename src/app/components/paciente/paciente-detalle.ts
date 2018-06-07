6ee62219316d06d39d87c281089815f80b09defc
Lautaro Molina <37875706+negro89@users.noreply.github.com>
2018-06-07 15:44:40 -0300

    nueva funcion "deshacer validación por renaper" (#533)
    * deshacer validacion por renaper
    
    * linteo

--- src/app/components/paciente/paciente-detalle.ts
+++ src/app/components/paciente/paciente-detalle.ts
@@ -8,6 +8,7 @@ import {
 } from './../../services/fuentesAutenticas/servicioRenaper.service';
 import { MAT_DATEPICKER_SCROLL_STRATEGY_PROVIDER_FACTORY } from '@angular/material';
 import { Plex } from '@andes/plex';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 
 @Component({
@@ -15,7 +16,7 @@ import { Plex } from '@andes/plex';
     templateUrl: 'paciente-detalle.html',
     styleUrls: ['paciente-detalle.scss']
 })
-export class PacienteDetalleComponent {
+export class PacienteDetalleComponent implements OnInit {
     /**
      * Recibe un paciente por parámetro
      *
@@ -34,10 +35,23 @@ export class PacienteDetalleComponent {
 
     _paciente: IPaciente;
     loading = false;
-    deshabilitar = false;
+    deshabilitarValidar = false;
     inconsistenciaDatos = false;
+    backUpDatos = [];
 
     constructor(private pacienteService: PacienteService, private renaperService: RenaperService, private plex: Plex) { }
+
+    ngOnInit() {
+        this.backUpDatos['nombre'] = this.paciente.nombre;
+        this.backUpDatos['apellido'] = this.paciente.apellido;
+        this.backUpDatos['estado'] = this.paciente.estado;
+        this.backUpDatos['genero'] = this.paciente.genero;
+        this.backUpDatos['fechaNacimiento'] = this.paciente.fechaNacimiento;
+        this.backUpDatos['foto'] = this.paciente.foto;
+        this.backUpDatos['cuil'] = this.paciente.cuil;
+        this.backUpDatos['direccion'] = this.paciente.direccion[0].valor;
+        this.backUpDatos['codigoPostal'] = this.paciente.direccion[0].codigoPostal;
+    }
 
     renaperVerification(patient) {
 
@@ -59,8 +73,8 @@ export class PacienteDetalleComponent {
 
         this.renaperService.get({ documento: documentoRena, sexo: sexoRena }).subscribe(resultado => {
             // Queda pendiente actualizar la localidad y provincia de renaper en caso que no la carguen
+            this.deshabilitarValidar = true;
             this.loading = false;
-            this.deshabilitar = true;
             let codigo = resultado.codigo;
             let datos = resultado.datos;
             if (resultado.datos.nroError === 0) {
@@ -84,11 +98,30 @@ export class PacienteDetalleComponent {
                 this.renaperNotification.emit(true);
             } else {
                 // TODO ver el tema de mostrar algún error si no trae nada
-                this.plex.toast('danger', resultado.datos.descripcionError + ' REVISAR LOS DATOS INGRESADOS');
-                this.deshabilitar = false;
+                this.plex.toast('danger', resultado.datos.descripcionError + ', REVISAR LOS DATOS INGRESADOS');
+                this.deshabilitarValidar = false;
             }
 
             window.document.getElementById('detalleContenedor').classList.remove('loadMode');
         });
+    }
+
+    setDatosOriginales(patient) {
+
+        patient.foto = this.backUpDatos['foto'];
+        this.paciente.direccion[0].valor = this.backUpDatos['direccion'];
+        this.paciente.direccion[0].codigoPostal = this.backUpDatos['codigoPostal'];
+
+        if (this.backUpDatos['estado'] === 'temporal') {
+            patient.nombre = this.backUpDatos['nombre'];
+            patient.apellido = this.backUpDatos['apellido'];
+            patient.fechaNacimiento = this.backUpDatos['fechaNacimiento'];
+            patient.cuil = this.backUpDatos['cuil'];
+            patient.estado = this.backUpDatos['estado'];
+            patient.genero = this.backUpDatos['genero'];
+            this.renaperNotification.emit(false);   // desbloquea los campos requeridos
+        }
+
+        this.deshabilitarValidar = false;
     }
 }
