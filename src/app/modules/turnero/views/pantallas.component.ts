3f28b3da391720de6a8e2019967ea57f7acc4599
Mariano Botta <marianoabotta@gmail.com>
2018-07-13 12:24:16 -0300

    turnero websocket

--- src/app/modules/turnero/views/pantallas.component.ts
+++ src/app/modules/turnero/views/pantallas.component.ts
@@ -26,34 +26,32 @@ export class PantallasComponent implements OnInit, OnDestroy  {
     ) {}
 
     ngOnInit () {
-        let temp, id;
+        let temp;
         this.ws.join('turnero');
         this.sub = this.ws.events.subscribe(( {event, data} ) => {
+            let { pantalla } = data;
             switch (event) {
                 case 'turnero-activated':
-                    id = data.id;
-                    temp = this.pantallas.find(item => item.id === id);
+                    temp = this.pantallas.find(item => item.id === pantalla.id);
                     if (temp) {
                         temp.token = null;
                         temp.expirationTime = null;
                     }
                     break;
                 case 'turnero-update':
-                        id = data.pantalla.id;
-                        temp = this.pantallas.find(item => item.id === id);
+                        temp = this.pantallas.find(item => item.id === pantalla.id);
                         if (temp) {
                             Object.assign(temp, data.pantalla);
                         }
                     break;
                 case 'turnero-create':
-                    const { pantalla } = data;
                     let i = this.pantallas.findIndex(item => item.id === pantalla.id);
                     if (i < 0) {
                         this.pantallas.push(pantalla);
                     }
                     break;
                 case 'turnero-remove':
-                    let index = this.pantallas.findIndex(item => item.id === data.id);
+                    let index = this.pantallas.findIndex(item => item.id === pantalla.id);
                     if (index >= 0) {
                         this.pantallas.splice(index, 1);
                         this.pantallasService.pantallas = [...this.pantallasService.pantallas];
