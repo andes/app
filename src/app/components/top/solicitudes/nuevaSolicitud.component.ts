9adcedd1b7ca21bcaeaa97b4939479818013277d
ndinolfo <nicodinolfo@gmail.com>
2018-07-25 14:26:56 -0300

    avances mod solicitud

--- src/app/components/top/solicitudes/nuevaSolicitud.component.ts
+++ src/app/components/top/solicitudes/nuevaSolicitud.component.ts
@@ -7,6 +7,7 @@ import { ProfesionalService } from '../../../services/profesional.service';
 import { Auth } from '@andes/auth';
 import { PrestacionesService } from '../../../modules/rup/services/prestaciones.service';
 import { TurnoService } from '../../../services/turnos/turno.service';
+import { ISolicitud } from '../../../modules/rup/interfaces/solicitud.interface';
 
 @Component({
     selector: 'nueva-solicitud',
@@ -14,36 +15,37 @@ import { TurnoService } from '../../../services/turnos/turno.service';
 })
 export class NuevaSolicitudComponent {
     showSeleccionarPaciente = true;
+    permisos = this.auth.getPermissions('turnos:darTurnos:prestacion:?');
     paciente: any;
-    profesionalDestino: any;
-    organizacionDestino: any;
-    registros: any = {
-        solicitudPrestacion: {
-            profesionales: [],
-            motivo: '',
-            autocitado: false
-        }
-    };
+    autocitado: boolean;
+    motivo: '';
+    fecha: any;
+
     modelo: any = {
-        paciente: {},
-        profesional: {},
-        organizacion: {},
+        paciente: {
+            id: '',
+            nombre: '',
+            apellido: '',
+            documento: '',
+            sexo: '',
+            fechaNacimiento: null
+        },
         solicitud: {
+            organizacion: null,
+            organizacionOrigen: null,
+            profesional: null,
+            profesionalOrigen: null,
             fecha: null,
-            paciente: {},
-            organizacion: {},
-            profesional: {},
-            organizacionOrigen: this.auth.organizacion,
-            profesionalOrigen: {},
-            turno: null
+            turno: null,
+            tipoPrestacion: null,
+            tipoPrestacionOrigen: null,
+            prestacionOrigen: null,
+            registros: []
         },
         estados: [
             { tipo: 'pendiente' }
         ]
     };
-
-    permisos = this.auth.getPermissions('turnos:darTurnos:prestacion:?');
-
 
     @Output() newSolicitudEmitter: EventEmitter<any> = new EventEmitter<any>();
     constructor(
@@ -59,10 +61,8 @@ export class NuevaSolicitudComponent {
 
 
     seleccionarPaciente(paciente: any): void {
-        if (paciente.id) {
-            this.paciente = paciente;
-            this.showSeleccionarPaciente = false;
-        }
+        this.paciente = paciente;
+        this.showSeleccionarPaciente = false;
     }
 
     loadOrganizacion(event) {
@@ -102,22 +102,29 @@ export class NuevaSolicitudComponent {
     guardarSolicitud($event) {
 
         if ($event.formValid) {
-
-            this.modelo.solicitud.registros = {
+            if (this.autocitado) {
+                this.modelo.solicitud.profesional = this.modelo.solicitud.profesionalOrigen;
+            }
+            this.modelo.solicitud.organizacionOrigen = this.auth.organizacion;
+            this.modelo.solicitud.registros.push({
                 nombre: this.modelo.solicitud.tipoPrestacion.term,
                 concepto: this.modelo.solicitud.tipoPrestacion,
                 valor: {
-                    solicitudPrestacion: this.registros.solicitudPrestacion
+                    solicitudPrestacion: {
+                        motivo: this.motivo,
+                        autocitado: this.autocitado
+                    }
                 },
                 tipo: 'solicitud'
+            });
+            this.modelo.paciente = {
+                id: this.paciente.id,
+                nombre: this.paciente.nombre,
+                apellido: this.paciente.apellido,
+                documento: this.paciente.documento,
+                sexo: this.paciente.sexo,
+                fechaNacimiento: this.paciente.fechaNacimiento
             };
-
-            this.modelo.paciente = this.paciente;
-            this.modelo.solicitud.organizacion = this.organizacionDestino;
-            this.modelo.solicitud.profesional = this.profesionalDestino ? this.profesionalDestino : { id: this.auth.profesional.id, nombre: this.auth.usuario.nombre, apellido: this.auth.usuario.apellido };
-            this.modelo.solicitud.organizacionOrigen = this.auth.organizacion;
-            this.modelo.solicitud.profesionalOrigen = { id: this.auth.profesional.id, nombre: this.auth.usuario.nombre, apellido: this.auth.usuario.apellido };
-
             // Se guarda la solicitud 'pendiente' de prestación
             this.servicioPrestacion.post(this.modelo).subscribe(respuesta => {
                 this.newSolicitudEmitter.emit();
@@ -125,30 +132,30 @@ export class NuevaSolicitudComponent {
                 // this.showCargarSolicitud = false;
                 // this.showBotonCargarSolicitud = true;
 
-                this.modelo = {
-                    paciente: this.paciente,
-                    profesional: {},
-                    organizacion: {},
-                    solicitud: {
-                        fecha: null,
-                        paciente: {},
-                        organizacion: {},
-                        organizacionOrigen: this.auth.organizacion,
-                        profesional: {},
-                        profesionalOrigen: {},
-                        turno: null
-                    },
-                    estados: [
-                        { tipo: 'pendiente' }
-                    ]
-                };
-                this.registros = {
-                    solicitudPrestacion: {
-                        profesionales: [],
-                        motivo: '',
-                        autocitado: false,
-                    }
-                };
+                // this.modelo = {
+                //     paciente: this.paciente,
+                //     profesional: {},
+                //     organizacion: {},
+                //     solicitud: {
+                //         fecha: null,
+                //         paciente: {},
+                //         organizacion: {},
+                //         organizacionOrigen: this.auth.organizacion,
+                //         profesional: {},
+                //         profesionalOrigen: {},
+                //         turno: null
+                //     },
+                //     estados: [
+                //         { tipo: 'pendiente' }
+                //     ]
+                // };
+                // this.registros = {
+                //     solicitudPrestacion: {
+                //         profesionales: [],
+                //         motivo: '',
+                //         autocitado: false,
+                //     }
+                // };
 
             });
 
@@ -158,30 +165,30 @@ export class NuevaSolicitudComponent {
     }
 
     cancelar() {
-        this.modelo = {
-            paciente: this.paciente,
-            profesional: {},
-            organizacion: this.auth.organizacion,
-            solicitud: {
-                fecha: null,
-                paciente: {},
-                organizacion: {},
-                organizacionOrigen: this.auth.organizacion,
-                profesional: {},
-                profesionalOrigen: {},
-                turno: null
-            },
-            estados: [
-                { tipo: 'pendiente' }
-            ]
-        };
-        this.registros = {
-            solicitudPrestacion: {
-                profesionales: [],
-                motivo: '',
-                autocitado: false
-            }
-        };
+        // this.modelo = {
+        //     paciente: this.paciente,
+        //     profesional: {},
+        //     organizacion: this.auth.organizacion,
+        //     solicitud: {
+        //         fecha: null,
+        //         paciente: {},
+        //         organizacion: {},
+        //         organizacionOrigen: this.auth.organizacion,
+        //         profesional: {},
+        //         profesionalOrigen: {},
+        //         turno: null
+        //     },
+        //     estados: [
+        //         { tipo: 'pendiente' }
+        //     ]
+        // };
+        // this.registros = {
+        //     solicitudPrestacion: {
+        //         profesionales: [],
+        //         motivo: '',
+        //         autocitado: false
+        //     }
+        // };
         this.newSolicitudEmitter.emit();
     }
 }
