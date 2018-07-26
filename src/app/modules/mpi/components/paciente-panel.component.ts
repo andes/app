e579cbf61ebbecb2ce2c1bd26e29f0fad66d84b5
jfgabriel <jgabriel@neuquen.gov.ar>
2018-07-25 22:19:37 -0300

    panel-paciente deshabilté cobertura social

--- src/app/modules/mpi/components/paciente-panel.component.ts
+++ src/app/modules/mpi/components/paciente-panel.component.ts
@@ -25,30 +25,31 @@ export class PacientePanelComponent {
     }
     set paciente(value: IPaciente) {
         this._paciente = value;
-        this.actualizarCoberturaSocial();
+        // this.actualizarCoberturaSocial();
     }
 
     constructor(private obraSocialService: ObraSocialService, private profeService: ProfeService) { }
 
-    actualizarCoberturaSocial() {
-        if (this.request) {
-            this.request.unsubscribe();
-        }
-        this.coberturaSocial = null;
-        this.coberturaSocialLoading = true;
-        this.coberturaSocialError = false;
+    // @jfgabriel | Deshabilitado momentáneamente hasta que se integren los nuevos servicios Obras Sociales
+    // actualizarCoberturaSocial() {
+    //     if (this.request) {
+    //         this.request.unsubscribe();
+    //     }
+    //     this.coberturaSocial = null;
+    //     this.coberturaSocialLoading = true;
+    //     this.coberturaSocialError = false;
 
-        // Llama a la API
-        if (this.paciente && this.paciente.documento) {
-            this.request = Observable.forkJoin([
-                this.obraSocialService.get({ dni: this.paciente.documento }, false),
-                this.profeService.get({ dni: this.paciente.documento }, false)]).subscribe(t => {
-                    this.coberturaSocialLoading = false;
-                    this.coberturaSocial = 'FAKE FAKE FAKE';
-                }, (err) => {
-                    this.coberturaSocialError = true;
-                    this.coberturaSocialLoading = false;
-                });
-        }
-    }
+    //     // Llama a la API
+    //     if (this.paciente && this.paciente.documento) {
+    //         this.request = Observable.forkJoin([
+    //             this.obraSocialService.get({ dni: this.paciente.documento }, false),
+    //             this.profeService.get({ dni: this.paciente.documento }, false)]).subscribe(t => {
+    //                 this.coberturaSocialLoading = false;
+    //                 this.coberturaSocial = 'FAKE FAKE FAKE';
+    //             }, (err) => {
+    //                 this.coberturaSocialError = true;
+    //                 this.coberturaSocialLoading = false;
+    //             });
+    //     }
+    // }
 }
