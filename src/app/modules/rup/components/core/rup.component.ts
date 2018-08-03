b25051c4d4d3ae889ab89439b3f6ee88eceaf2bd
Andrés <andresin@gmail.com>
2018-08-03 08:38:22 -0300

    método para navegación intracomponente (scroll)

--- src/app/modules/rup/components/core/rup.component.ts
+++ src/app/modules/rup/components/core/rup.component.ts
@@ -152,4 +152,20 @@ export class RUPComponent implements OnInit {
     * @memberof RUPComponent
     */
     public getMensajes() { }
+
+
+    jumpToId(fragment) {
+
+        // Use the browser to navigate
+        window.location.hash = fragment;
+
+        // But also scroll when routing / deep-linking to dynamic page
+        // or re-clicking same anchor
+        if (fragment) {
+            const element = document.querySelector('[name="' + fragment + '"]');
+            if (element) {
+                element.scrollIntoView();
+            }
+        }
+    }
 }
