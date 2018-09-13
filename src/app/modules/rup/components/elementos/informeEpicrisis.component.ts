672011d7af1257b5f6db73a2a6a7df608c18e6ee
fersastre <fersastre23@gmail.com>
2018-09-13 09:59:57 -0300

    agrego accordions a la epicrisis mas el select de organizaciones y el boton de desplegar todos o colapsar todos

--- src/app/modules/rup/components/elementos/informeEpicrisis.component.ts
+++ src/app/modules/rup/components/elementos/informeEpicrisis.component.ts
@@ -3,15 +3,35 @@ import { RUPComponent } from './../core/rup.component';
 
 @Component({
     selector: 'rup-InformeEpicrisisComponent',
-    templateUrl: '../core/rup.html'
+    templateUrl: 'informeEpicrisis.html'
 })
 
 export class InformeEpicrisisComponent extends RUPComponent implements OnInit {
 
+    public accordionActive = 0;
+    public unidadesOrganizativas = [];
+    public desplegarTodo = false;
+    public mensajeAccionAccordion = 'Desplegar';
 
     ngOnInit() {
+        if (!this.registro.valor) {
+            this.registro.valor = {
+                unidadOrganizativa: null
+            };
+        }
 
+        this.servicioOrganizacion.getById(this.auth.organizacion.id).subscribe(organizacion => {
+            this.unidadesOrganizativas = organizacion.unidadesOrganizativas;
+        });
     }
 
+    accordionSeleccionado(i) {
+        this.accordionActive = i;
+    }
 
+    desplegarAccordions() {
+        this.desplegarTodo = !this.desplegarTodo;
+        this.mensajeAccionAccordion = this.desplegarTodo ? 'Colapsar' : 'Desplegar';
+        this.accordionActive = -1;
+    }
 }
