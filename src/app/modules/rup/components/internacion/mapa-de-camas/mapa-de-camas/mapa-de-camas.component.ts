e0fe0913a0862ef7a926d41b6cbb5f066480151a
matiurrutia <urrutia.mati@gmail.com>
2018-09-18 08:05:37 -0300

    mostramos internacion actual, en mapa de camas se crea o edita epicrisis

--- src/app/modules/rup/components/internacion/mapa-de-camas/mapa-de-camas/mapa-de-camas.component.ts
+++ src/app/modules/rup/components/internacion/mapa-de-camas/mapa-de-camas/mapa-de-camas.component.ts
@@ -320,15 +320,17 @@ export class MapaDeCamasComponent implements OnInit {
     }
 
     countFiltros() {
-        this.cantidadXEstado = {
-            ocupada: this.camas.filter(c => c.ultimoEstado.estado === 'ocupada'),
-            desocupada: this.camas.filter(c => c.ultimoEstado.estado === 'desocupada'),
-            reparacion: this.camas.filter(c => c.ultimoEstado.estado === 'reparacion'),
-            bloqueada: this.camas.filter(c => c.ultimoEstado.estado === 'bloqueada'),
-            oxigeno: this.camas.filter(c => c.equipamiento.find(e => e.conceptId === '261746005')),
-            disponible: this.camas.filter(c => c.ultimoEstado.estado === 'disponible')
-        };
-        this.loadCountFiltros = true;
+        if (this.camas) {
+            this.cantidadXEstado = {
+                ocupada: this.camas.filter(c => c.ultimoEstado.estado === 'ocupada'),
+                desocupada: this.camas.filter(c => c.ultimoEstado.estado === 'desocupada'),
+                reparacion: this.camas.filter(c => c.ultimoEstado.estado === 'reparacion'),
+                bloqueada: this.camas.filter(c => c.ultimoEstado.estado === 'bloqueada'),
+                oxigeno: this.camas.filter(c => c.equipamiento.find(e => e.conceptId === '261746005')),
+                disponible: this.camas.filter(c => c.ultimoEstado.estado === 'disponible')
+            };
+            this.loadCountFiltros = true;
+        }
     }
 
     // selecionarCama(cama) {
@@ -448,19 +450,17 @@ export class MapaDeCamasComponent implements OnInit {
        * Nos rutea a la ejecucion de RUP.
        */
     generaEpicrisis() {
-        let epicrisisEjecucion;
         this.servicioPrestacion.get({ idPrestacionOrigen: this.prestacionPorInternacion.id }).subscribe(prestacionExiste => {
-            epicrisisEjecucion = prestacionExiste;
+            if (prestacionExiste.length === 0) {
+                let nuevaPrestacion = this.servicioPrestacion.inicializarPrestacion(this.prestacionPorInternacion.paciente, this.epicrisis, 'ejecucion', 'internacion');
+                nuevaPrestacion.solicitud.prestacionOrigen = this.prestacionPorInternacion.id;
+                this.servicioPrestacion.post(nuevaPrestacion).subscribe(prestacion => {
+                    this.router.navigate(['rup/ejecucion', prestacion.id]);
+                });
+            } else {
+                this.router.navigate(['rup/ejecucion', prestacionExiste[0].id]);
+            }
         });
-        if (!epicrisisEjecucion) {
-            let nuevaPrestacion = this.servicioPrestacion.inicializarPrestacion(this.prestacionPorInternacion.paciente, this.epicrisis, 'ejecucion', 'internacion');
-            nuevaPrestacion.solicitud.prestacionOrigen = this.prestacionPorInternacion.id;
-            this.servicioPrestacion.post(nuevaPrestacion).subscribe(prestacion => {
-                this.router.navigate(['rup/ejecucion', prestacion.id]);
-            });
-        } else {
-            this.router.navigate(['rup/ejecucion', epicrisisEjecucion[0].id]);
-        }
     }
 
 
