83ea6cc321f5dfe876f1728ee822f4e0200ad212
Andrés <andresin@gmail.com>
2018-10-29 10:52:01 -0300

    componente ColonoscopiaControlComponent

---
+++ src/app/modules/rup/components/elementos/colonoscopiaControl.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { RUPComponent } from '../core/rup.component';
+
+@Component({
+    selector: 'rup-colonoscopia-control',
+    // templateUrl: 'colonoscopia.html'
+    templateUrl: '../core/rup.html'
+})
+
+export class ColonoscopiaControlComponent extends RUPComponent implements OnInit {
+
+    ngOnInit() {
+
+    }
+}
