37d48fa64ecfbf6d048fd5380c22bf5e2d9b143d
Lautaro Molina <negro89_nqn@hotmail.com>
2018-10-31 15:52:36 -0300

    Se corrige comparacion de sexo de un paciente.

--- src/app/components/paciente/paciente-detalle.ts
+++ src/app/components/paciente/paciente-detalle.ts
@@ -62,56 +62,58 @@ export class PacienteDetalleComponent implements OnInit {
     }
 
     renaperVerification(patient) {
-        // TODO llamar al servicio de renaper y actualizar: Datos básicos y Foto
-        // En caso que el paciente ya esté validado sólo traer la foto!
+        debugger;
+        if (!patient.documento || (patient.sexo.id === 'otro')) {
+            this.plex.toast('danger', 'La validación por RENAPER requiere sexo MASCULINO o FEMENINO', 'Error', 3000);
+        } else {
+            this.loading = true;
+            let sexoRena = null;
+            let documentoRena = null;
 
-        this.loading = true;
-        let sexoRena = null;
-        let documentoRena = null;
+            patient.sexo = ((typeof patient.sexo === 'string')) ? patient.sexo : (Object(patient.sexo).id);
+            sexoRena = patient.sexo === 'masculino' ? 'M' : 'F';
+            documentoRena = patient.documento;
 
-        patient.sexo = ((typeof patient.sexo === 'string')) ? patient.sexo : (Object(patient.sexo).id);
-        sexoRena = patient.sexo === 'masculino' ? 'M' : 'F';
-        documentoRena = patient.documento;
-
-        this.renaperService.get({ documento: documentoRena, sexo: sexoRena }).subscribe(
-            resultado => {
-                // Queda pendiente actualizar la localidad y provincia de renaper en caso que no la carguen
-                this.deshabilitarValidar = true;
-                let datos = resultado.datos;
-                if (resultado.datos.nroError === 0) {
-                    if (patient.estado === 'temporal') {
-                        patient.fechaNacimiento = moment(datos.fechaNacimiento, 'YYYY-MM-DD');
-                        // si nombre y apellido contienen solo caracteres válidos ..
-                        if (this.nombrePattern.test(datos.nombres) && this.nombrePattern.test(datos.apellido)) {
-                            patient.nombre = datos.nombres;
-                            patient.apellido = datos.apellido;
-                            patient.estado = 'validado';
-                            this.renaperNotification.emit(true);
-                            this.loading = false;
-                        } else {
-                            this.plex.info('danger', '', 'Intento de validación fallida. Se volverá a intentar.', 5000);
-                            this.getSisa(patient);
-                        }
-                        this.paciente.direccion[0].valor = datos.calle + ' ' + datos.numero;
-                        this.paciente.direccion[0].codigoPostal = datos.cpostal;
-                        patient.cuil = datos.cuil;
-                    } else {
-                        if (!this.paciente.direccion[0].valor) {
+            this.renaperService.get({ documento: documentoRena, sexo: sexoRena }).subscribe(
+                resultado => {
+                    // Queda pendiente actualizar la localidad y provincia de renaper en caso que no la carguen
+                    this.deshabilitarValidar = true;
+                    let datos = resultado.datos;
+                    if (resultado.datos.nroError === 0) {
+                        if (patient.estado === 'temporal') {
+                            patient.fechaNacimiento = moment(datos.fechaNacimiento, 'YYYY-MM-DD');
+                            // si nombre y apellido contienen solo caracteres válidos ..
+                            if (this.nombrePattern.test(datos.nombres) && this.nombrePattern.test(datos.apellido)) {
+                                patient.nombre = datos.nombres;
+                                patient.apellido = datos.apellido;
+                                patient.estado = 'validado';
+                                this.renaperNotification.emit(true);
+                                this.loading = false;
+                            } else {
+                                this.plex.info('danger', '', 'Intento de validación fallida. Se volverá a intentar.', 5000);
+                                this.getSisa(patient);
+                            }
                             this.paciente.direccion[0].valor = datos.calle + ' ' + datos.numero;
+                            this.paciente.direccion[0].codigoPostal = datos.cpostal;
+                            patient.cuil = datos.cuil;
+                        } else {
+                            if (!this.paciente.direccion[0].valor) {
+                                this.paciente.direccion[0].valor = datos.calle + ' ' + datos.numero;
+                            }
+                            if (!this.paciente.cuil) {
+                                this.paciente.cuil = datos.cuil;
+                            }
+                            this.loading = false;
                         }
-                        if (!this.paciente.cuil) {
-                            this.paciente.cuil = datos.cuil;
-                        }
-                        this.loading = false;
+                        patient.foto = resultado.datos.foto;
+                    } else {
+                        // TODO ver el tema de mostrar algún error si no trae nada
+                        // this.plex.toast('danger', resultado.datos.descripcionError + ', REVISAR LOS DATOS INGRESADOS');
+                        this.deshabilitarValidar = false;
+                        this.getSisa(patient);
                     }
-                    patient.foto = resultado.datos.foto;
-                } else {
-                    // TODO ver el tema de mostrar algún error si no trae nada
-                    // this.plex.toast('danger', resultado.datos.descripcionError + ', REVISAR LOS DATOS INGRESADOS');
-                    this.deshabilitarValidar = false;
-                    this.getSisa(patient);
-                }
-            });
+                });
+        }
     }
 
     private getSisa(paciente: any) {
