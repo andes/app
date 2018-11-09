62c3e8c7619a6cce750d7d811b5cbfe8c0fa3c72
fersastre <fersastre23@gmail.com>
2018-11-09 09:58:12 -0300

    campo Otro en seleccion binaria.

--- src/app/modules/rup/components/elementos/seleccionBinaria.component.ts
+++ src/app/modules/rup/components/elementos/seleccionBinaria.component.ts
@@ -9,12 +9,20 @@ import { RUPComponent } from '../core/rup.component';
 export class SeleccionBinariaComponent extends RUPComponent implements OnInit {
 
     public options;
+    public valor;
 
     ngOnInit() {
+        this.options = this.params.defaultOptions;
         if (!this.registro.valor) {
             this.registro.valor = null;
+        } else {
+            this.valor = this.registro.valor.id;
         }
-        this.options = this.params.defaultOptions;
+    }
+
+    onChange() {
+        this.registro.valor = this.options.find(i => i.id === this.valor);
+        this.emitChange();
     }
 
 }
