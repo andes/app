d8985e1c379d4da23b4564cd029e133d83787111
ndinolfo <nicodinolfo@gmail.com>
2018-11-12 14:02:29 -0300

    mejora pipe fecha

--- src/app/shared/pipes/fecha.pipe.ts
+++ src/app/shared/pipes/fecha.pipe.ts
@@ -9,7 +9,8 @@ import { Pipe, PipeTransform } from '@angular/core';
  */
 @Pipe({ name: 'fecha' })
 export class FechaPipe implements PipeTransform {
-    transform(value: any): any {
-        return moment(value).format('DD/MM/YYYY');
+    transform(value: any, args: string[]): any {
+
+        return (args[0] === 'utc') ? moment(value).utc().format('DD/MM/YYYY') : moment(value).format('DD/MM/YYYY');
     }
 }
