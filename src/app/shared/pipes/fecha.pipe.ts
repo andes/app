df7e0a73268b9235a822fc638e5bb72a120a859d
ndinolfo <nicodinolfo@gmail.com>
2018-11-13 14:04:31 -0300

    agrega parámetro al pipe fecha para parsear el date como UTC

--- src/app/shared/pipes/fecha.pipe.ts
+++ src/app/shared/pipes/fecha.pipe.ts
@@ -9,8 +9,11 @@ import { Pipe, PipeTransform } from '@angular/core';
  */
 @Pipe({ name: 'fecha' })
 export class FechaPipe implements PipeTransform {
-    transform(value: any, args: string[]): any {
-
-        return (args[0] === 'utc') ? moment(value).utc().format('DD/MM/YYYY') : moment(value).format('DD/MM/YYYY');
+    transform(value: any, arg1: string): any {
+        if (arg1 && arg1 === 'utc') {
+            return moment(value).utc().format('DD/MM/YYYY');
+        } else {
+            return moment(value).format('DD/MM/YYYY');
+        }
     }
 }
