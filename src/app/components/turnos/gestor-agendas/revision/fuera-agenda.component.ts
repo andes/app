dce1d52caf1fcafb14b5ac9bbd12a1675172e351
Carolina <celeste.carolina.s@gmail.com>
2018-11-22 14:40:15 -0300

    se agrega reparo en componente fuera-agenda

--- src/app/components/turnos/gestor-agendas/revision/fuera-agenda.component.ts
+++ src/app/components/turnos/gestor-agendas/revision/fuera-agenda.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostBinding, Output, EventEmitter } from '@angular/core';
 import { Plex, SelectEvent } from '@andes/plex';
 import { ICodificacionPrestacion } from './../../../../modules/rup/interfaces/ICodificacion';
-
+import { CodificacionService } from './../../../../modules/rup/services/codificacion.service';
 
 
 @Component({
@@ -10,117 +10,89 @@ import { ICodificacionPrestacion } from './../../../../modules/rup/interfaces/IC
 })
 export class RevisionFueraAgendaComponent implements OnInit {
     // Propiedades privadas
+    @HostBinding('class.plex-layout') layout = true;
 
     // Propiedades públicas
     public prestaciones: ICodificacionPrestacion[];
     public prestacionSeleccionada: ICodificacionPrestacion;
+    public showRegistros;
+    public showReparo = false;
+    public indiceReparo: number;
+    public esAgendaOdonto = false;
+    public diagnosticos = [];
+
     // Eventos
     @Output() save: EventEmitter<ICodificacionPrestacion[]> = new EventEmitter<ICodificacionPrestacion[]>();
+    @Output() volverAlGestor = new EventEmitter<boolean>();
 
     // Constructor
-    constructor(private plex: Plex) { }
+    constructor(private plex: Plex, private serviceCodificacion: CodificacionService) { }
 
     // Métodos
     ngOnInit() {
-        this.prestaciones = [{
-            id: '5bf55f13ca9bf5331aa2c34d',
-            idPrestacion: '5bf55ef0ca9bf5331aa2c334',
-            paciente: {
-                id: '58da55ed0bb1a96b254dda54',
-                nombre: 'JUAN FRANCISCO',
-                apellido: 'GABRIEL',
-                documento: '26108063',
-                sexo: 'masculino',
-                fechaNacimiento: new Date('1977-10-02T00:00:00.000-03:00')
-            },
-            diagnostico: {
-                codificaciones: [{
-                    'codificacionProfesional': {
-                        'snomed': {
-                            'refsetIds': [
-                                '900000000000497000'
-                            ],
-                            'conceptId': '58214004',
-                            'term': 'esquizofrenia',
-                            'fsn': 'esquizofrenia (trastorno)',
-                            'semanticTag': 'trastorno'
-                        },
-                        'cie10': {
-                            'causa': 'F20',
-                            'subcausa': '9',
-                            'codigo': 'F20.9',
-                            'nombre': 'Esquizofrenia, no especificada',
-                            'sinonimo': 'Esquizofrenia',
-                            'c2': false
-                        },
-                    },
-                    primeraVez: false
-                }]
+        this.serviceCodificacion.get({}).subscribe(datos => {
+            this.prestaciones = datos;
+        }, err => {
+            if (err) {
             }
-
-        },
-        {
-            id: '5bf58911da5b26600c0d90eb',
-            'diagnostico': {
-                'codificaciones': [
-                    {
-                        'codificacionProfesional': {
-                            'snomed': {
-                                'refsetIds': [
-                                    '900000000000497000'
-                                ],
-                                'conceptId': '58214004',
-                                'term': 'esquizofrenia',
-                                'fsn': 'esquizofrenia (trastorno)',
-                                'semanticTag': 'trastorno'
-                            },
-                            'cie10': {
-                                'causa': 'F20',
-                                'subcausa': '9',
-                                'codigo': 'F20.9',
-                                'nombre': 'Esquizofrenia, no especificada',
-                                'sinonimo': 'Esquizofrenia',
-                                'c2': false
-                            },
-                        },
-                        primeraVez: false
-                    }
-                ]
-            },
-            idPrestacion: '5bf5888eda5b26600c0d905b',
-            'paciente': {
-                'id': '586e6e8e27d3107fde1487f5',
-                'nombre': 'ELBA',
-                'apellido': 'ROA',
-                'documento': '12235825',
-                'sexo': 'femenino',
-                'fechaNacimiento': new Date('1956-01-15T03:00:00.000-03:00')
-            }
-        }];
+        });
     }
 
     estaSeleccionada(prestacion: ICodificacionPrestacion) {
-        // this.showRegistrosTurno = true;
-        // return (this.turnoSeleccionado === turno); //
+        return (this.prestacionSeleccionada === prestacion);
+        this.showRegistros = true;
     }
 
     seleccionarPrestacion(prestacion: ICodificacionPrestacion) {
         this.prestacionSeleccionada = prestacion;
+        this.diagnosticos = [];
+        this.showReparo = false;
+        if (prestacion.diagnostico.codificaciones && prestacion.diagnostico.codificaciones.length) {
+            this.diagnosticos = this.diagnosticos.concat(prestacion.diagnostico.codificaciones);
+        }
+
+    }
+
+    mostrarReparo(index) {
+        this.indiceReparo = index;
+        this.showReparo = true;
     }
 
     /**
-     * Guarda los datos del formulario y emite el dato guardado
+     * Agrega el diagnóstico provisto por el revisor, y persiste el cambio automáticamente
      *
-     * @param {any} $event formulario a validar
+     * @param {any} reparo
+     * @memberof RevisionAgendaComponent
      */
-    guardar($event) {
-        if ($event.formValid) {
-            // ...
-            this.plex.info('success', 'Los datos están correctos');
-            this.save.emit(this.prestaciones);
-        } else {
-            this.plex.info('warning', 'Completar datos requeridos');
+    repararDiagnostico(reparo: any) {
+        if (reparo) {
+            this.diagnosticos[this.indiceReparo].codificacionAuditoria = reparo;
+            this.showReparo = false;
         }
+        this.onSave();
+    }
+
+    aprobar(index) {
+        this.diagnosticos[index].codificacionAuditoria = this.diagnosticos[index].codificacionProfesional.cie10;
+        this.onSave();
+    }
+
+    borrarReparo(index) {
+        this.diagnosticos[index].codificacionAuditoria = null;
+        this.showReparo = false;
+        this.onSave();
+    }
+
+    onSave() {
+        // Se guarda la prestación seleccionada
+        if (this.diagnosticos) {
+            this.prestacionSeleccionada.diagnostico.codificaciones = this.diagnosticos;
+        }
+        this.serviceCodificacion.patch(this.prestacionSeleccionada.id, { codificaciones: this.diagnosticos }).subscribe();
+    }
+
+    volver() {
+        this.volverAlGestor.emit(true);
     }
 
 
