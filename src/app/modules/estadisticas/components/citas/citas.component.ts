51a3ea2f95e5a5268af53dfee2ee49028f9e379f
matiurrutia <urrutia.mati@gmail.com>
2019-01-30 15:34:19 -0300

    se agregan selects dinamicos

--- src/app/modules/estadisticas/components/citas/citas.component.ts
+++ src/app/modules/estadisticas/components/citas/citas.component.ts
@@ -30,6 +30,9 @@ export class CitasComponent implements AfterViewInit {
     public estadoLabels = [];
     public estadoData = [];
 
+    public estadoAgendaLabels = [];
+    public estadoAgendaData = [];
+
     public tipoTurnoLabels = [];
     public tipoTurnoData = [];
 
@@ -68,6 +71,8 @@ export class CitasComponent implements AfterViewInit {
         this.prestacionData = [];
         this.estadoLabels = [];
         this.estadoData = [];
+        this.estadoAgendaLabels = [];
+        this.estadoAgendaData = [];
         this.tipoTurnoLabels = [];
         this.tipoTurnoData = [];
         this.params = {
@@ -99,10 +104,15 @@ export class CitasComponent implements AfterViewInit {
 
         if (this.data.estado_turno) {
             this.data.estado_turno.forEach((item) => {
-                console.log(item._id);
                 this.estadoLabels.push(item._id);
-
                 this.estadoData.push(item.count);
+            });
+        }
+
+        if (this.data.estado_agenda) {
+            this.data.estado_agenda.forEach((item) => {
+                this.estadoAgendaLabels.push(item._id);
+                this.estadoAgendaData.push(item.total);
             });
         }
 
@@ -114,49 +124,59 @@ export class CitasComponent implements AfterViewInit {
         }
 
 
-        this.setFilters();
+        // this.setFilters();
 
     }
 
-    setFilters() {
-        if (this.data.prestacion) {
-            this.filtros['prestacion'] = [];
-            this.data.prestacion.forEach((item) => {
-                this.filtros['prestacion'].push({
-                    id: item._id,
-                    nombre: item.nombre
-                });
-            });
-        }
-        if (this.data.profesionales) {
-            this.filtros['profesional'] = [];
-            this.data.profesionales.forEach((item) => {
-                this.filtros['profesional'].push({
-                    id: item._id,
-                    nombre: item.apellido + ' ' + item.nombre
-                });
-            });
-        }
+    // setFilters() {
+    //     if (this.data.prestacion) {
+    //         this.filtros['prestacion'] = [];
+    //         this.data.prestacion.forEach((item) => {
+    //             this.filtros['prestacion'].push({
+    //                 id: item._id,
+    //                 nombre: item.nombre
+    //             });
+    //         });
+    //     }
+    //     if (this.data.profesionales) {
+    //         this.filtros['profesional'] = [];
+    //         this.data.profesionales.forEach((item) => {
+    //             this.filtros['profesional'].push({
+    //                 id: item._id,
+    //                 nombre: item.apellido + ' ' + item.nombre
+    //             });
+    //         });
+    //     }
 
-        if (this.data.estado_turno) {
-            this.filtros['estado_turno'] = [];
-            this.data.estado_turno.forEach((item) => {
-                this.filtros['estado_turno'].push({
-                    id: item._id,
-                    nombre: item._id
-                });
-            });
-        }
+    //     if (this.data.estado_turno) {
+    //         this.filtros['estado_turno'] = [];
+    //         this.data.estado_turno.forEach((item) => {
+    //             this.filtros['estado_turno'].push({
+    //                 id: item._id,
+    //                 nombre: item._id
+    //             });
+    //         });
+    //     }
 
-        if (this.data.tipoTurno) {
-            this.filtros['tipoTurno'] = [];
-            this.data.tipoTurno.forEach((item) => {
-                this.filtros['tipoTurno'].push({
-                    id: item._id.tipoTurno,
-                    nombre: item._id.tipoTurno
-                });
-            });
-        }
-    }
+    //     if (this.data.estado_agenda) {
+    //         this.filtros['estado_agenda'] = [];
+    //         this.data.estado_agenda.forEach((item) => {
+    //             this.filtros['estado_agenda'].push({
+    //                 id: item._id,
+    //                 nombre: item._id
+    //             });
+    //         });
+    //     }
+
+    //     if (this.data.tipoTurno) {
+    //         this.filtros['tipoTurno'] = [];
+    //         this.data.tipoTurno.forEach((item) => {
+    //             this.filtros['tipoTurno'].push({
+    //                 id: item._id.tipoTurno,
+    //                 nombre: item._id.tipoTurno
+    //             });
+    //         });
+    //     }
+    // }
 
 }
--- src/app/modules/estadisticas/components/citas/filtros.component.ts
+++ src/app/modules/estadisticas/components/citas/filtros.component.ts
@@ -1,6 +1,8 @@
 import * as moment from 'moment';
 import { Component, AfterViewInit, HostBinding, EventEmitter, Output, Input, SimpleChanges, SimpleChange, OnChanges } from '@angular/core';
 import { Plex } from '@andes/plex';
+import { TipoPrestacionService } from '../../../../services/tipoPrestacion.service';
+import { ProfesionalService } from '../../../../services/profesional.service';
 
 @Component({
     selector: 'turnos-filtros',
@@ -20,20 +22,26 @@ import { Plex } from '@andes/plex';
         </div>
     </div>
     <div class="row">
-        <div class="col-3" *ngIf="params.prestacion">
-            <plex-select [multiple]="true" [data]="params.prestacion" [(ngModel)]="seleccion.prestacion" (change)="onChange($event)" placeholder="Seleccione..." label="Prestacion">
+        <div class="col-3">
+            <plex-select [multiple]="true" [(ngModel)]="seleccion.prestacion" (getData)="loadPrestaciones($event)" name="prestaciones"
+                label="Prestación" ngModelOptions="{standalone: true}">
             </plex-select>
         </div>
-        <div class="col-3" *ngIf="params.profesional">
-            <plex-select [multiple]="true" [data]="params.profesional" [(ngModel)]="seleccion.profesional" (change)="onChange($event)" placeholder="Seleccione..." label="Profesional">
+        <div class="col-3">
+            <plex-select [multiple]="true" [(ngModel)]="seleccion.profesional" name="profesional" (getData)="loadProfesionales($event)"
+                label="Profesional" placeholder="Escriba el apellido del Profesional" labelField="apellido + ' ' + nombre">
             </plex-select>
         </div>
-        <div class="col-3" *ngIf="seleccion.tipoDeFiltro.id === 'turnos' && params.estado_turno">
-            <plex-select [multiple]="true" [data]="params.estado_turno" [(ngModel)]="seleccion.estado_turno" (change)="onChange($event)" placeholder="Seleccione..." label="Estado">
+        <div class="col-3" *ngIf="seleccion.tipoDeFiltro.id === 'turnos'">
+            <plex-select [multiple]="true" [data]="estadoTurnos" [(ngModel)]="seleccion.estado_turno" placeholder="Seleccione..." label="Estado">
             </plex-select>
         </div>
-        <div class="col-3" *ngIf="seleccion.tipoDeFiltro.id === 'turnos' && params.tipoTurno">
-            <plex-select [multiple]="true" [data]="params.tipoTurno" [(ngModel)]="seleccion.tipoTurno" (change)="onChange($event)" placeholder="Seleccione..." label="Tipo Turno">
+        <div class="col-3" *ngIf="seleccion.tipoDeFiltro.id === 'turnos'">
+            <plex-select [multiple]="true" [data]="tipoTurnos" [(ngModel)]="seleccion.tipoTurno" placeholder="Seleccione..." label="Tipo Turno">
+            </plex-select>
+        </div>
+        <div class="col-3" *ngIf="seleccion.tipoDeFiltro.id === 'agendas'">
+            <plex-select [multiple]="true" [data]="estadosAgendas" [(ngModel)]="seleccion.estado_agenda" placeholder="Seleccione..." label="Estado">
             </plex-select>
         </div>
     </div>
@@ -47,34 +55,85 @@ export class FiltrosComponent implements AfterViewInit, OnChanges {
     public hasta: Date = new Date();
     public hoy = new Date();
     public opciones = [{ id: 'agendas', nombre: 'Agendas' }, { id: 'turnos', nombre: 'Turnos' }];
+    public estadosAgendas = [
+        {id: 'planificacion', nombre: 'Planificacion'},
+        {id: 'disponible', nombre: 'Disponible'},
+        {id: 'publicada', nombre: 'Publicada'},
+        {id: 'suspendida', nombre: 'Suspendida'},
+        {id: 'pausada', nombre: 'Pausada'},
+        {id: 'pendienteAsistencia', nombre: 'Pendiente Asistencia'},
+        {id: 'pendienteAuditoria', nombre: 'Pendiente Auditoria'},
+        {id: 'auditada', nombre: 'Auditada'},
+        {id: 'borrada', nombre: 'Borrada'}
+    ];
+
+    public estadoTurnos = [
+        {id: 'disponible', nombre: 'Disponible'},
+        {id: 'asignado', nombre: 'Asignado'},
+        {id: 'suspendido', nombre: 'Suspendido'},
+        {id: 'turnoDoble', nombre: 'Turno Doble'}
+    ];
+
+    public tipoTurnos = [
+        {id: 'delDia', nombre: 'Del Dia'},
+        {id: 'programado', nombre: 'Programado'},
+        {id: 'gestion', nombre: 'Gestion'},
+        {id: 'profesional', nombre: 'Profesional'}
+    ];
 
     @Input() params: any = {};
     @Output() filter = new EventEmitter();
 
     public seleccion: any = {
         tipoDeFiltro: { id: 'turnos', nombre: 'Turnos' },
-        profesional: null,
-        prestacion: null,
-        estado_turno: [],
-        tipoTurno: []
+        profesional: undefined,
+        prestacion: undefined,
+        estado_turno: undefined,
+        estado_agenda: undefined,
+        tipoTurno: undefined
     };
 
-    constructor(private plex: Plex) { }
+    constructor(
+        private plex: Plex,
+        public servicioProfesional: ProfesionalService,
+        public servicioPrestacion: TipoPrestacionService) {
+    }
 
     ngAfterViewInit() {
         this.onChange();
     }
 
-    onChange() {
+    loadProfesionales(event) {
+        let listaProfesionales = [];
+        if (event.query) {
+            let query = {
+                nombreCompleto: event.query
+            };
+            this.servicioProfesional.get(query).subscribe(resultado => {
+                listaProfesionales = resultado;
+                event.callback(listaProfesionales);
+            });
+        } else {
+            event.callback([]);
+        }
+    }
 
+    loadPrestaciones(event) {
+        this.servicioPrestacion.get({}).subscribe(result => {
+            event.callback(result);
+        });
+    }
+
+    onChange() {
         let params = {
             fechaDesde: this.desde,
             fechaHasta: this.hasta,
             tipoDeFiltro: this.seleccion.tipoDeFiltro ? this.seleccion.tipoDeFiltro.id : undefined,
-            prestacion: this.seleccion.prestacion ? this.seleccion.prestacion.map(pr => pr.id) : undefined,
+            prestacion: this.seleccion.prestacion ? this.seleccion.prestacion.map(pr => pr.conceptId) : undefined,
             profesional: this.seleccion.profesional ? this.seleccion.profesional.map(prof => prof.id) : undefined,
-            estado_turno: this.seleccion.estado_turno.length ? this.seleccion.estado_turno.map(et => et.id) : undefined,
-            tipoTurno: this.seleccion.tipoTurno.length ? this.seleccion.tipoTurno.map(tt => tt.id) : undefined
+            estado_turno: this.seleccion.estado_turno ? this.seleccion.estado_turno.map(et => et.id) : undefined,
+            estado_agenda: this.seleccion.estado_agenda ? this.seleccion.estado_agenda.map(et => et.id) : undefined,
+            tipoTurno: this.seleccion.tipoTurno ? this.seleccion.tipoTurno.map(tt => tt.id) : undefined
         };
         this.filter.emit(params);
     }
