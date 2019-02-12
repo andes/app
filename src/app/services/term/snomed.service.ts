b80d83f0490f6417e307d9c27bdb87b50cee27d4
Mariano Andres Botta <marianoabotta@gmail.com>
2019-02-12 12:45:11 -0300

    snomed: implementa cache in-memory

--- src/app/services/term/snomed.service.ts
+++ src/app/services/term/snomed.service.ts
@@ -1,25 +1,33 @@
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Server } from '@andes/shared';
-import { environment } from '../../../environments/environment';
 
-// import { Injectable } from '@angular/core';
-// import { Http, Response, Headers, RequestOptions, URLSearchParams } from '@angular/http';
-// import { Observable } from 'rxjs/Observable';
-// import 'rxjs/add/operator/map';
+export function Cache ({key}) {
+    let _cache: any = {};
+    return function ( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        const fn = descriptor.value as Function;
+        descriptor.value = function (...args) {
+            const objectKey = key ? (typeof key === 'string' ? args[0][key] : args[0]) : 'default';
+            if (!_cache[objectKey]) {
+                return fn.apply(this, args).do(x => _cache[objectKey] = x);
+            } else {
+                return new Observable(resultado => resultado.next(_cache[objectKey]));
+            }
+        };
+        return descriptor;
+    };
+}
+
 
 @Injectable()
 export class SnomedService {
     private snomedURL = '/core/term/snomed';  // URL to web api
-    private snomedConceptURL = '/core/term/snomed/concepts';  // URL to web api
-    private snomedURLProblema = '/core/term/snomed/problema';  // URL to web api
-    private snomedURLProcedimiento = '/core/term/snomed/procedimiento';  // URL to web api
-    private snomedURLEquipamiento = '/core/term/snomed/equipamiento';  // URL to web api
     private snomedURLexpression = '/core/term/snomed/expression';
 
     constructor(private server: Server) {
     }
 
+    @Cache({ key : 'search' })
     get(params: any): Observable<any[]> {
         return this.server.get(this.snomedURL, { params: params, showError: true });
     }
