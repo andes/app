191fcb9fdbaaea857b45c26cfb9a68ba43861a8a
matiurrutia <urrutia.mati@gmail.com>
2019-02-20 14:23:53 -0300

    en prestamos cambio _id por numero para visualizar nroCarpeta

--- src/app/components/prestamosHC/prestamos/listar-prestamos.component.ts
+++ src/app/components/prestamosHC/prestamos/listar-prestamos.component.ts
@@ -223,10 +223,10 @@ export class ListarPrestamosComponent implements OnInit {
 
     sortCarpetas() { // se divide this.carpetas en letras y en numeros para hacer el sort correspondiente
         let val = this.sortDescending ? -1 : 1;
-        let carpetas_numeros = this.carpetas.filter(x => !isNaN(x._id));
-        let carpetas_letras = this.carpetas.filter(x => isNaN(x._id));
-        carpetas_letras.sort((a, b) => { return (a._id > b._id) ? val : (b._id > a._id) ? -val : 0; });
-        carpetas_numeros.sort((a, b) => { return (parseInt(a._id, 10) > parseInt(b._id, 10)) ? val : ((parseInt(b._id, 10) > parseInt(a._id, 10)) ? -val : 0); });
+        let carpetas_numeros = this.carpetas.filter(x => !isNaN(x.numero));
+        let carpetas_letras = this.carpetas.filter(x => isNaN(x.numero));
+        carpetas_letras.sort((a, b) => { return (a.numero > b.numero) ? val : (b.numero > a.numero) ? -val : 0; });
+        carpetas_numeros.sort((a, b) => { return (parseInt(a.numero, 10) > parseInt(b.numero, 10)) ? val : ((parseInt(b.numero, 10) > parseInt(a.numero, 10)) ? -val : 0); });
 
         let carpetas_sort = carpetas_numeros.concat(carpetas_letras);
         this.carpetas = [];
