a5674d7c532f00289f7db73303b7bbd0ab691e38
Mariano Andres Botta <marianoabotta@gmail.com>
2019-03-21 15:35:04 -0300

    rup: navegacion de conceptos

--- src/app/modules/estadisticas/services/snomed.service.ts
+++ src/app/modules/estadisticas/services/snomed.service.ts
@@ -45,4 +45,8 @@ export class SnomedService {
     getQuery(params: any): Observable<any[]> {
         return this.server.get(this.snomedURLexpression, { params: params, showError: true });
     }
+
+    getConcepts(sctids): Observable<any[]> {
+        return this.server.get(this.snomedURL + '/concepts', { params: { sctids }, showError: true });
+    }
 }
