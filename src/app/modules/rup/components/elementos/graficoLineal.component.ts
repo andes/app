bbf76564b71eb9672df677037ab6807016084bfd
ndinolfo <nicodinolfo@gmail.com>
2019-03-27 11:28:33 -0300

    lint

--- src/app/modules/rup/components/elementos/graficoLineal.component.ts
+++ src/app/modules/rup/components/elementos/graficoLineal.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RUPComponent } from '../core/rup.component';
-
 import * as moment from 'moment';
-import 'moment/locale/es';
 import { isNumber } from 'util';
 import { RupElement } from '.';
 
