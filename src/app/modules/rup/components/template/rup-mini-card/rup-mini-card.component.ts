2eaf842f19ae019f599c0e8ac42d6a1b7629ad04
Andrés <andresin@gmail.com>
2019-04-01 08:16:11 -0300

    Componente rup-mini-card

---
+++ src/app/modules/rup/components/template/rup-mini-card/rup-mini-card.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { PrestacionesService } from '../../../services/prestaciones.service';
+import { TipoPrestacionService } from '../../../../../services/tipoPrestacion.service';
+import { HUDSService } from '../../../services/huds.service';
+
+@Component({
+  selector: 'rup-mini-card',
+  templateUrl: './rup-mini-card.component.html',
+  styleUrls: ['rup-mini-card.component.scss']
+})
+
+export class RupMiniCardComponent implements OnInit {
+
+  @Input() registro;
+  @Input() filtroActual;
+  @Input() emitirConceptos;
+  @Input() snomed;
+
+  @Output() registroVolador: EventEmitter<any> = new EventEmitter<any>();
+  @Output() accionSecundaria: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(
+    public huds: HUDSService,
+    public servicioPrestacion: PrestacionesService,
+    public servicioTipoPrestacion: TipoPrestacionService) { }
+
+  ngOnInit() {
+
+  }
+
+  emitirRegistro(registro, tipo, destino) {
+    this.registroVolador.emit({ registro, tipo, destino });
+  }
+
+  emitirAccion(registro, tipo, destino) {
+    this.accionSecundaria.emit({ registro, tipo, destino });
+  }
+}
