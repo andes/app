7ea5bc66ad6fd71406ef0833358f75ca53d551b8
ndinolfo <nicodinolfo@gmail.com>
2019-04-16 08:54:13 -0300

    muestra pestaña de turnos luego de dar turno

--- src/app/components/turnos/punto-inicio/puntoInicio-turnos.component.ts
+++ src/app/components/turnos/punto-inicio/puntoInicio-turnos.component.ts
@@ -191,7 +191,7 @@ export class PuntoInicioTurnosComponent implements OnInit {
     afterDarTurno(pac) {
         this.showDarTurnos = false;
         this.showDashboard = true;
-        this.showTab = 2;
+        this.showTab = 1;
         if (this.paciente && this.paciente.id) {
             // this.onPacienteSelected(this.paciente);
             if (pac && pac.carpetaEfectores && pac.carpetaEfectores.length > 0) {
