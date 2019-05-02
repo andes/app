ce127c0da8db15d9ffd5b38cd49b83314b641e34
Luis <luisparadawagner@gmail.com>
2019-05-02 11:42:22 -0300

    Se corrige os en paciente sumar

--- src/app/components/turnos/punto-inicio/turnos-paciente.component.ts
+++ src/app/components/turnos/punto-inicio/turnos-paciente.component.ts
@@ -63,7 +63,7 @@ export class TurnosPacienteComponent implements OnInit {
         if (value) {
             this._turnos = value;
             this.turnosPaciente = value;
-            this.turnosPaciente.obraSocial = this._paciente.financiador[0].nombre;
+            this.turnosPaciente.obraSocial = (this._paciente.financiador.length > 0) ? this._paciente.financiador[0].nombre : null;
         }
     }
     get turnos(): any {
