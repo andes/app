47ca87e491ec6e9f6b2c5696822ed08cf7935472
Pablo Lammel <lammelpablo@gmail.com>
2019-05-21 13:30:42 -0300

    remueve ViewChild innecesario

--- src/app/components/turnos/gestor-agendas/gestor-agendas.component.ts
+++ src/app/components/turnos/gestor-agendas/gestor-agendas.component.ts
@@ -31,8 +31,6 @@ export class GestorAgendasComponent implements OnInit, OnDestroy {
         this.guardarAgendaPanel = theElementRef;
     }
 
-    @ViewChild('botonesAgenda') botonesAgendaComponent: BotonesAgendaComponent;
-
     agendasSeleccionadas: IAgenda[] = [];
     turnosSeleccionados: ITurno[] = [];
 
