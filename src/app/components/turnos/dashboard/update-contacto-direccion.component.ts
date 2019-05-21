1162c06a7ca5ebfcbec0a581c11a0fd30e69d339
ndinolfo <nicodinolfo@gmail.com>
2019-05-21 14:28:46 -0300

    fix - al cambiar de paciente no refresca direccion pto inicio turnos

--- src/app/components/turnos/dashboard/update-contacto-direccion.component.ts
+++ src/app/components/turnos/dashboard/update-contacto-direccion.component.ts
@@ -23,8 +23,16 @@ import { ILocalidad } from '../../../interfaces/ILocalidad';
 
 export class UpdateContactoDireccionComponent implements OnInit {
 
-    @Input() paciente: IPaciente;
+    @Input()
+    get pac(): IPaciente {
+        return this.paciente;
+    }
+    set pac(value: IPaciente) {
+        this.paciente = value;
+        this.loadPaciente();
+    }
 
+    paciente: IPaciente;
     arrayContactos: IContacto[] = [];
     provincias: IProvincia[] = [];
     localidades: ILocalidad[] = [];
@@ -71,18 +79,7 @@ export class UpdateContactoDireccionComponent implements OnInit {
         this.tipoComunicacion = enumerados.getObjTipoComunicacion();
         this.tipoComunicacion.splice(this.tipoComunicacion.length - 1, 1);  // eliminamos 'Email'
 
-        if (this.paciente && this.paciente.contacto && this.paciente.contacto.length) {
-            this.arrayContactos = this.paciente.contacto;
-        } else {
-            this.arrayContactos = [this.contacto];
-        }
-
-        if (this.paciente && this.paciente.direccion && this.paciente.direccion.length && this.paciente.direccion[0].valor) {
-            this.direccion = this.paciente.direccion[0];
-            if (this.direccion.ubicacion && this.direccion.ubicacion.provincia) {
-                this.changeProvincia(this.direccion.ubicacion.provincia);
-            }
-        }
+        this.loadPaciente();
 
         // Set País Argentina
         this.paisService.get({
@@ -102,6 +99,20 @@ export class UpdateContactoDireccionComponent implements OnInit {
         }).subscribe(Prov => {
             this.provinciaNeuquen = Prov[0];
         });
+    }
+
+    private loadPaciente() {
+        if (this.paciente && this.paciente.contacto && this.paciente.contacto.length) {
+            this.arrayContactos = this.paciente.contacto;
+        } else {
+            this.arrayContactos = [this.contacto];
+        }
+        if (this.paciente && this.paciente.direccion && this.paciente.direccion[0].valor) {
+            this.direccion = this.paciente.direccion[0];
+            if (this.direccion.ubicacion.provincia) {
+                this.changeProvincia(this.direccion.ubicacion.provincia);
+            }
+        }
     }
 
     addContacto(key, valor) {
