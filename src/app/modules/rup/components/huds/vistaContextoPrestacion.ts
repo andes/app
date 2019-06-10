df59262cc21b4c30512b3dd82840a193c9a354b5
Andrés <andresin@gmail.com>
2019-06-10 10:37:36 -0300

    Se muestran badges usando servicio de Prestaciones

--- src/app/modules/rup/components/huds/vistaContextoPrestacion.ts
+++ src/app/modules/rup/components/huds/vistaContextoPrestacion.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { IPrestacionRegistro } from '../../interfaces/prestacion.registro.interface';
 import { IPrestacion } from '../../interfaces/prestacion.interface';
+import { PrestacionesService } from '../../services/prestaciones.service';
 
 @Component({
     selector: 'vista-contexto-prestacion',
@@ -13,7 +14,7 @@ export class VistaContextoPrestacionComponent implements OnInit {
     @Input() registro: IPrestacionRegistro;
     @Input() prestacion: IPrestacion;
 
-    constructor() { }
+    constructor(public _prestacionesService: PrestacionesService) { }
 
     ngOnInit() {
 
