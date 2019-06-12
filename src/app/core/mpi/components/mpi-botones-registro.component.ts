adde5fe6919fd4dbe76bae29312be31123658fb9
ndinolfo <nicodinolfo@gmail.com>
2019-06-12 14:39:26 -0300

    refactor(MPI):agrega input  hostcomponent a mpi botones component
    este parámetro se utiliza para conocer el componente origen desde donde se hace el redirect utilizando el dropdown

--- src/app/core/mpi/components/mpi-botones-registro.component.ts
+++ src/app/core/mpi/components/mpi-botones-registro.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'mpi-botones-registro',
     templateUrl: 'mpi-botones-registro.html',
     styleUrls: ['mpi-botones-registro.scss']
 })
-export class BotonesRegistroComponent {
+export class BotonesRegistroComponent implements OnInit {
     private toggler = false;
+    routes;
     @Input() disabled: boolean;
-    routes = [
-        { label: 'BEBÉ', route: '/apps/mpi/bebe' },
-        { label: 'EXTRANJERO', route: '/apps/mpi/extranjero' },
-        { label: 'CON DNI ARGENTINO', route: '/apps/mpi/paciente/con-dni' },
-        { label: 'SIN DNI ARGENTINO', route: '/apps/mpi/paciente/sin-dni' },
-    ];
+    @Input() hostComponent = '';
+
+
+    ngOnInit(): void {
+        this.routes = [
+            { label: 'BEBÉ', route: `/apps/mpi/bebe/${this.hostComponent}` },
+            { label: 'EXTRANJERO', route: `/apps/mpi/extranjero/${this.hostComponent}` },
+            { label: 'CON DNI ARGENTINO', route: `/apps/mpi/paciente/con-dni/${this.hostComponent}` },
+            { label: 'SIN DNI ARGENTINO', route: `/apps/mpi/paciente/sin-dni/${this.hostComponent}` },
+        ];
+    }
 
     toggleMenu() {
         this.toggler = !this.toggler;
