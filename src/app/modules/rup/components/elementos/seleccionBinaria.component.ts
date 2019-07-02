3e7ae7af5ea6ab1767c63ad9e57b4e2fe80feda8
Lautaro Molina <negro89_nqn@hotmail.com>
2019-07-02 10:42:53 -0300

    refactor (RUP): Mejoras varias en registro de colonoscopia
    Se modifican/agregan conceptos y campos en formulario de colonoscopía de control.

--- src/app/modules/rup/components/elementos/seleccionBinaria.component.ts
+++ src/app/modules/rup/components/elementos/seleccionBinaria.component.ts
@@ -9,23 +9,32 @@ import { RupElement } from '.';
 @RupElement('SeleccionBinariaComponent')
 export class SeleccionBinariaComponent extends RUPComponent implements OnInit {
 
-    public options;
-    public valor;
-    public label;
+    public options = null;
+    public valor = null;
+    public label = null;
+    public okayOptions = null;
+    public showOkayOptions = null;
 
     ngOnInit() {
         this.label = this.params && this.params.title ? this.params.title : this.registro.concepto.term;
         this.options = this.params.defaultOptions;
+        this.okayOptions = this.params.okayOptions ? this.params.okayOptions : null;
         if (!this.registro.valor) {
             this.registro.valor = null;
         } else {
             this.valor = this.registro.valor.id;
+            this.showOkayOptions = (this.okayOptions && this.okayOptions.some(opt => this.valor === opt.id));
         }
     }
 
     onChange() {
         this.registro.valor = this.options.find(i => i.id === this.valor);
+        (this.valor === 'Si' && this.okayOptions) ? (this.showOkayOptions = true) : (this.showOkayOptions = false);
         this.emitChange();
     }
 
+    onChangeOkayOpt() {
+        this.registro.valor = this.okayOptions.find(i => i.id === this.valor);
+        this.emitChange();
+    }
 }
