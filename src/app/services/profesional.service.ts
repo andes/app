5639b426c1e5c49b4334da6ab14749040d6d1c25
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-09-19 15:36:47 -0300

    spec(tm): agrega al servicio de profesional la posibilidad de editar en lugar de crear

--- src/app/services/profesional.service.ts
+++ src/app/services/profesional.service.ts
@@ -44,8 +44,9 @@ export class ProfesionalService {
         return this.server.get(this.profesionalUrl + '/foto/', { params: params });
     }
 
-    saveProfesional(profesionalModel: any) {
-        return this.server.post(this.profesionalUrl, profesionalModel);
+    saveProfesional(profesionalModel: any, esAltaProfesional: boolean = true) {
+        return esAltaProfesional ? this.server.post(this.profesionalUrl, profesionalModel) :
+            this.server.patch(`${this.profesionalUrl}/${profesionalModel.profesional.id}`, profesionalModel.profesional);
     }
 
     disable(profesional: IProfesional): Observable<IProfesional> {
