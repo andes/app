1edb66b10c23fd796d00818aaac569651697da3e
DESKTOP-BANPLN5\Sebas <pseba20@gmail.com>
2019-09-19 10:33:04 -0300

    modificacion funcion ancho de columnas servicio xlsx

--- src/app/services/excel.service.ts
+++ src/app/services/excel.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
+import { config } from 'rxjs';
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
 @Injectable()
 export class ExcelService {
 constructor() { }
 public exportAsExcelFile(tbl: any, excelFileName: string): void {
-  let fl = XLSX.utils.table_to_book(tbl, {raw: true});
-  XLSX.writeFile(fl, excelFileName + '_export_' + new  Date().getTime() + EXCEL_EXTENSION, {cellStyles: true});
+  let fl = XLSX.utils.table_to_book(tbl, { /*Sheets: 'Reporte'}*/ );
+
+
+  let x = tbl.rows[0].cells[0].innerHTML.length;
+    //console.dir(x)
+    //console.dir(tbl.rows[0].cells.length);
+
+   let configCol: any = [] ;
+
+      for (let i = 0; i < tbl.rows[0].cells.length; i++) {
+     configCol.push( {'wpx':  180}); //tbl.rows[0].cells[i].innerHTML.length
+   }
+
+   console.dir(configCol);
+   console.dir(fl);
+  fl['Sheets']['Sheet1']['!cols'] = configCol;
+  XLSX.writeFile(fl, excelFileName + '_Reporte_' + new  Date().getTime() + EXCEL_EXTENSION, {cellStyles: true});
 }
+
 
 }
