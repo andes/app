6e704cf2e8fd917033e3aa9cc034a06892408d38
LucasGonzalez <lucasgonzalezm3@gmail.com>
2019-10-04 09:32:49 -0300

    feat(top): parametriza componente de visualizacion de reglas

--- src/app/components/top/reglas/visualizacionReglas.component.ts
+++ src/app/components/top/reglas/visualizacionReglas.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ReglaService } from '../../../services/top/reglas.service';
 import { OrganizacionService } from '../../../services/organizacion.service';
 import { TipoPrestacionService } from './../../../services/tipoPrestacion.service';
 import { IRegla } from '../../../interfaces/IRegla';
 import { IOrganizacion } from '../../../interfaces/IOrganizacion';
 import { ITipoPrestacion } from '../../../interfaces/ITipoPrestacion';
+import { Auth } from '@andes/auth';
 
 @Component({
     selector: 'visualizacion-reglas',
     templateUrl: './visualizacionReglas.html'
 })
-export class VisualizacionReglasComponent {
+export class VisualizacionReglasComponent implements OnInit {
+    @Input()
+    esParametrizado = false;
     /**
      * Organización ingresada en el filtro de organización origen
      * @type {IOrganizacion}
@@ -23,6 +26,7 @@ export class VisualizacionReglasComponent {
      */
     organizacionOrigen: IOrganizacion;
     prestacionOrigen: ITipoPrestacion;
+    prestacionesOrigen: ITipoPrestacion[] = [];
     /**
      * Organización ingresada en el filtro de organización destino
      * @type {IOrganizacion}
@@ -39,13 +43,24 @@ export class VisualizacionReglasComponent {
      * Datos de las filas de la tabla resultados. Se realiza en typescript por ser
      * más sencillo que en HTML
      *
-     * @type {{}[]}
+     * @type {any[]}
      * @memberof VisualizacionReglasComponent
      */
-    filas: {}[];
+    filas: any[];
 
-    constructor(private servicioReglas: ReglaService, private servicioOrganizacion: OrganizacionService, public servicioPrestacion: TipoPrestacionService) { }
+    constructor(private servicioReglas: ReglaService, private servicioOrganizacion: OrganizacionService, public servicioPrestacion: TipoPrestacionService, private auth: Auth, private serviceTipoPrestacion: TipoPrestacionService) { }
 
+    ngOnInit() {
+        if (this.esParametrizado) {
+            this.organizacionOrigen = this.auth.organizacion;
+
+            let idPrestacionesPermisoProfesional = this.auth.getPermissions('rup:tipoPrestacion:?');
+            this.servicioPrestacion.get({ id: idPrestacionesPermisoProfesional }).subscribe(prestaciones => {
+                this.prestacionesOrigen = prestaciones;
+                this.actualizarTabla();
+            });
+        }
+    }
     /**
      * Trae las organizaciones con nombre "igual" al texto ingresado.
      * Si se ingresa "spi dan" trae Hospital Bouquet Roldan
@@ -85,10 +100,15 @@ export class VisualizacionReglasComponent {
         if (this.filtroIngresado()) {
             let parametros = {
                 organizacionOrigen: this.organizacionOrigen ? this.organizacionOrigen.id : '',
-                prestacionOrigen: this.prestacionOrigen ? this.prestacionOrigen.conceptId : '',
                 organizacionDestino: this.organizacionDestino ? this.organizacionDestino.id : '',
                 prestacionDestino: this.prestacionDestino ? this.prestacionDestino.conceptId : ''
             };
+            if (this.esParametrizado) {
+                parametros['prestacionesOrigen'] = this.prestacionesOrigen.map(prestacion => { return prestacion.conceptId; });
+            } else {
+                parametros['prestacionOrigen'] = this.prestacionOrigen ? this.prestacionOrigen.conceptId : '';
+            }
+
             this.servicioReglas.get(parametros).subscribe((reglas: [IRegla]) => {
                 this.obtenerFilasTabla(reglas);
             });
@@ -116,10 +136,12 @@ export class VisualizacionReglasComponent {
      */
     obtenerFilasTabla(reglas: [IRegla]) {
         this.filas = [];
-
+        const conceptIds = this.prestacionesOrigen.map(prestacion => { return prestacion.conceptId; });
         for (let regla of reglas) {
             regla.origen.prestaciones.forEach((prestacionAux: any) => { // prestacionAux es cada celda del arreglo de origen.prestaciones. Tiene la prestación y si es auditable
-                if ((!this.prestacionOrigen || this.prestacionOrigen.conceptId === prestacionAux.prestacion.conceptId)) {
+                if ((this.prestacionOrigen && this.prestacionOrigen.conceptId === prestacionAux.prestacion.conceptId) ||
+                    (conceptIds.length && conceptIds.some(conceptIdOrigen => { return conceptIdOrigen === prestacionAux.prestacion.conceptId; })) ||
+                    (!this.prestacionOrigen && !conceptIds.length)) {
                     /* Es necesaria esta validación porque una regla tiene un origen y un destino. El origen se compone de
                      * una organización y una lista de prestaciones. Entonces si filtra por prestación origen, que muestre
                      * solo aquellas partes de la regla que cumpla con los filtros ingresados. El destino es una organización
