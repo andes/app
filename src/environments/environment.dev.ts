cd8d32b320fdb6555abb3315d61ddfe633a8557d
marceloperegil <marcelo.gil@outlook.com>
2019-12-19 12:56:00 -0300

    correccion http

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -5,8 +5,8 @@ import { apiKeys } from './apiKeyMaps';
 export const environment = {
   production: true,
   environmentName: 'development',
-  WS: 'http://localhost:3002',
-  API: 'http://localhost:3002/api',
+  WS: '//localhost:3002',
+  API: '//localhost:3002/api',
   APIStatusCheck: false,
   version: _package.version,
   MAPS_KEY: apiKeys.develop
