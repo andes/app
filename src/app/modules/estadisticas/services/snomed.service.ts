46dbd310eb3e9fba33f5a9158681adec7acb0663
plammel <lammelpablo@gmail.com>
2020-02-05 11:00:54 -0300

    fix(RUP): Corrige ruta de mapeo CIE10

--- src/app/modules/estadisticas/services/snomed.service.ts
+++ src/app/modules/estadisticas/services/snomed.service.ts
@@ -10,9 +10,6 @@ import { Server } from '@andes/shared';
 @Injectable()
 export class SnomedService {
     private snomedURL = '/core/term/snomed';  // URL to web api
-    private snomedURLProblema = '/core/term/snomed/problema';  // URL to web api
-    private snomedURLProcedimiento = '/core/term/snomed/procedimiento';  // URL to web api
-    private snomedURLEquipamiento = '/core/term/snomed/equipamiento';  // URL to web api
     private snomedURLexpression = '/core/term/snomed/expression';
 
     constructor(private server: Server) {
@@ -20,10 +17,6 @@ export class SnomedService {
 
     get(params: any): Observable<any[]> {
         return this.server.get(this.snomedURL, { params: params, showError: true });
-    }
-
-    getCie10(params: any): Observable<any> {
-        return this.server.get(this.snomedURL + '/map', { params: params, showError: false });
     }
 
     getQuery(params: any): Observable<any[]> {
