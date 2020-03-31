3ff4d341b8da4449042f715effc2db0f95890d7c
Juliana <juli.otero01@gmail.com>
2020-03-31 11:04:27 -0300

    fix(internacion): fix linteo

--- src/app/apps/rup/mapa-camas/views/censos/censo-diario/censo-diario.component.ts
+++ src/app/apps/rup/mapa-camas/views/censos/censo-diario/censo-diario.component.ts
@@ -101,7 +101,7 @@ export class CensosDiariosComponent implements OnInit {
                             }
 
                         }
-                    })
+                    });
                 });
             });
     }
