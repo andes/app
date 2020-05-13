eb0032d4da1278a7bc799c1663d31d950745e272
root <root@danilaptop>
2020-05-13 08:22:51 -0300

    Pestaña seguimiento paciente unicamente

--- src/app/modules/rup/components/ejecucion/resumen-paciente/resumenPaciente-dinamico.component.ts
+++ src/app/modules/rup/components/ejecucion/resumen-paciente/resumenPaciente-dinamico.component.ts
@@ -2,10 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Auth } from '@andes/auth';
 import { IPaciente } from '../../../../../core/mpi/interfaces/IPaciente';
 import { ISnomedConcept } from '../../../interfaces/snomed-concept.interface';
-import { ISeguimientoPaciente } from '../../../interfaces/seguimientoPaciente.interface';
 import { ElementosRUPService } from '../../../services/elementosRUP.service';
 import { PrestacionesService } from '../../../services/prestaciones.service';
-import { SeguimientoPacientesService } from '../../../services/seguimientoPacientes.service';
 import { subscribeOn } from 'rxjs/operators';
 
 @Component({
@@ -19,9 +17,6 @@ export class ResumenPacienteDinamicoComponent implements OnInit {
     public registros: any[] = [];
     public graficos: any[] = [];
     public elementos: any[] = [];
-
-    public registrosSeguimiento: ISeguimientoPaciente[] = [];
-    public elementoSeguimiento: any[] = [];
 
     public conceptosGrafico: ISnomedConcept[] = [
         {
@@ -60,35 +55,10 @@ export class ResumenPacienteDinamicoComponent implements OnInit {
         }
     ];
 
-    // Seguimiento paciente San Juan
-
-    conceptoSeguimientoPaciente: ISnomedConcept[] = [{
-            conceptId: '408403008',
-            term: 'registro llevado por el paciente',
-            fsn: 'registro llevado por el paciente (elemento de registro)',
-            semanticTag: 'elemento de registro'
-        },
-        {
-            conceptId: '413153004',
-            term: 'tensión arterial registrada por el paciente en su hogar',
-            fsn: 'tensión arterial registrada por el paciente en su hogar (procedimiento)',
-            semanticTag: 'procedimiento'
-        },
-        {
-            conceptId: '103228002',
-            term: 'saturación de oxígeno en sangre',
-            fsn: 'saturación de hemoglobina con oxígeno (entidad observable)',
-            semanticTag: 'entidad observable'
-        }
-    ] ;
-
-
-
     constructor(
         public elementosRUPService: ElementosRUPService,
         public auth: Auth,
-        public prestacionesService: PrestacionesService,
-        public seguimientoPacientesService: SeguimientoPacientesService
+        public prestacionesService: PrestacionesService
     ) { }
 
     ngOnInit() {
@@ -106,21 +76,6 @@ export class ResumenPacienteDinamicoComponent implements OnInit {
             });
 
         }
-
-        // Seguimiento paciente San Juan
-        for (let concepto of this.conceptoSeguimientoPaciente) {
-            this.elementoSeguimiento =  [...this.elementoSeguimiento, this.elementosRUPService.buscarElemento(concepto, false)];
-        }
-
-        this.seguimientoPacientesService.getRegistros({idPaciente: this.paciente.id}).subscribe(seguimientoPacientes => {
-               if (seguimientoPacientes.length) {
-                   for (let seguimientoPaciente of seguimientoPacientes) {
-                        this.registrosSeguimiento = [...this.registrosSeguimiento, seguimientoPaciente];
-                   }
-               }
-        });
-
-
 
     }
 }
