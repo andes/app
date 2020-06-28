5211b9507180bd642e075b20eb756c6d66ccb7c1
Andrés Boc-Ho <andresin@gmail.com>
2020-06-28 16:55:57 -0300

    feat: se cambia a nueva colección de módulos

--- src/app/services/novedades/modulos.service.ts
+++ src/app/services/novedades/modulos.service.ts
@@ -3,6 +3,6 @@ import { Server, ResourceBaseHttp } from '@andes/shared';
 
 @Injectable()
 export class ModulosService extends ResourceBaseHttp {
-    protected url = '/core/tm/modulos';
+    protected url = '/core/tm/modulos_new';
     constructor(protected server: Server) { super(server); }
 }
