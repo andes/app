da7325c0928eb28689844492ec1fce6cf3d4a331
Rodrigo Kokot <rodrigokokot@gmail.com>
2020-06-30 10:26:52 -0300

    Alerta time zone incorrecta

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,14 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/';
 import * as bowser from 'bowser';
 
+const timeZone = new Date().getTimezoneOffset();
+
 // Check browser version
 if (!(bowser as any).check({ chrome: '54' }) || !(bowser as any).check({ firefox: '54' })) {
   window.document.getElementById('incompatible-browser').style.display = 'block';
+  window.document.getElementById('preloader').style.display = 'none';
+} else if (timeZone !== 180) {
+  window.document.getElementById('incompatible-timeZone').style.display = 'block';
   window.document.getElementById('preloader').style.display = 'none';
 } else {
   // Enabled production mode
@@ -19,3 +24,14 @@ if (!(bowser as any).check({ chrome: '54' }) || !(bowser as any).check({ firefox
   platformBrowserDynamic().bootstrapModule(AppModule);
 }
 
+
+// function convertUTCDateToLocalDate(date) {
+//   var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+
+//   var offset = date.getTimezoneOffset() / 60;
+//   var hours = date.getHours();
+
+//   newDate.setHours(hours - offset);
+
+//   return newDate;   
+// }
