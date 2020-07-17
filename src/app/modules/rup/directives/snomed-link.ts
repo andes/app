f66291275f4d83311a238e216a966c954615457a
Mariano Andres Botta <marianoabotta@gmail.com>
2020-07-17 12:15:14 -0300

    feat(rup): link a snomed browser en buscardor

---
+++ src/app/modules/rup/directives/snomed-link.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { ISnomedConcept } from '../interfaces/snomed-concept.interface';
+
+@Component({
+    selector: 'snomed-link',
+    template: `
+    <a target="_blank"
+       title="ver detalle"
+       class="snomed-link"
+       href="https://browser.ihtsdotools.org/?perspective=full&conceptId1={{concepto.conceptId}}&edition=MAIN/SNOMEDCT-ES/SNOMEDCT-AR/2020-05-31&release=&languages=es">
+        {{concepto.term}}
+    </a>
+    `,
+    styles: [
+        `
+        .snomed-link {
+            color: #292b2c
+        }
+        `
+    ]
+})
+export class SnomedLinkComponent {
+    @Input() concepto: ISnomedConcept;
+}
