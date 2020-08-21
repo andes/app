32c9330e9ad86ffe68d47f1e3c545ea85109b7df
Rodrigo Kokot <rodrigokokot@gmail.com>
2020-08-21 09:04:51 -0300

    se quito la modificacion de la zona horaria

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,9 @@ import { environment } from './environments/environment';
 import { AppModule } from './app/';
 import * as bowser from 'bowser';
 
-const timeZone = new Date().getTimezoneOffset();
-
 // Check browser version
 if (!(bowser as any).check({ chrome: '54' }) || !(bowser as any).check({ firefox: '54' })) {
   window.document.getElementById('incompatible-browser').style.display = 'block';
-  window.document.getElementById('preloader').style.display = 'none';
-} else if (timeZone !== 180) {
-  window.document.getElementById('incompatible-timeZone').style.display = 'block';
   window.document.getElementById('preloader').style.display = 'none';
 } else {
   // Enabled production mode
