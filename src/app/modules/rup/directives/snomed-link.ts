7de51ebc1329ff7989fdbb0a352e384bd8576dba
waltergarrido <blackwalter7@gmail.com>
2020-09-04 09:03:43 -0300

    feat(RUP):buscador, mostrar fsn cuando no coicide con el term

--- src/app/modules/rup/directives/snomed-link.ts
+++ src/app/modules/rup/directives/snomed-link.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ISnomedConcept } from '../interfaces/snomed-concept.interface';
+import { ConceptosTurneablesService } from '../../../services/conceptos-turneables.service';
 
 @Component({
     selector: 'snomed-link',
@@ -8,7 +9,11 @@ import { ISnomedConcept } from '../interfaces/snomed-concept.interface';
        title="ver detalle"
        class="snomed-link"
        href="https://browser.ihtsdotools.org/?perspective=full&conceptId1={{concepto.conceptId}}&edition=MAIN/SNOMEDCT-ES/SNOMEDCT-AR/2020-05-31&release=&languages=es">
-        {{concepto.term}}
+       {{concepto.term}}
+        <ng-container *ngIf="sinonimo">
+                 <br>
+                 <p class="text-muted">{{fsn}}</p>
+         </ng-container>
     </a>
     `,
     styles: [
@@ -19,6 +24,18 @@ import { ISnomedConcept } from '../interfaces/snomed-concept.interface';
         `
     ]
 })
-export class SnomedLinkComponent {
+export class SnomedLinkComponent implements OnInit {
     @Input() concepto: ISnomedConcept;
+    public sinonimo = false;
+    public fsn;
+
+    ngOnInit() {
+        const indice = this.concepto.fsn.indexOf('(');
+        this.fsn = this.concepto.fsn.substring(0, indice);
+
+        if (this.concepto.term.trim() !== this.fsn.trim()) {
+            this.sinonimo = true;
+        }
+    }
+
 }
