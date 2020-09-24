9fde0f95913b22572ffab7669a7a91a255fd32d4
Mariano Andres Botta <marianoabotta@gmail.com>
2020-09-24 14:45:52 -0300

    feat(citas): evita nueva consulta al cambiar de tab

--- src/app/components/turnos/dashboard/estadisticas-pacientes.component.ts
+++ src/app/components/turnos/dashboard/estadisticas-pacientes.component.ts
@@ -1,9 +1,12 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, OnChanges, SimpleChange } from '@angular/core';
 import * as moment from 'moment';
 import { IPaciente } from '../../../core/mpi/interfaces/IPaciente';
 import { TurnoService } from '../../../services/turnos/turno.service';
 import { Auth } from '@andes/auth';
 import { LogPacienteService } from '../../../services/logPaciente.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { cache } from '@andes/shared';
 
 
 @Component({
@@ -14,28 +17,14 @@ import { LogPacienteService } from '../../../services/logPaciente.service';
 
 export class EstadisticasPacientesComponent implements OnInit {
     pacienteFields = ['sexo', 'fechaNacimiento', 'financiador', 'numeroAfiliado', 'direccion', 'telefono'];
-    nroCarpeta: any;
-    _paciente: IPaciente;
-    turnosPaciente: any;
-    ultimosTurnos: any;
-    pacienteSeleccionado: IPaciente;
-    fechaDesde: any;
-    fechaHasta: any;
-    turnosOtorgados = 0;
-    inasistencias = 0;
-    idOrganizacion = this.auth.organizacion.id;
-    carpetaEfector: any;
-    currentTab = 0;
-    @Input() showTab: Number = 0;
-    @Input('paciente')
-    set paciente(value: any) {
-        this.pacienteSeleccionado = value;
-        this._paciente = value;
-    }
-    get paciente(): any {
-        return this._paciente;
-    }
 
+    historial$: Observable<any[]>;
+    turnosPaciente$: Observable<any[]>;
+    ultimosTurnos$: Observable<any[]>;
+
+
+    @Input() showTab: Number = 0;
+    @Input() paciente: IPaciente;
     @Output() showArancelamientoForm = new EventEmitter<any>();
 
     // Inicialización
@@ -46,55 +35,31 @@ export class EstadisticasPacientesComponent implements OnInit {
     ) { }
 
     ngOnInit() {
-        this.carpetaEfector = {
-            organizacion: {
-                id: this.auth.organizacion.id,
-                nombre: this.auth.organizacion.nombre
-            },
-            nroCarpeta: ''
-        };
+        this.refresh();
+    }
+
+    refresh() {
+        this.historial$ = this.serviceTurno.getHistorial({ pacienteId: this.paciente.id }).pipe(
+            map(turnos => this.sortByHoraInicio(turnos)),
+            cache()
+        );
+        this.turnosPaciente$ = this.historial$.pipe(
+            map(turnos => turnos.filter(t => moment(t.horaInicio).isSameOrAfter(new Date(), 'day') && t.estado !== 'liberado'))
+        );
+        this.ultimosTurnos$ = this.historial$.pipe(
+            map(turnos => turnos.filter(t => moment(t.horaInicio).isSameOrBefore(new Date(), 'day')))
+        );
     }
 
     arancelamiento(turno) {
         this.showArancelamientoForm.emit(turno);
     }
 
-    updateHistorial() {
-        if (this._paciente && this._paciente.id) {
-            let cantInasistencias = 0;
-            // Se muestra la cantidad de turnos otorgados e inasistencias
-            this.serviceTurno.getHistorial({ pacienteId: this._paciente.id }).subscribe(turnos => {
-                turnos.forEach(turno => {
-                    if (turno.asistencia && turno.asistencia === 'noAsistio') {
-                        cantInasistencias++;
-                    }
-                });
-                this.turnosOtorgados = turnos.length;
-                this.inasistencias = cantInasistencias;
-                this.sortTurnos(turnos);
-                this.turnosPaciente = turnos.filter(t => moment(t.horaInicio).isSameOrAfter(new Date(), 'day') && t.estado !== 'liberado');
-                this.ultimosTurnos = turnos.filter(t => moment(t.horaInicio).isSameOrBefore(new Date(), 'day'));
-            });
-        }
-    }
-
-    private sortTurnos(turnos) {
-        turnos = turnos.sort((a, b) => {
-            let inia = a.horaInicio ? new Date(a.horaInicio) : null;
-            let inib = b.horaInicio ? new Date(b.horaInicio) : null;
-            {
-                return ((inia && inib) ? (inib.getTime() - inia.getTime()) : 0);
-            }
+    private sortByHoraInicio(turnos: any[]) {
+        return turnos.sort((a, b) => {
+            const inia = a.horaInicio ? new Date(a.horaInicio) : null;
+            const inib = b.horaInicio ? new Date(b.horaInicio) : null;
+            return ((inia && inib) ? (inib.getTime() - inia.getTime()) : 0);
         });
     }
-
-    changeTab(event) {
-        this.currentTab = event;
-        if (this._paciente && this._paciente.id) {
-            if (event === 1 || event === 2) {
-                this.updateHistorial();
-            }
-        }
-    }
-
 }
