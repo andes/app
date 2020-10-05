e9ba3d364518b6ef1a0ff43dddcd6c82009971a9
waltergarrido <blackwalter7@gmail.com>
2020-10-05 12:27:52 -0300

    fix(RUP):correccion para contemplar futuros parametros y se etira encolumnado

--- src/app/modules/rup/components/elementos/adjuntarDocumento.component.ts
+++ src/app/modules/rup/components/elementos/adjuntarDocumento.component.ts
@@ -67,7 +67,7 @@ export class AdjuntarDocumentoComponent extends RUPComponent implements OnInit {
     }
 
     onValidate() {
-        if (!this.params) {
+        if (!this.params?.required) {
             this.uploadValid = this.registro.valor.documentos.length > 0;
             return this.uploadValid;
         }
