654c0db7a43f321ba848c8285d5eac3a606830dc
Pancho <martinebucarey@gmail,com>
2020-10-19 15:00:33 -0300

    feat(visualizacion): export huds

--- src/app/modules/visualizacion-informacion/visualizacion-informacion.routing.ts
+++ src/app/modules/visualizacion-informacion/visualizacion-informacion.routing.ts
@@ -2,11 +2,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BiQueriesComponent } from './components/bi-queries/bi-queries.component';
+import { ExportarHudsComponent } from './components/exportar-huds/exportar-huds.component';
 
 const routes: Routes = [
     {
         path: 'bi-queries',
         component: BiQueriesComponent
+    },
+    {
+        path: 'exportar-huds',
+        component: ExportarHudsComponent
     }
 ]; @NgModule({
     imports: [RouterModule.forChild(routes)],
