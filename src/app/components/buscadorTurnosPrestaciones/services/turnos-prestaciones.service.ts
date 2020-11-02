e12548a0d3b5b5499d7520efe2fbeb986018b159
martinebucarey <56874534+martinebucarey@users.noreply.github.com>
2020-11-02 09:56:26 -0300

    feat(buscador): ordenar asc/desc columnas (#2000)
    Co-authored-by: Pancho <martinebucarey@gmail,com>

--- src/app/components/buscadorTurnosPrestaciones/services/turnos-prestaciones.service.ts
+++ src/app/components/buscadorTurnosPrestaciones/services/turnos-prestaciones.service.ts
@@ -1,14 +1,32 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest } from 'rxjs';
 import { Server } from '@andes/shared';
 import { ITurnosPrestaciones } from '../interfaces/turnos-prestaciones.interface';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class TurnosPrestacionesService {
 
     private turnosPrestacionesURL = '/modules/estadistica/turnos_prestaciones';  // URL to web api
 
-    constructor(private server: Server) { }
+    public prestacionesOrdenada$: Observable<any[]>;
+    public prestacionesFiltrada$ = new BehaviorSubject<any[]>(null);
+
+    public sortBy$ = new BehaviorSubject<string>('fecha'); // Seteo con fecha para que el primer orden sea por fecha
+    public sortOrder$ = new BehaviorSubject<string>(null);
+
+
+    constructor(private server: Server) {
+        this.prestacionesOrdenada$ = combineLatest(
+            this.prestacionesFiltrada$,
+            this.sortBy$,
+            this.sortOrder$
+        ).pipe(
+            map(([prestaciones, sortBy, sortOrder]) =>
+                this.sortPrestaciones(prestaciones, sortBy, sortOrder)
+            )
+        );
+    }
 
     /**
      * Metodo get.
@@ -16,5 +34,37 @@ export class TurnosPrestacionesService {
      */
     get(params: any): Observable<ITurnosPrestaciones[]> {
         return this.server.get(this.turnosPrestacionesURL, { params: params, showError: true });
+    }
+
+
+
+    sortPrestaciones(prestaciones, sortBy: string, sortOrder: string) {
+        if (sortOrder === 'desc') {
+            prestaciones = prestaciones.reverse();
+        } else {
+            switch (sortBy) {
+                case 'fecha':
+                    prestaciones = prestaciones.sort((a, b) => a.fecha.getTime() - b.fecha.getTime());
+                    break;
+                case 'documento':
+                    prestaciones = prestaciones.sort((a, b) => (!a.paciente) ? 1 : (!b.paciente) ? -1 : a.paciente.documento.localeCompare((b.paciente.documento as string)));
+                    break;
+                case 'paciente':
+                    prestaciones = prestaciones.sort((a, b) => (!a.paciente) ? 1 : (!b.paciente) ? -1 : a.paciente.apellido.localeCompare((b.paciente.apellido as string)));
+                    break;
+                case 'prestacion':
+                    prestaciones = prestaciones.sort((a, b) => (!a.prestacion) ? 1 : (!b.prestacion) ? -1 : a.prestacion.term.localeCompare((b.prestacion.term as string)));
+                    break;
+                case 'profesional':
+                    prestaciones = prestaciones.sort((a, b) => (!a.profesionales[0]) ? 1 : (!b.profesionales[0]) ? -1 : a.profesionales[0].apellido.localeCompare((b.profesionales[0].apellido as string)));
+                    break;
+                case 'estado':
+                    prestaciones = prestaciones.sort((a, b) => (!a.estado) ? 1 : (!b.estado) ? -1 : a.estado.localeCompare((b.estado as string)));
+                    break;
+                default:
+                    break;
+            }
+        }
+        return prestaciones;
     }
 }
