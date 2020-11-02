7ce89325215f874d38c52665c9270cebb21a2582
Lautaro Molina <37875706+negro89@users.noreply.github.com>
2020-11-02 15:00:16 -0300

    feat(MPI): agrega funcionalidad a pipe edad (#1981)

--- projects/shared/src/lib/pipes/edad.pipe.ts
+++ projects/shared/src/lib/pipes/edad.pipe.ts
@@ -6,7 +6,7 @@ const moment = moment_;
 // pure: false - Info: https://stackoverflow.com/questions/34456430/ngfor-doesnt-update-data-with-pipe-in-angular2
 export class EdadPipe implements PipeTransform {
     transform(value: any): any {
-        const fechaActual = moment();
+        const fechaLimite = value.fechaFallecimiento ? moment(value.fechaFallecimiento) : moment();
         let fechaNac: any;
         let difAnios: number;
         let difDias: number;
@@ -15,11 +15,11 @@ export class EdadPipe implements PipeTransform {
         let difMn: number;
 
         fechaNac = moment(value.fechaNacimiento, 'YYYY-MM-DD HH:mm:ss');
-        difDias = fechaActual.diff(fechaNac, 'd'); // Diferencia en días
+        difDias = fechaLimite.diff(fechaNac, 'd'); // Diferencia en días
         difAnios = Math.floor(difDias / 365.25);
         difMeses = Math.floor(difDias / 30.4375);
-        difHs = fechaActual.diff(fechaNac, 'h'); // Diferencia en horas
-        difMn = fechaActual.diff(fechaNac, 'm'); // Diferencia en minutos
+        difHs = fechaLimite.diff(fechaNac, 'h'); // Diferencia en horas
+        difMn = fechaLimite.diff(fechaNac, 'm'); // Diferencia en minutos
 
         if (difAnios !== 0) {
             return `${String(difAnios)} años`;
@@ -28,7 +28,7 @@ export class EdadPipe implements PipeTransform {
         } else if (difDias !== 0) {
             return `${String(difDias)} días`;
         } else if (difHs !== 0) {
-            return `${String(difHs)} horas`;
+            return `${String(difHs)} horas y ${difMn - (difHs * 60)} minutos`;
         } else if (difMn !== 0) {
             return `${String(difMn)} minutos`;
         }
