5704bdf00ec569a99be78b44b2b754ffe3e21a42
Mariano Andres Botta <marianoabotta@gmail.com>
2020-12-15 11:51:51 -0300

    fix(rup): adjuste evoluciones

--- src/app/modules/rup/components/huds/vistaProcedimiento.ts
+++ src/app/modules/rup/components/huds/vistaProcedimiento.ts
@@ -19,16 +19,35 @@ export class VistaProcedimientoComponent implements OnInit {
     @Input() prestacion: IPrestacion;
     @Input() evolucionActual: any;
     @Input() indice = 0;
+
     elementoRUP: IElementoRUP;
     public relacionAux;
-    constructor(public prestacionesService: PrestacionesService, public elementosRUPService: ElementosRUPService) { }
+
+    constructor(
+        public prestacionesService: PrestacionesService,
+        public elementosRUPService: ElementosRUPService
+    ) { }
 
     ngOnInit() {
         this.prestacionesService.getByPaciente(this.paciente.id).subscribe(prestacion => {
             this.prestacion = prestacion.find(x => x.id === this.registro.idPrestacion);
+            this.buscarRelacion();
         });
-        if (this.registro.evoluciones[0]?.relacionadoCon[0]?.concepto.conceptId === '1921000013108') {
-            this.relacionAux = this.registro.evoluciones[0].relacionadoCon[0];
+    }
+
+    buscarRelacion() {
+        const relacionInvertida = this.prestacion.ejecucion.registros.find(r => {
+            return r.relacionadoCon.some(rl => rl.concepto.conceptId === this.registro.concepto.conceptId);
+        });
+
+        if (relacionInvertida) {
+            this.relacionAux = relacionInvertida;
+        } else {
+            if (this.registro.evoluciones[this.indice]?.relacionadoCon[0]?.concepto.conceptId === '1921000013108') {
+                this.relacionAux = this.registro.evoluciones[this.indice].relacionadoCon[0];
+            } else {
+                this.relacionAux = null;
+            }
         }
     }
 
