8d0f83c2a1c7ea23a5026ea4c098a2856e6ae887
Natalia <natalia.huenchuman@gmail.com>
2021-01-19 12:43:29 -0300

    fix(rup): cambia check Dosis

--- src/app/modules/rup/components/elementos/vacunas.component.ts
+++ src/app/modules/rup/components/elementos/vacunas.component.ts
@@ -149,7 +149,7 @@ export class VacunasComponent extends RUPComponent implements OnInit {
         if (this.registro.valor.vacuna.dosis && this.vacunasEncontradas && this.vacunasEncontradas.length) {
             const ultimoRegistro = this.vacunasEncontradas[0];
             const tiempoInterdosis = this.registro.valor.vacuna.dosis.tiempoInterdosis;
-            let diasdiferencia = this.prestacion.ejecucion.fecha.getTime() - ultimoRegistro.fechaAplicacion.getTime();
+            let diasdiferencia = this.registro.valor.vacuna.fechaAplicacion.getTime() - ultimoRegistro.fechaAplicacion.getTime();
             let contdias = Math.round(diasdiferencia / (1000 * 60 * 60 * 24));
             if (contdias < tiempoInterdosis) {
                 this.plex.info('danger', 'No se cumple el tiempo interdosis', 'Problemas con la dosis seleccionada');
