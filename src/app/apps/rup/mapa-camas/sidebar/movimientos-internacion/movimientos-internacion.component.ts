dcb67dd77b21ed7b308286564aaedf813d61be2d
Mariano Andres Botta <marianoabotta@gmail.com>
2021-02-11 08:23:32 -0300

    feat(mapa-camas): varios ajustes

--- src/app/apps/rup/mapa-camas/sidebar/movimientos-internacion/movimientos-internacion.component.ts
+++ src/app/apps/rup/mapa-camas/sidebar/movimientos-internacion/movimientos-internacion.component.ts
@@ -1,31 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MapaCamasService } from '../../services/mapa-camas.service';
-import { IPrestacion } from '../../../../../modules/rup/interfaces/prestacion.interface';
-import { Observable, Subject, Subscription, combineLatest } from 'rxjs';
-import { startWith, map, switchMap } from 'rxjs/operators';
-import { ISnapshot } from '../../interfaces/ISnapshot';
+import { Observable, Subject, combineLatest, of } from 'rxjs';
+import { startWith, map, auditTime, catchError, takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'app-movimientos-internacion',
     templateUrl: './movimientos-internacion.component.html',
 })
 
-export class MovimientosInternacionComponent implements OnInit {
-    public historial = new Subject();
+export class MovimientosInternacionComponent implements OnInit, OnDestroy {
+    public fechasSeleccionadas = new Subject();
 
     public historial$: Observable<any>;
 
     public desde = moment(this.mapaCamasService.fecha).subtract(7, 'd').toDate();
     public hasta = this.mapaCamasService.fecha;
 
+    onDestroy$ = new Subject();
+
     constructor(
         private mapaCamasService: MapaCamasService,
     ) { }
 
+    getFechasInternacion() {
+        const capa = this.mapaCamasService.capa2.getValue();
+        if (capa === 'estadistica') {
+            return this.mapaCamasService.prestacion$.pipe(
+                map(prestacion => {
+                    return {
+                        desde: prestacion.ejecucion.registros[0].valor.informeIngreso.fechaIngreso,
+                        hasta: prestacion.ejecucion.registros[1]?.valor.InformeEgreso?.fechaEgreso || new Date()
+                    };
+                }),
+                catchError(() => of(null))
+            );
+        } else {
+            return this.mapaCamasService.resumenInternacion$.pipe(
+                map(prestacion => {
+                    return {
+                        desde: prestacion.fechaIngreso,
+                        hasta: prestacion.fechaEgreso || new Date()
+                    };
+                }),
+                catchError(() => of(null))
+            );
+        }
+    }
+
+    ngOnDestroy() {
+        this.onDestroy$.next();
+        this.onDestroy$.complete();
+    }
+
+
     ngOnInit() {
         this.onChange();
 
-        const fechaPipe = this.historial.pipe(
+        this.getFechasInternacion().pipe(
+            takeUntil(this.onDestroy$),
+            auditTime(0)
+        ).subscribe((datos: any) => {
+            if (datos) {
+                this.fechasSeleccionadas.next(datos);
+                this.desde = datos.desde;
+                this.hasta = datos.hasta;
+            }
+        });
+
+        const fechaPipe = this.fechasSeleccionadas.pipe(
             startWith({
                 desde: this.desde, hasta: this.hasta
             }),
@@ -37,13 +79,16 @@ export class MovimientosInternacionComponent implements OnInit {
             })
         );
 
-        this.historial$ = combineLatest(
+        this.historial$ = combineLatest([
             this.mapaCamasService.historialInternacion$,
             fechaPipe
-        ).pipe(
+        ]).pipe(
             map(([movimientos, { desde, hasta }]) => {
                 return movimientos.filter((mov) => {
                     return desde.getTime() < mov.fecha.getTime() && mov.fecha.getTime() <= hasta.getTime();
+                }).map(mov => {
+                    mov.sectorName = [...mov.sectores].reverse().map(s => s.nombre).join(', ');
+                    return mov;
                 });
             })
         );
@@ -61,7 +106,7 @@ export class MovimientosInternacionComponent implements OnInit {
             const fechaHastaValida = (this.hasta <= moment().toDate() && this.hasta >= this.desde);
 
             if (fechaDesdeValida && fechaHastaValida) {
-                this.historial.next(filtros);
+                this.fechasSeleccionadas.next(filtros);
             }
         }
     }
