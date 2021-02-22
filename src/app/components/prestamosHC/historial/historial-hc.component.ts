d905a09a5b5184f55ca7418e8ca8362bc09088b1
Lautaro Molina <negro89_nqn@hotmail.com>
2021-02-22 15:26:16 -0300

    fix/refactor(mpi): filtro prestaciones y componentes plex

--- src/app/components/prestamosHC/historial/historial-hc.component.ts
+++ src/app/components/prestamosHC/historial/historial-hc.component.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PrestamosService } from './../../../services/prestamosHC/prestamos-hc.service';
 import { Auth } from '@andes/auth';
-import { Plex } from '@andes/plex';
-import * as moment from 'moment';
-
 
 @Component({
     selector: 'app-historial-carpetas',
@@ -11,45 +8,39 @@ import * as moment from 'moment';
     styleUrls: ['../prestamos-hc.scss']
 })
 
-export class HistorialCarpetasComponent implements OnInit {
-    public historial: any[] = [];
+export class HistorialCarpetasComponent {
+    public historial = null;
     public numeroCarpeta = '';
-    public inicioBusqueda = false;
     public paciente;
+    public loading = false;
 
     constructor(
         public prestamosService: PrestamosService,
         public auth: Auth
-    ) {
-    }
-
-    ngOnInit() {
-    }
+    ) { }
 
     buscarHistorial() {
-        this.paciente = null;
-        this.historial = [];
-        this.inicioBusqueda = true;
-        if (this.numeroCarpeta) {
-            this.prestamosService.getHistorialCarpetas({ numero: this.numeroCarpeta, organizacion: this.auth.organizacion.id })
-                .subscribe(resultado => {
-                    if (resultado?.historial?.length > 0) {
-                        this.historial = resultado.historial;
-                    } else {
-                        this.historial = [];
-                    }
-
-                    if (resultado.paciente) {
-                        this.paciente = resultado.paciente;
-                    }
-                });
-        } else {
-            this.inicioBusqueda = false;
+        if (this.numeroCarpeta.length) {
+            this.loading = true;
+            this.paciente = null;
+            if (this.numeroCarpeta) {
+                this.prestamosService.getHistorialCarpetas({ numero: this.numeroCarpeta, organizacion: this.auth.organizacion.id })
+                    .subscribe(resultado => {
+                        this.loading = false;
+                        if (resultado?.historial?.length > 0) {
+                            this.historial = resultado.historial;
+                        } else {
+                            this.historial = [];
+                        }
+                        if (resultado.paciente) {
+                            this.paciente = resultado.paciente;
+                        }
+                    });
+            }
         }
     }
 
     loadCarpetas(event) {
-        let listaProfesionales = [];
         if (event.query) {
             let query = {
                 nombreCompleto: event.query
