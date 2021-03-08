bbdf18e5bef08a5cb4ef4169e7e2135d5a5d9df2
waltergarrido <blackwalter7@gmail.com>
2021-03-08 15:19:02 -0300

    (PDP): login

--- projects/portal/src/app/app-routing.module.ts
+++ projects/portal/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RoutingGuard, RoutingNavBar } from 'src/app/app.routings-guard.class';
 import { HomeComponent } from './home/home.component';
-
+import { LoginComponent } from './login/login-portal-paciente';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
 ];
 
