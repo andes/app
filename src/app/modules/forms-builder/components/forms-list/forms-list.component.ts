a7f48998f5ceb719c855759a104803b4fcc55181
plammel <lammelpablo@gmail.com>
2021-03-18 15:15:21 -0300

    MISC: permiso create y update en form builder

--- src/app/modules/forms-builder/components/forms-list/forms-list.component.ts
+++ src/app/modules/forms-builder/components/forms-list/forms-list.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { FormsService, Form } from '../../services/form.service';
 import { Observable } from 'rxjs';
 import { cache } from '@andes/shared';
+import { Auth } from '@andes/auth';
+import { Router } from '@angular/router';
 @Component({
     selector: 'forms-list',
     templateUrl: './forms-list.component.html'
 })
 export class FormsListComponent implements OnInit {
+    canCreate: Boolean;
+    canUpdate: Boolean;
+
     forms$: Observable<Form[]>;
 
     public columns = [
@@ -35,8 +40,14 @@ export class FormsListComponent implements OnInit {
             opcional: true
         }
     ];
-    constructor(private formsService: FormsService) { }
+    constructor(private formsService: FormsService, private auth: Auth, private router: Router) { }
     ngOnInit() {
+        if (!this.auth.getPermissions('formBuilder:?').length) {
+            this.router.navigate(['inicio']);
+        }
+        this.canCreate = this.auth.check('formBuilder:create');
+        this.canUpdate = this.auth.check('formBuilder:update');
+
         this.forms$ = this.formsService.search().pipe(cache());
     }
 }
