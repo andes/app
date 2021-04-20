f495453e9edfc011358587882b8983c686c88248
juliotero <juli.otero01@gmail.com>
2021-04-20 08:13:19 -0300

    VAC: Cambio de distribución en datos de inscripción en sidebar (#2220)
    * feat(vac): poder editar datos basicos si notiene paciente asociado
    
    * refactor(vac): cambio en distribución de datos de inscripción en el sidebar

--- src/app/apps/vacunacion/components/detalle-inscripcion.component.ts
+++ src/app/apps/vacunacion/components/detalle-inscripcion.component.ts
@@ -1,16 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { GrupoPoblacionalService } from './../../../services/grupo-poblacional.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
     selector: 'detalle-inscripcion',
     templateUrl: 'detalle-inscripcion.html'
 })
 
-export class DetalleInscripcionComponent {
+export class DetalleInscripcionComponent implements OnInit {
     public inscripcion: any;
+    public gruposPoblacionales: any[];
 
     @Input('inscripcion')
     set _inscripcion(value) {
         this.inscripcion = value;
+    }
+
+    constructor(private gruposService: GrupoPoblacionalService) { }
+
+    ngOnInit() {
+        this.gruposService.search().subscribe(resp => {
+            this.gruposPoblacionales = resp;
+        });
+    }
+
+
+    grupoPoblacional(nombre: string) {
+        let descripcion;
+        if (this.gruposPoblacionales) {
+            descripcion = this.gruposPoblacionales.find(item => item.nombre === nombre).descripcion;
+        }
+        return descripcion;
     }
 
 }
