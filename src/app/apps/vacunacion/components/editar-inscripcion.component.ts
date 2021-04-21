b71d51eab97b340dacee72b1563b41ca0cb72ada
condorpiedra <natalia.huenchuman@gmail.com>
2021-04-21 15:33:19 -0300

    feat(inscripcion): permite modificar localidad (#2231)
    * feat(inscripcion): permite modificar localidad
    
    * ref(vac): correcciones
    
    Co-authored-by: Lautaro Molina <negro89_nqn@hotmail.com>

--- src/app/apps/vacunacion/components/editar-inscripcion.component.ts
+++ src/app/apps/vacunacion/components/editar-inscripcion.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GrupoPoblacionalService } from 'src/app/services/grupo-poblacional.service';
-import { cache } from '@andes/shared';
 import { InscripcionService } from '../services/inscripcion.service';
+import { cache } from '@andes/shared';
 import { Plex } from '@andes/plex';
 import { Auth } from '@andes/auth';
+import { LocalidadService } from 'src/app/services/localidad.service';
 
 @Component({
     selector: 'editar-inscripcion',
@@ -25,10 +26,11 @@ export class EditarInscripcionComponent implements OnInit {
     public nuevoEstado;
     public opcionesGrupos$: Observable<any>;
     public permisosEdicion;
+    public localidades$: Observable<any>;
 
     @Input('inscripcion')
     set _inscripcion(value) {
-        this.inscripcion = value;
+        this.inscripcion = { ...value };
         const validadoDomicilio = this.inscripcion.validaciones.includes('domicilio');
         if (this.inscripcion.grupo.nombre === 'mayores60' && !this.inscripcion.paciente) {
             this.permiteCambioFechaNacimiento = true;
@@ -48,10 +50,14 @@ export class EditarInscripcionComponent implements OnInit {
     constructor(
         private grupoPoblacionalService: GrupoPoblacionalService,
         private inscripcionService: InscripcionService,
+        private localidadService: LocalidadService,
         public plex: Plex,
         private auth: Auth
     ) {
         this.opcionesGrupos$ = this.grupoPoblacionalService.search({ nombre: ['discapacidad', 'factores-riesgo'] }).pipe(
+            cache()
+        );
+        this.localidades$ = this.localidadService.get({ codigo: 15 }).pipe(
             cache()
         );
     }
