aaf6c1a0dbd0b21623cf5d9270f3b224333b5b51
waltergarrido <blackwalter7@gmail.com>
2021-05-17 11:31:43 -0300

    feat(PACS-22):vista mapa agendas por semana

--- src/app/components/turnos/gestor-agendas/mapa-agendas/mapa-agendas.component.ts
+++ src/app/components/turnos/gestor-agendas/mapa-agendas/mapa-agendas.component.ts
@@ -2,7 +2,8 @@ import { Router } from '@angular/router';
 import { Auth } from '@andes/auth';
 import { Component, OnInit } from '@angular/core';
 import { AgendaService } from 'src/app/services/turnos/agenda.service';
-import { FechaPipe } from 'projects/shared/src/lib/pipes/fecha.pipe';
+import { ConceptosTurneablesService } from 'src/app/services/conceptos-turneables.service';
+
 @Component({
     selector: 'mapa-agenda',
     templateUrl: 'mapa-agendas.component.html',
@@ -10,89 +11,121 @@ import { FechaPipe } from 'projects/shared/src/lib/pipes/fecha.pipe';
 })
 export class MapaAgendasComponent implements OnInit {
 
-    public headers = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+    public headers = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
     public semana;
     public horaInicio = 6;
     public horaFin = 20;
     public horarios = [];
     public filasPorHorario;
-    public colorPrestacion = '#00FFFF';
     public parametros;
     public intervalo = 30;
+    public diaInicio;
     public agendasDeLaSemana = [];
+    public prestacionesPermisos;
     constructor(
 
-        private agendaService: AgendaService
+        private agendaService: AgendaService,
+        private auth: Auth,
+        private conceptoTurneablesService: ConceptosTurneablesService
 
     ) { }
 
     ngOnInit() {
+        this.prestacionesPermisos = this.auth.getPermissions('turnos:planificarAgenda:prestacion:?');
+
+        this.turnosPorHora();
+        this.cargarSemana();
+
+    }
+
+    private cargarSemana() {
 
         this.horarios = this.generarArregloHorarios();
-        this.filasPorHorario = Array(2).fill(30).map((x, i) => i);
+        this.diaInicio = moment(this.diaInicio).startOf('week');
+        this.filasPorHorario = this.turnosPorHora();
         this.semana = this.headers.map((dia, index) => {
             return {
                 nombre: dia,
-                fecha: moment().weekday(index).toDate(),
+                fecha: this.diaInicio.weekday(index).toDate(),
                 horarios: this.generarArregloHorarios()
 
             };
 
         });
-
         this.cargarTabla();
-
     }
 
-
-
-
     private cargarTabla() {
-        // solo busco las agendas de la semana en trasnscurso
+
         this.parametros = {
-            fechaDesde: moment().startOf('week').toDate(),
-            fechaHasta: moment().endOf('week').toDate(),
+            fechaDesde: moment(this.diaInicio).startOf('week').toDate(),
+            fechaHasta: moment(this.diaInicio).endOf('week').toDate(),
             organizacion: '',
             idTipoPrestacion: '',
             idProfesional: '',
             espacioFisico: '',
             otroEspacioFisico: '',
-            estado: '',
-            skip: 0,
-            limit: 15
+            estado: ''
         };
-
-        this.agendaService.get(this.parametros).subscribe((agendas: any) => {
-            agendas.forEach(agenda => {
-                this.agendasDeLaSemana.push(agenda);
-            });
-            this.semana.forEach((dia, index) => this.agendasDeLaSemana.filter(agenda => {
-                if (moment(dia.fecha).isSame(agenda.horaInicio, 'day')) {
-                    let turnos = agenda.bloques[0].turnos.filter(turno => turno.estado === 'asignado');
-                    if (turnos) {
-                        turnos.map(turno => dia.horarios.filter((hora, i) => {
-                            this.filasPorHorario.map((n, aux) => {
-                                let horaTurno = moment(turno.horaInicio);
-                                let horaInicial = moment(dia.fecha).hour(hora.hora).minute(this.intervalo * aux);
-                                let intervalo = moment(dia.fecha).hour(hora.hora).minute(this.intervalo * (aux + 1));
-                                if (horaTurno.isBetween(horaInicial, intervalo, 'hours', '[)')) {
-                                    this.semana[index].horarios[i].intervalo.push(turno);
-                                }
-
-                            });
-
-
-                        }));
-                    }
-
-                }
-
-            }));
-
-        }, err => {
-            if (err) {
+        if (this.prestacionesPermisos.length > 0 && this.prestacionesPermisos[0] !== '*') {
+            this.parametros['tipoPrestaciones'] = this.prestacionesPermisos;
+        }
+        this.conceptoTurneablesService.getAll().subscribe((data) => {
+            let dataF;
+            if (this.prestacionesPermisos[0] === '*') {
+                dataF = data;
+            } else {
+                dataF = data.filter((x) => { return this.prestacionesPermisos.indexOf(x.id) >= 0; });
             }
+
+            this.agendaService.get(this.parametros).subscribe((agendas: any) => {
+                agendas.forEach(agenda => {
+
+                    this.semana.forEach(dia => {
+
+                        if (moment(dia.fecha).isSame(agenda.horaInicio, 'day')) {
+
+                            let turnos = agenda.bloques[0].turnos.filter(turno => turno.estado === 'asignado');
+                            turnos.forEach(t => dataF.forEach(tipoPrestacion => {
+
+                                if (tipoPrestacion.color && t.tipoPrestacion.conceptId === tipoPrestacion.conceptId) {
+
+                                    t.tipoPrestacion['color'] = tipoPrestacion.color;
+
+                                }
+                            }));
+
+                            turnos.forEach(turno => dia.horarios.forEach(hora => {
+
+                                hora.intervalos.forEach((intervalo, aux) => {
+
+                                    let horaTurno = moment(turno.horaInicio);
+                                    let horaInicial = moment(dia.fecha).hour(hora.hora).minute(this.intervalo * aux);
+                                    let horaFin = moment(dia.fecha).hour(hora.hora).minute(this.intervalo * (aux + 1));
+
+                                    if (horaTurno.isBetween(horaInicial, horaFin, null, '[)')) {
+
+                                        intervalo.turnos.push(turno);
+
+                                    }
+
+                                });
+
+
+                            }));
+
+
+                        }
+
+                    });
+                });
+
+            }, err => {
+                if (err) {
+                }
+            });
         });
+
     }
 
     private generarArregloHorarios() {
@@ -101,9 +134,20 @@ export class MapaAgendasComponent implements OnInit {
 
             horarios.push({
                 hora: this.horaInicio + index,
-                intervalo: []
+                intervalos: this.turnosPorHora()
             });
         }
         return horarios;
+    }
+
+
+    private turnosPorHora() {
+        let cantidadTurnosHora = 60 / this.intervalo;
+        let horarioTurnos = [];
+        for (let index = 0; index < cantidadTurnosHora; index++) {
+            horarioTurnos.push({ turnos: [] });
+
+        }
+        return horarioTurnos;
     }
 }
