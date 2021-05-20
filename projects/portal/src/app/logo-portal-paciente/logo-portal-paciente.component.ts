b3f02cd8009e8ebc3c7c7c2648e9a0f503280e36
Andrés Boc-Ho <andresin@gmail.com>
2021-05-20 09:46:42 -0300

    chore: lint

--- projects/portal/src/app/logo-portal-paciente/logo-portal-paciente.component.ts
+++ projects/portal/src/app/logo-portal-paciente/logo-portal-paciente.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-logo-portal-paciente',
+  selector: 'pdp-logo',
   templateUrl: './logo-portal-paciente.component.html',
   styleUrls: ['./logo-portal-paciente.component.scss']
 })
