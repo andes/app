9556b56fe7d1705f18f92efda7d2a3eeeef34aab
mariano <marianocampe94@gmail.com>
2021-06-03 15:34:43 -0300

    refactor(VAC): Mostrar las vacunas junto con sus lotes y agregar un nuevo lote

--- src/app/apps/vacunacion/components/lote/lote.component.ts
+++ src/app/apps/vacunacion/components/lote/lote.component.ts
@@ -13,18 +13,13 @@ export class LoteComponent implements OnInit {
   public showSidebar = false;
   public vacunas$: Observable<any[]>;
   public lotes$: Observable<any[]>;
-  public lote;
+  public lotes2$: Observable<any[]>;
+  public loteEncontrado;
   public lote2;
   public vacunasEncontradas;
   public creando = false;
-  public columns = [
-    {
-      key: 'nombre',
-      label: 'Nombre',
-      sorteable: true,
-      opcional: false
-    }
-  ];
+  public seEncuentra = false;
+
   public loteSelected =
     {
       codigo: '',
@@ -39,17 +34,17 @@ export class LoteComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.vacunas$ = this.vacunasService.getNomivacVacunas({ habilitado: true, calendarioNacional: 'NO', sort: 'nombre' }).pipe(
-      map(vacunas => {
-        return vacunas.map((v) => {
-          return { _id: v._id, codigo: v.codigo, nombre: v.nombre, snomed_conceptId: v.snomed_conceptId };
-        });
-      })
-    );
+    this.vacunas$ = this.vacunasService.getNomivacVacunas({
+      habilitado: true,
+      calendarioNacional: 'NO',
+      sort: 'nombre'
+    });
   }
 
   showInSideBar(vacuna) {
     if (vacuna) {
+      this.limpiarForm();
+      vacuna.id = vacuna._id;
       this.loteSelected.vacuna = vacuna;
       this.vacunaSelected = vacuna;
       this.showSidebar = true;
@@ -58,15 +53,11 @@ export class LoteComponent implements OnInit {
   }
 
   loadLote() {
-    this.lote = null;
-    this.lotes$ = this.vacunasService.getNomivacLotes({ habilitado: true, sort: 'codigo' }).pipe(
-      map(lot => {
-        if (this.vacunaSelected) {
-          this.lote = lot.filter(l => (this.vacunaSelected.codigo === l.vacuna.codigo));
-          return this.lote;
-        }
-      })
-    );
+    this.lotes$ = this.vacunasService.getNomivacLotes({
+      habilitado: true,
+      vacuna: this.vacunaSelected._id,
+      sort: 'codigo'
+    });
   }
 
   closeSidebar() {
@@ -79,22 +70,28 @@ export class LoteComponent implements OnInit {
 
   cerrar() {
     this.creando = false;
+    this.limpiarForm();
+  }
+
+  limpiarForm() {
+    this.loteSelected.codigo = '';
+    this.loteSelected.descripcion = '';
   }
 
   guardar() {
-
-    this.lote2 = this.lote.find(l => (this.loteSelected.codigo.toUpperCase() === l.codigo.toUpperCase()));
-    if (this.lote2 === undefined) {
-      this.vacunasService.guardarNomivacLotes(this.loteSelected).subscribe(resultado => {
-        if (resultado) {
-          this.plex.toast('success', 'Lote creado con éxito.');
-          this.cerrar();
-        } else {
-          this.plex.toast('danger', 'A ocurrido un error y no se pudo crear el nuevo lote, intentelo nuevamente.');
-        }
-      });
-    } else {
-      this.plex.toast('danger', 'El lote ya existe.');
-    }
+    this.seEncuentra = false;
+    this.lotes$.subscribe(lotes => {
+      this.seEncuentra = lotes.some(lote => lote.codigo.toUpperCase() === this.loteSelected.codigo.toUpperCase());
+      if (this.seEncuentra) {
+        this.plex.toast('danger', 'El lote que esta creando ya existe.');
+      } else {
+        this.vacunasService.guardarNomivacLotes(this.loteSelected).subscribe(resultado => {
+          if (resultado) {
+            this.plex.toast('success', 'Lote creado con éxito.');
+            this.cerrar();
+          }
+        });
+      }
+    });
   }
 }
