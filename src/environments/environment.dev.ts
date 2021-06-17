e1e64cf24f4f224fc310539bfbd8f489b1e34a3d
Mariano Andres Botta <marianoabotta@gmail.com>
2021-06-17 15:22:05 -0300

    chore(env): change dev url

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ import { apiKeys, analytics, hotjar, password_recovery, captcha } from './apiKey
 export const environment = {
   production: false,
   environmentName: 'development',
-  API: 'https://demo.andes.gob.ar/api',
+  API: '//localhost:3002/api',
   WS: '//localhost:3002',
   APIStatusCheck: false,
   version: _package.version,
