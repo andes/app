62e16fb5fa5d8b05eff37984fca0ceb93e4cf2b4
condorpiedra <natalia.huenchuman@gmail.com>
2021-07-20 11:04:06 -0300

    fix(inscripcion): modifica rango edad de FR (#2376)

--- src/app/apps/vacunacion/components/listado-inscriptos.component.ts
+++ src/app/apps/vacunacion/components/listado-inscriptos.component.ts
@@ -163,7 +163,7 @@ export class ListadoInscriptosVacunacionComponent implements OnInit {
             switch (grupo.nombre) {
                 case 'discapacidad':
                     this.fechaMinimaNacimiento = moment('1900-01-01').toDate();
-                    this.fechaMaximaNacimiento = moment().subtract(18, 'years').toDate();
+                    this.fechaMaximaNacimiento = moment().subtract(12, 'years').toDate();
                     break;
                 case 'mayores60':
                     this.fechaMinimaNacimiento = moment('1900-01-01').toDate();
@@ -176,7 +176,7 @@ export class ListadoInscriptosVacunacionComponent implements OnInit {
                     break;
                 case 'factores-riesgo': {
                     this.fechaMinimaNacimiento = moment().subtract(60, 'years').toDate();
-                    this.fechaMaximaNacimiento = moment().subtract(18, 'years').toDate();
+                    this.fechaMaximaNacimiento = moment().subtract(12, 'years').toDate();
                     break;
                 }
             }
