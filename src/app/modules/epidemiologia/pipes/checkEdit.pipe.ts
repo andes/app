5319572baeefc3c9a4a025a3a626575f1e466e39
Pancho <martinebucarey@gmail,com>
2021-09-06 10:01:34 -0300

    fix(checkEditPipe): uso de auth

--- src/app/modules/epidemiologia/pipes/checkEdit.pipe.ts
+++ src/app/modules/epidemiologia/pipes/checkEdit.pipe.ts
@@ -2,21 +2,19 @@ import { Auth } from '@andes/auth';
 import { Pipe, PipeTransform } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { PermisosService } from 'src/app/apps/gestor-usuarios/services/permisos.service';
 
 @Pipe({
     name: 'checkEdit'
 })
 export class CheckEditPipe implements PipeTransform {
     constructor(
-        private auth: Auth,
-        private permisosService: PermisosService
+        private auth: Auth
     ) { }
 
     transform(ficha: any): Observable<boolean> {
         if (this.auth.check('epidemiologia:update')) {
-            return this.permisosService.organizaciones().pipe(
-                map(permisos => permisos.some(org => org.id === ficha.secciones[0].fields[0].organizacion?.id))
+            return this.auth.organizaciones().pipe(
+                map(organizacion => organizacion.some(org => org.id === ficha.secciones[0].fields[0].organizacion?.id))
             );
         } else {
             return of(false);
