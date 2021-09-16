253a612826ee90849393de7ffb1c418d9273a1e3
Lautaro Molina <37875706+negro89@users.noreply.github.com>
2021-09-16 09:22:01 -0300

    PDP - Visualizacion de turnos pendientes (#2437)
    * ref(PDP): Visualizacion de turnos pendientes
    
    * correcciones

--- projects/portal/src/app/pages/mis-turnos/mis-turnos.component.ts
+++ projects/portal/src/app/pages/mis-turnos/mis-turnos.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { TurnoService } from '../../services/turno.service';
 import { ActivatedRoute, Router } from '@angular/router';
-
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
+import { cache } from '@andes/shared';
 
 @Component({
     selector: 'pdp-mis-turnos',
@@ -15,6 +14,7 @@ export class PDPMisTurnosComponent implements OnInit {
     public width: number;
     public turnos$: Observable<any>;
     public showTurnosDisponibles = false;
+    public showProximos;
 
     constructor(
         private turnoService: TurnoService,
@@ -23,8 +23,9 @@ export class PDPMisTurnosComponent implements OnInit {
         private el: ElementRef) { }
 
     ngOnInit(): void {
-        this.turnos$ = this.turnoService.getTurnos().pipe(
-            map(turnos => this.sortTurnos(turnos))
+        this.turnos$ = this.turnoService.turnosFiltrados$.pipe(
+            map(turnos => this.sortTurnos(turnos)),
+            cache()
         );
     }
 
@@ -58,5 +59,14 @@ export class PDPMisTurnosComponent implements OnInit {
 
     turnosDisponibles(agenda) {
         this.router.navigate(['dar-turno-detalle', agenda.idAgenda, agenda.prestacion._id], { relativeTo: this.activeRoute });
+    }
+
+    /**
+     * Filtra los turnos con respecto a la fecha actual
+     * @param {boolean} mostrarProximos define si filtramos por proximos o historicos
+     */
+    filtrarTurnos(mostrarProximos = false){
+        this.showProximos = mostrarProximos;
+        this.turnoService.filtroProximos.next(mostrarProximos);
     }
 }
