e064baa4a1b0698a6367b34d456a5f76b39040e7
palita1991 <marianopalavecino7@gmail.com>
2021-09-29 11:55:29 -0300

    feat(PER):control para que en alertas solo traiga las correspondientes al efector logueado

--- src/app/modules/perinatal/components/alertas-perinatal.component.ts
+++ src/app/modules/perinatal/components/alertas-perinatal.component.ts
@@ -1,24 +1,24 @@
+import { Auth } from '@andes/auth';
 import { CarnetPerinatalService } from './../services/carnet-perinatal.service';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-
 @Component({
     selector: 'alertas-perinatal',
     templateUrl: './alertas-perinatal.component.html'
 })
 export class AlertasPerinatalComponent implements OnInit {
-    public listado$: Observable<any[]>;
-
-    constructor(private carnetPerinatalService: CarnetPerinatalService) { }
+    public listado: any[] = [];
+    constructor(private carnetPerinatalService: CarnetPerinatalService, public auth: Auth) { }
 
     ngOnInit(): void {
-        this.listado$ = this.carnetPerinatalService.carnetsFiltrados$.pipe(
-            map(resp => {
-                return resp.filter(item => (moment().diff(moment(item.fechaProximoControl), 'days') >= 1) && !item.fechaFinEmbarazo);
-            })
-        );
+        const params: any = {
+            organizacion: this.auth.organizacion.id
+        };
+
+        this.carnetPerinatalService.search(params).subscribe(resultado => {
+            this.listado = resultado.filter(item => (moment().diff(moment(item.fechaProximoControl), 'days') >= 1) && !item.fechaFinEmbarazo);
+        });
     }
+
+    get hayItems() { return this.listado.length; }
 
 }
