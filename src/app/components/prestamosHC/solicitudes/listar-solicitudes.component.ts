0762da0040048498888753e4fae9a92f383dedd0
waltergarrido <blackwalter7@gmail.com>
2021-09-30 09:50:43 -0300

    fix(HUDS):error al cargar dos historias clínicas el mismo día

--- src/app/components/prestamosHC/solicitudes/listar-solicitudes.component.ts
+++ src/app/components/prestamosHC/solicitudes/listar-solicitudes.component.ts
@@ -362,7 +362,7 @@ export class ListarSolicitudesComponent implements OnInit {
                 conceptId: '2881000013106'
             };
             const metadata = {
-                id: _id,
+                id: _id + Date.now(),
                 tipoPrestacion: conceptSnomed.conceptId,
                 fecha: new Date(),
                 paciente: carpeta.paciente,
