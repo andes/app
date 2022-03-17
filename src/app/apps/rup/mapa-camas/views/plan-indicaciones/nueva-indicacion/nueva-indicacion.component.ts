6e5e2c9f9223fdab2e712f5d9becebbd282d5dc7
Lautaro Molina <negro89_nqn@hotmail.com>
2022-03-17 15:55:39 -0300

    ref(IN): refactor visual y de comportamiento

--- src/app/apps/rup/mapa-camas/views/plan-indicaciones/nueva-indicacion/nueva-indicacion.component.ts
+++ src/app/apps/rup/mapa-camas/views/plan-indicaciones/nueva-indicacion/nueva-indicacion.component.ts
@@ -5,7 +5,6 @@ import { IPrestacion } from 'src/app/modules/rup/interfaces/prestacion.interface
 import { IPrestacionRegistro } from 'src/app/modules/rup/interfaces/prestacion.registro.interface';
 import { ElementosRUPService } from 'src/app/modules/rup/services/elementosRUP.service';
 import { PlantillasService } from 'src/app/modules/rup/services/plantillas.service';
-import { PlanIndicacionesServices } from '../../../services/plan-indicaciones.service';
 
 @Component({
     selector: 'plan-indicaciones-nueva-indicacion',
@@ -19,7 +18,6 @@ export class PlanIndicacionesNuevaIndicacionComponent implements OnInit {
     private ambito: string;
     private idInternacion: string;
 
-
     selectedConcept = null;
     elementoRUP = null;
     registro = null;
@@ -30,10 +28,23 @@ export class PlanIndicacionesNuevaIndicacionComponent implements OnInit {
         'fsn': 'plan de indicaciones médicas (procedimiento)',
         'semanticTag': 'procedimiento'
     };
-
-    @Input() seccion = null;
     @Input() indicacion = null;
-
+    @Input('seccion')
+    set seccion(value) {
+        this._seccion = value;
+        let concepto;
+        if (value && value.registro) {
+            concepto = {
+                ...value.registro,
+                esSolicitud: true
+            };
+        }
+        this.onConceptoSelect(concepto);
+    }
+    get seccion() {
+        return this._seccion;
+    }
+    private _seccion = null;
 
     prestacion: IPrestacion = {
         solicitud: {
@@ -46,36 +57,45 @@ export class PlanIndicacionesNuevaIndicacionComponent implements OnInit {
         }
     } as any;
 
+    get nombreSeccion() {
+        const seccionTerm = this.seccion.concepto.term;
+        return seccionTerm.length > 30 ? seccionTerm.slice('seccion: indicaciones de'.length) : seccionTerm.slice('seccion: '.length);
+    }
+
     constructor(
         private elementoRUPService: ElementosRUPService,
         public ps: PlantillasService,
-        private planIndicacionesServices: PlanIndicacionesServices,
         private route: ActivatedRoute,
         private auth: Auth
-    ) {
-
-    }
+    ) { }
 
     ngOnInit() {
         this.capa = this.route.snapshot.paramMap.get('capa');
         this.ambito = this.route.snapshot.paramMap.get('ambito');
         this.idInternacion = this.route.snapshot.paramMap.get('idInternacion');
 
+        let concepto;
         if (this.seccion && this.seccion.registro) {
-            this.onConceptoSelect({
+            concepto = {
                 ...this.seccion.registro,
                 esSolicitud: true
-            });
+            };
+        } else if (this.indicacion) {
+            concepto = {
+                ...this.indicacion.concepto,
+                esSolicitud: true
+            };
         }
+        this.onConceptoSelect(concepto);
     }
 
     onConceptoSelect(concepto) {
         this.selectedConcept = concepto;
-        this.elementoRUP = this.elementoRUPService.buscarElemento(concepto, concepto.esSolicitud);
-        this.ps.get(concepto.conceptId, true).subscribe(() => { });
-        this.registro = this.indicacion ? { valor: this.indicacion.valor } : new IPrestacionRegistro(this.elementoRUP, concepto);
-
-
+        if (concepto) {
+            this.elementoRUP = this.elementoRUPService.buscarElemento(concepto, concepto.esSolicitud);
+            this.ps.get(concepto.conceptId, true).subscribe(() => { });
+            this.registro = this.indicacion ? { valor: this.indicacion.valor, concepto } : new IPrestacionRegistro(this.elementoRUP, concepto);
+        }
     }
 
     onSave() {
@@ -121,8 +141,6 @@ export class PlanIndicacionesNuevaIndicacionComponent implements OnInit {
         } else {
             this.save.emit(indicacion);
         }
-
-        // this.planIndicacionesServices.create(indicacion);
     }
 
     onCancel() {
