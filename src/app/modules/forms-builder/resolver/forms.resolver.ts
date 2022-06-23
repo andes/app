2075674590704643077100a61ed2e6b08e26f1f2
martinebucarey <martinebucarey@gmail.com>
2022-06-23 11:29:51 -0300

    chore(core): angular 11

--- src/app/modules/forms-builder/resolver/forms.resolver.ts
+++ src/app/modules/forms-builder/resolver/forms.resolver.ts
@@ -5,7 +5,7 @@ import { Form, FormsService } from '../services/form.service';
 
 @Injectable()
 export class FormsResolver implements Resolve<Form> {
-    constructor(private formsService: FormsService) {}
+    constructor(private formsService: FormsService) { }
 
     resolve(route: ActivatedRouteSnapshot): Observable<Form> {
         return this.formsService.get(route.params.id);
