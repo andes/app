0ecd60f603591ebae573be0014a69d65bd148059
Lautaro Molina <lautaro.mlagos@gmail.com>
2023-06-14 09:37:49 -0300

    correccion en filtros por zona

--- src/app/modules/visualizacion-informacion/components/bi-queries/bi-queries.component.ts
+++ src/app/modules/visualizacion-informacion/components/bi-queries/bi-queries.component.ts
@@ -1,13 +1,14 @@
 import { Auth } from '@andes/auth';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { IZonaSanitaria } from 'src/app/interfaces/IZonaSanitaria';
 import { FormsService } from 'src/app/modules/forms-builder/services/form.service';
 import { ProfesionalService } from '../../../../services/profesional.service';
 import { QueriesService } from '../../../../services/query.service';
 import { ZonaSanitariaService } from './../../../../services/zonaSanitaria.service';
 import { OrganizacionService } from '../../../../services/organizacion.service';
+import { map, switchMap } from 'rxjs/operators';
 @Component({
     selector: 'app-bi-queries',
     templateUrl: './bi-queries.component.html',
@@ -16,19 +17,15 @@ import { OrganizacionService } from '../../../../services/organizacion.service';
 export class BiQueriesComponent implements OnInit {
     @Input() queries;
     public consultaSeleccionada;
-    public opciones = [];
     public queries$: Observable<any>;
-    public organizaciones$: Observable<any>;
+    public organizaciones = []; // total de organizaciones segun permisos
+    public organizacionesOrigenFiltradas = []; // resultado de filtro por zona origen
+    public organizacionesDestinoFiltradas = []; // resultado de filtro por zona destino
     public argumentos;
-    public argumentosSalida = [];
-    public resultados;
-    public mostrarSalida = false;
-    public tipoPrestaciones;
     public totalOrganizaciones = false;
-    public totalZonasSanitarias = false;
     public permisosZonas: any[];
     public zonasSanitarias: IZonaSanitaria[] = [];
-    public zonaRequired = false;
+    public disabledFiltroZonas = false;
     public inProgress = false;
     private idSubmoduloQueries = '60cce2a42e15361fe51b373d'; // coleccion modulos, mod. visualizacion, submodulo bi-queries
 
@@ -46,13 +43,20 @@ export class BiQueriesComponent implements OnInit {
     ngOnInit() {
         const permisos = this.auth.getPermissions('visualizacionInformacion:biQueries:?');
         this.totalOrganizaciones = this.auth.check('visualizacionInformacion:totalOrganizaciones');
-        this.totalZonasSanitarias = this.auth.check('visualizacionInformacion:zonasSanitarias:*');
         this.permisosZonas = this.auth.getPermissions('visualizacionInformacion:zonasSanitarias:?');
-        if (this.permisosZonas.length > 0) {
+
+        if (this.permisosZonas.length) { // tiene permisos para zona
+            // cargamos organizaciones segun permisos de zona
             this.loadZonasSanitarias();
+        } else {
+            // cargamos organizaciones segun permisos
+            this.disabledFiltroZonas = true;
+            this.auth.getModuleOrganizaciones(this.idSubmoduloQueries).subscribe(orgs => this.organizaciones = orgs);
         }
+
         let params;
         if (permisos.length) {
+            // cargamos queries segun permisos
             if (permisos[0] === '*') {
                 if (this.queries?.length) {
                     params = { _id: this.queries };
@@ -79,40 +83,37 @@ export class BiQueriesComponent implements OnInit {
 
     getArgumentos() {
         this.inProgress = true;
+        this.organizacionesOrigenFiltradas = [];
+        this.organizacionesDestinoFiltradas = [];
+
         if (this.consultaSeleccionada) {
             // filtramos zonas y organizaciones segun permisos
-            this.zonaRequired = false;
-            const idsZonasPermitidas = this.zonasSanitarias.map(zona => zona.id);
-            const zonaIndex = this.consultaSeleccionada.argumentos.findIndex(arg => arg.key === 'zona');
-            let zonas = this.consultaSeleccionada.argumentos[zonaIndex]?.items;
+            const filtrosPorZona = this.consultaSeleccionada.argumentos.filter(arg => arg.tipo === 'zonaSanitaria');
 
-            if (idsZonasPermitidas.length) {
-                if (zonas) {
-                    if (this.permisosZonas[0] === '*') {
-                        // todas las zonas
-                        this.organizaciones$ = this.organizacionService.get({ activo: true });
-                    } else {
-                        // algunas zonas
-                        zonas = zonas.filter(zona => idsZonasPermitidas.includes(zona.id));
-                        zonas.length ? this.consultaSeleccionada.argumentos[zonaIndex] = zonas : delete this.consultaSeleccionada.argumentos[zonaIndex];
-                        this.organizaciones$ = this.organizacionService.get({ 'zonaSanitaria.id': { $in: idsZonasPermitidas }, activo: true });
-                        this.zonaRequired = true;
-                    }
+            if (this.zonasSanitarias.length) { // tiene permiso para zonas?
+                if (this.permisosZonas[0] !== '*') {
+                    // permiso solo para algunas zonas
+                    filtrosPorZona.map(filtro => filtro.required = true);
                 }
             } else {
-                if (zonaIndex !== -1) {
-                    // si no tiene permizo para zonas, eliminamos el argumento
-                    this.consultaSeleccionada.argumentos.splice(zonaIndex, 1);
-                }
-                if (this.totalOrganizaciones) {
-                    // todas las organizaciones
-                    this.organizaciones$ = this.organizacionService.get({ activo: true });
-                } else {
-                    this.organizaciones$ = this.auth.getModuleOrganizaciones(this.idSubmoduloQueries);
-                }
+                this.organizacionesOrigenFiltradas = this.organizaciones;
+                this.organizacionesDestinoFiltradas = this.organizaciones;
             }
             this.argumentos = this.consultaSeleccionada.argumentos;
             this.inProgress = false;
+        }
+    }
+
+    changeZona(zona, key) {
+        if (zona) {
+            const orgFiltradas = this.organizaciones.filter(org => org.zonaSanitaria.id === zona.id);
+            key === 'zonaOrigen' ? this.organizacionesOrigenFiltradas = orgFiltradas : this.organizacionesDestinoFiltradas = orgFiltradas;
+        } else if (key === 'zonaOrigen') {
+            this.argumentos['organizacionOrigen'] = null;
+            this.organizacionesOrigenFiltradas = [];
+        } else {
+            this.argumentos['organizacionDestino'] = null;
+            this.organizacionesDestinoFiltradas = [];
         }
     }
 
@@ -147,9 +148,14 @@ export class BiQueriesComponent implements OnInit {
         if (this.permisosZonas[0] !== '*') {
             params['ids'] = this.permisosZonas;
         }
-        this.zonaSanitariaService.search(params).subscribe(resultado => {
-            this.zonasSanitarias = resultado;
-        });
+        this.zonaSanitariaService.search(params).pipe(
+            switchMap(zonas => {
+                this.zonasSanitarias = zonas;
+                const idZonas = zonas.map(zona => zona.id);
+                return this.organizacionService.get({ idsZonasSanitarias: idZonas, activo: true });
+            }),
+            map(orgs => this.organizaciones = orgs)
+        ).subscribe();
     }
 
     loadFomTypes(event) {
