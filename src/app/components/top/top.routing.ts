bc97e9e3cbe384cba0ac5d21926849a204c309a3
ma7payne <matich04@gmail.com>
2024-03-01 12:22:17 -0300

    feat(TOP): implementa mockups para reglas de entrada

--- src/app/components/top/top.routing.ts
+++ src/app/components/top/top.routing.ts
@@ -1,10 +1,10 @@
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { PlexModule } from '@andes/plex';
 import { SharedModule } from '@andes/shared';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { DirectiveLibModule } from 'src/app/directives/directives.module';
@@ -13,22 +13,22 @@ import { RUPLibModule } from 'src/app/modules/rup/rup-lib.module';
 import { CITASLibModule } from '../turnos/citas.module';
 import { TOPLibModule } from './top.module';
 
-import { NuevaSolicitudComponent } from './solicitudes/nuevaSolicitud.component';
-import { SolicitudesComponent } from './solicitudes/solicitudes.component';
-import { ListaReglasComponent } from './reglas/listaReglas.component';
-import { ReglasComponent } from './reglas/reglas.component';
-import { AuditarSolicitudComponent } from './solicitudes/auditarSolicitud.component';
-import { ReferirSolicitudComponent } from './solicitudes/referirSolicitud.component';
-import { BusquedaPacienteComponent } from './solicitudes/busquedaPaciente.component';
-import { DetalleSolicitudComponent } from './solicitudes/detalleSolicitud.component';
-import { VisualizacionReglasTopComponent } from './reglas/visualizacionReglasTop.component';
-import { EstadoSolicitudPipe } from './pipes/estado-solicitud.pipe';
-import { EstadoPrestacionPipe } from './pipes/estado-prestacion.pipe';
 import { AuditoriasSolicitudPipe } from './pipes/auditorias-solicitud.pipe';
 import { BotonesSolicitudPipe } from './pipes/botones.pipe';
-import { LinkTurnoRemotoPipe } from './pipes/linkTurnoRemoto.pipe';
+import { EstadoPrestacionPipe } from './pipes/estado-prestacion.pipe';
+import { EstadoSolicitudPipe } from './pipes/estado-solicitud.pipe';
 import { IsEmptyPipe } from './pipes/isEmpty.pipe';
 import { IsNotEmptyPipe } from './pipes/isNotEmpty.pipe';
+import { LinkTurnoRemotoPipe } from './pipes/linkTurnoRemoto.pipe';
+import { ListaReglasComponent } from './reglas/listaReglas.component';
+import { ReglasComponent } from './reglas/reglas.component';
+import { VisualizacionReglasTopComponent } from './reglas/visualizacionReglasTop.component';
+import { AuditarSolicitudComponent } from './solicitudes/auditarSolicitud.component';
+import { BusquedaPacienteComponent } from './solicitudes/busquedaPaciente.component';
+import { DetalleSolicitudComponent } from './solicitudes/detalleSolicitud.component';
+import { NuevaSolicitudComponent } from './solicitudes/nuevaSolicitud.component';
+import { ReferirSolicitudComponent } from './solicitudes/referirSolicitud.component';
+import { SolicitudesComponent } from './solicitudes/solicitudes.component';
 
 export const TOP_ROUTES = [
     { path: '', component: SolicitudesComponent, pathMatch: 'full' },
