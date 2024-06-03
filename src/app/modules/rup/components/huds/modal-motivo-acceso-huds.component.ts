291a3a2acc9191695c33cb5f155688cb169d601d
aldoEMatamala <123381977+aldoEMatamala@users.noreply.github.com>
2024-06-03 13:31:59 -0300

    feat(HUDS-77): Declaración jurada de acceso a la HUDS (#3009)
    Co-authored-by: aldoEMatamala <aldo.emanuel.matamla@gmail.com>

--- src/app/modules/rup/components/huds/modal-motivo-acceso-huds.component.ts
+++ src/app/modules/rup/components/huds/modal-motivo-acceso-huds.component.ts
@@ -1,14 +1,35 @@
-import { Component, Output, ViewChild, Input, EventEmitter } from '@angular/core';
+import { Component, Output, ViewChild, Input, EventEmitter, OnInit } from '@angular/core';
 import { PlexModalComponent } from '@andes/plex/src/lib/modal/modal.component';
+import { Auth } from '@andes/auth';
+import { ProfesionalService } from 'src/app/services/profesional.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { catchError, of } from 'rxjs';
+
 @Component({
     selector: 'modal-motivo-acceso-huds',
     templateUrl: 'modal-motivo-acceso-huds.html'
 })
 
-export class ModalMotivoAccesoHudsComponent {
-
+export class ModalMotivoAccesoHudsComponent implements OnInit {
+    public profesional;
+    public binaryString = null;
+    public firmas = null;
+    public urlFirma = null;
+    public base64textString: String = '';
+    public binaryStringAdmin = null;
+    public urlFirmaAdmin = null;
+    public base64textStringAdmin: String = '';
+    public nombreAdministrativo = '';
+    public firmaAdmin = null;
     @ViewChild('modal', { static: true }) modal: PlexModalComponent;
+    foto: any;
+    tieneFoto: boolean;
+    constructor(
+        public auth: Auth,
+        public _profesionalService: ProfesionalService,
+        public sanitizer: DomSanitizer,
 
+    ) { };
     @Input()
     set show(value) {
         if (value) {
@@ -25,7 +46,26 @@ export class ModalMotivoAccesoHudsComponent {
     ];
     public motivoSelected = null;
     public detalleMotivo = '';
-    // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+
+    ngOnInit(): void {
+        if (this.auth.profesional) {
+
+            this._profesionalService.getFirma({ id: this.auth.profesional }).pipe(catchError(() => of(null))).subscribe(resp => {
+                this.urlFirma = resp.length ? this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpeg;base64,' + resp) : null;
+            });
+            this._profesionalService.getFirma({ firmaAdmin: this.auth.profesional }).pipe(catchError(() => of(null))).subscribe(resp => {
+                if (resp?.firma) {
+                    this.urlFirmaAdmin = this.sanitizer.bypassSecurityTrustResourceUrl('data:image/jpeg;base64,' + resp.firma);
+                    this.firmaAdmin = resp.firma;
+                    this.nombreAdministrativo = resp.administracion;
+                } else {
+                    this.urlFirmaAdmin = null;
+                    this.firmaAdmin = null;
+                    this.nombreAdministrativo = '';
+                }
+            });
+        }
+    }
 
     motivoSelect() {
         return this.motivoSelected === null;
