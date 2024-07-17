42e906b261ea62e41b5483785338d9133379ff76
ma7payne <matich04@gmail.com>
2024-07-17 09:37:13 -0300

    fix(RUP): repara acceso a vistas de registros HUDS

--- src/app/modules/rup/huds-lib.module.ts
+++ src/app/modules/rup/huds-lib.module.ts
@@ -16,24 +16,22 @@ import { ChartComponent } from './components/ejecucion/resumen-paciente/chart.co
 import { ResumenPacienteDinamicoNinoComponent } from './components/ejecucion/resumen-paciente/resumenPaciente-dinamico-nino.component';
 import { ResumenPacienteDinamicoComponent } from './components/ejecucion/resumen-paciente/resumenPaciente-dinamico.component';
 import { ResumenPacienteEstaticoComponent } from './components/ejecucion/resumen-paciente/resumenPaciente-estatico.component';
-import { VistaAccesosHudsComponent } from './components/huds/vista-accesos-huds.component';
+import { DetallePrestacionComponent } from './components/huds/detallePrestacion';
+import { DetalleRegistronComponent } from './components/huds/detalleRegistro';
+import { DetalleRegistroInternoComponent } from './components/huds/detalleRegistroInterno';
+import { DetalleRegistroInternacionComponent } from './components/huds/internacion/detalleRegistroInternación.component';
+import { DetallePacienteComponent } from './components/huds/internacion/paciente/detallePaciente';
+import { ListadoRegistrosComponent } from './components/huds/internacion/registro/listadoRegistros';
 import { ListadoInternacionHudsComponent } from './components/huds/listado-internacion-huds.component';
+import { VistaAccesosHudsComponent } from './components/huds/vista-accesos-huds.component';
 import { VistaIPSComponent } from './components/huds/vista-ips.component';
 import { VistaCDAComponent } from './components/huds/vistaCDA.component';
 import { VistaContextoPrestacionComponent } from './components/huds/vistaContextoPrestacion';
-import { DetallePrestacionComponent } from './components/huds/detallePrestacion';
-import { DetalleRegistroInternoComponent } from './components/huds/detalleRegistroInterno';
 import { VistaDetalleRegistroComponent } from './components/huds/vistaDetalleRegistro';
-import { VistaProcedimientoComponent } from './components/huds/vistaProcedimiento';
-import { VistaRegistroComponent } from './components/huds/vistaRegistro';
+import { VistaHistorialTurnosComponent } from './components/huds/vistaHistorialTurnos.component';
 import { VistaSolicitudTopComponent } from './components/huds/vistaSolicitudTop';
 import { ElementosRUPModule } from './elementos-rup.module';
 import { RUPLibModule } from './rup-lib.module';
-import { VistaHistorialTurnosComponent } from './components/huds/vistaHistorialTurnos.component';
-import { DetalleRegistronComponent } from './components/huds/detalleRegistro';
-import { DetalleRegistroInternacionComponent } from './components/huds/internacion/detalleRegistroInternación.component';
-import { ListadoRegistrosComponent } from './components/huds/internacion/registro/listadoRegistros';
-import { DetallePacienteComponent } from './components/huds/internacion/paciente/detallePaciente';
 
 @NgModule({
     imports: [
@@ -61,8 +59,6 @@ import { DetallePacienteComponent } from './components/huds/internacion/paciente
         ResumenPacienteDinamicoComponent,
         ResumenPacienteDinamicoNinoComponent,
         ChartComponent,
-        VistaRegistroComponent,
-        VistaProcedimientoComponent,
         VistaContextoPrestacionComponent,
         DetallePrestacionComponent,
         DetalleRegistroInternacionComponent,
@@ -84,8 +80,6 @@ import { DetallePacienteComponent } from './components/huds/internacion/paciente
         ResumenPacienteDinamicoComponent,
         ResumenPacienteDinamicoNinoComponent,
         ChartComponent,
-        VistaRegistroComponent,
-        VistaProcedimientoComponent,
         VistaContextoPrestacionComponent,
         DetallePrestacionComponent,
         DetalleRegistroInternacionComponent,
