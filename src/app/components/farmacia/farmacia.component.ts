aa56a3e758c73a71409b713b7cb75c681dbaceeb
MarianoCampetella <marianocampe94@gmail.com>
2024-08-23 14:55:28 -0300

    feat(TM-34): ABM farmacias

--- src/app/components/farmacia/farmacia.component.ts
+++ src/app/components/farmacia/farmacia.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IFarmacia } from 'src/app/interfaces/IFarmacia';
 import { FarmaciaService } from '../../services/farmacia.service';
 import { Router } from '@angular/router';
 import { Auth } from '@andes/auth';
+import { Plex } from '@andes/plex';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
     selector: 'farmacias',
@@ -16,7 +18,6 @@ export class FarmaciaComponent implements OnInit {
     filtros: any = {};
     public listadoFarmacia$: Observable<IFarmacia[]>;
     public listadoActual: any[];
-    public loader = false;
     public farmaciaSelected;
     public showSidebar = false;
     public showcreate = false;
@@ -29,13 +30,22 @@ export class FarmaciaComponent implements OnInit {
     public numeroSanc;
     public descripcionDisp;
     public descripcionSanc;
-    public contacto;
-    public email;
+    public accion;
+    public posicion;
+    public disableGuardar = false;
+    private breakpointObserver: BreakpointObserver;
+    public arrayAsociado = [
+        { id: 'colegio', nombre: 'Colegio de Farmacéuticos' },
+        { id: 'farmacia', nombre: 'Farmacias Sociales' },
+        { id: 'camara', nombre: 'Camara de Farmacéuticos' },
+        { id: 'independiente', nombre: 'Independientes' }
+    ];
 
     constructor(
         private farmaciaService: FarmaciaService,
         private router: Router,
-        private auth: Auth
+        private auth: Auth,
+        private plex: Plex,
     ) { }
 
     public columns = [
@@ -49,54 +59,61 @@ export class FarmaciaComponent implements OnInit {
             key: 'razonSocial',
             label: 'Razón Social',
             sorteable: true,
+            opcional: true,
             sort: (a: any, b: any) => a.razonSocial.localeCompare(b.razonSocial)
         },
         {
             key: 'cuit',
             label: 'Cuit',
             sorteable: true,
+            opcional: true,
             sort: (a: any, b: any) => a.cuit.localeCompare(b.cuit)
         },
         {
             key: 'DTResponsable',
             label: 'D.T Responsable',
             sorteable: true,
+            opcional: true,
             sort: (a: any, b: any) => a.DTResponsable.localeCompare(b.DTResponsable)
         },
         {
             key: 'matriculaDTR',
             label: 'Matrícula D.T',
             sorteable: true,
+            opcional: true,
             sort: (a: any, b: any) => a.matriculaDTResponsable.localeCompare(b.matriculaDTResponsable)
         },
         {
             key: 'disposicionAltaDT',
             label: 'Disposición Alta D.T',
             sorteable: true,
+            opcional: true,
             sort: (a: any, b: any) => a.disposicionAltaDT.localeCompare(b.disposicionAltaDT)
         },
-    ]
+        {
+            key: 'asociado',
+            label: 'Asociado',
+            sorteable: true,
+            opcional: true,
+            sort: (a: any, b: any) => a.asociadoA.localeCompare(b.asociadoA)
+        },
+        {}
+    ];
 
     public columFarmac = [
         {
             key: 'farmaceutico',
-            label: 'Farmaceutico',
-            // sorteable: true,
-            // sort: (a: any, b: any) => a.farmaceutico.localeCompare(b.farmaceutico)
+            label: 'Farmaceutico'
         },
         {
             key: 'matricula',
-            label: 'Matrícula',
-            // sorteable: true,
-            // sort: (a: any, b: any) => a.denominacion.localeCompare(b.denominacion)
+            label: 'Matrícula'
         },
         {
             key: 'disposicionAlta',
-            label: 'Disposición/Alta',
-            // sorteable: true,
-            // sort: (a: any, b: any) => a.denominacion.localeCompare(b.denominacion)
+            label: 'Disposición/Alta'
         },
-    ]
+    ];
 
     public columSancion = [
         {
@@ -108,16 +125,16 @@ export class FarmaciaComponent implements OnInit {
             label: 'Descripción'
         },
         {}
-    ]
+    ];
 
     ngOnInit() {
         if (this.auth.getPermissions('tm:farmacia:?').length < 1) {
             this.router.navigate(['inicio']);
         }
+        this.filtrar();
         this.listadoFarmacia$ = this.farmaciaService.farmaciasFiltradas$.pipe(
             map(resp => {
-                // this.listadoActual = resp;
-                // this.loader = false;
+                this.listadoActual = resp;
                 return resp;
             })
         );
@@ -127,34 +144,35 @@ export class FarmaciaComponent implements OnInit {
         this.farmaciaService.lastResults.next(this.listadoActual);
     }
 
+    isMobile() {
+        return this.breakpointObserver?.isMatched('(max-width: 599px)');
+    }
+
     filtrar() {
         this.farmaciaService.lastResults.next(null);
         this.farmaciaService.denominacion.next(this.filtros.denominacion);
+        this.farmaciaService.razonSocial.next(this.filtros.razonSocial);
+        this.farmaciaService.cuit.next(this.filtros.cuit);
+        this.farmaciaService.DTResponsable.next(this.filtros.DTResponsable);
+        this.farmaciaService.asociado.next(this.filtros.asociado);
     }
 
     seleccionarFarmacia(farmacia) {
         this.farmaciaSelected = farmacia;
         this.showSidebar = true;
-        this.tieneDisposicion = (farmacia.disposiciones.length) ? true : false;
-        this.tieneSancion = (farmacia.sancion.length) ? true : false;
-        if (this.farmaciaSelected.contactos?.length) {
-            const indexCont = this.farmaciaSelected.contactos.findIndex(c => c.tipo === 'celular' || c.tipo === 'fijo');
-            const indexEmail = this.farmaciaSelected.contactos.findIndex(c => c.tipo === 'email');
-            this.contacto = (indexCont >= 0) ? this.farmaciaSelected.contactos[indexCont].valor : 'Sin contacto';
-            this.email = (indexEmail >= 0) ? this.farmaciaSelected.contactos[indexEmail].valor : 'Sin email';
-        } else {
-            this.contacto = 'Sin contacto';
-            this.email = 'Sin email'
-        }
-
+        this.tieneDisposicionAsociacion(farmacia);
     }
 
     puedeCrearFarmacia() {
-        return this.auth.getPermissions('tm:*').length || this.auth.getPermissions('tm:farmacia:*').length || this.auth.getPermissions('tm:farmacia:?').some(x => x === 'create')
+        return this.auth.getPermissions('tm:*').length || this.auth.getPermissions('tm:farmacia:*').length || this.auth.getPermissions('tm:farmacia:?').some(x => x === 'create');
     }
 
     puedeEditarFarmacia() {
-        return this.auth.getPermissions('tm:*').length || this.auth.getPermissions('tm:farmacia:*').length || this.auth.getPermissions('tm:farmacia:?').some(x => x === 'edit')
+        return this.auth.getPermissions('tm:*').length || this.auth.getPermissions('tm:farmacia:*').length || this.auth.getPermissions('tm:farmacia:?').some(x => x === 'edit');
+    }
+
+    puedeEliminarFarmacia() {
+        return this.auth.getPermissions('tm:*').length || this.auth.getPermissions('tm:farmacia:*').length || this.auth.getPermissions('tm:farmacia:?').some(x => x === 'delete');
     }
 
     crearEditarFarmacia(action) {
@@ -171,13 +189,19 @@ export class FarmaciaComponent implements OnInit {
 
     volver() {
         this.showcreate = false;
+        this.filtrar();
     }
 
     agregarElem(tipo) {
-        tipo === 'disposicion' ? this.showFormDisp = true : this.showFormSanc = true;
+        if (tipo === 'disposicion') {
+            this.showFormDisp = true;
+        } else {
+            this.showFormSanc = true;
+        }
+        this.accion = 'agregar';
     }
 
-    editar(tipo, element) {
+    editar(tipo, element, pos) {
         if (tipo === 'disposicion') {
             this.showFormDisp = true;
             this.numeroDisp = element.numero;
@@ -187,10 +211,41 @@ export class FarmaciaComponent implements OnInit {
             this.numeroSanc = element.numero;
             this.descripcionSanc = element.descripcion;
         }
+        this.accion = 'editar';
+        this.posicion = pos;
     }
 
-    eliminar(tipo) {
+    eliminar(tipo, pos) {
+        let mensaje = '';
+        if (tipo === 'disposicion') {
+            this.farmaciaSelected.disposiciones.splice(pos, 1);
+        } else {
+            this.farmaciaSelected.sancion.splice(pos, 1);
+        }
+        return this.farmaciaService.update(this.farmaciaSelected.id, this.farmaciaSelected).subscribe((farmaciaEdit) => {
+            this.tieneDisposicionAsociacion(farmaciaEdit);
+            mensaje = (tipo === 'disposicion') ? 'Disposición' : 'Sanción';
+            this.plex.toast('success', `${mensaje} eliminada exitosamente`);
+            this.cancelar(tipo);
+        });
+    }
 
+    eliminarFarmacia(farmacia) {
+        this.plex.confirm('¿Borrar Farmacia?').then((confirmado) => {
+            if (!confirmado) {
+                return false;
+            } else {
+                farmacia.activo = false;
+                this.farmaciaService.update(farmacia.id, farmacia).subscribe({
+                    next: () => {
+                        this.plex.toast('success', 'La Farmacia se eliminó exitosamente.');
+                        this.cerrar();
+                        this.filtrar();
+                    },
+                    error: () => this.plex.toast('danger', 'No se pudo eliminar la farmacia.')
+                });
+            }
+        });
     }
 
     cancelar(tipo) {
@@ -206,6 +261,61 @@ export class FarmaciaComponent implements OnInit {
     }
 
     guardar(tipo) {
+        this.disableGuardar = true;
+        if (this.accion === 'agregar') {
+            if (tipo === 'disposicion') {
+                this.farmaciaSelected.disposiciones.push({
+                    numero: this.numeroDisp,
+                    descripcion: this.descripcionDisp
+                });
+            } else {
+                this.farmaciaSelected.sancion.push({
+                    numero: this.numeroSanc,
+                    descripcion: this.descripcionSanc
+                });
+            }
+        } else {
+            if (tipo === 'disposicion') {
+                this.farmaciaSelected.disposiciones[this.posicion].numero = this.numeroDisp;
+                this.farmaciaSelected.disposiciones[this.posicion].descripcion = this.descripcionDisp;
+            } else {
+                this.farmaciaSelected.sancion[this.posicion].numero = this.numeroSanc;
+                this.farmaciaSelected.sancion[this.posicion].descripcion = this.descripcionSanc;
+            }
+        }
+        let mensaje = '';
+        let accionRealizada = '';
+        return this.farmaciaService.update(this.farmaciaSelected.id, this.farmaciaSelected).subscribe({
+            next: (farmaciaEdit) => {
+                this.tieneDisposicionAsociacion(farmaciaEdit);
+                mensaje = (tipo === 'disposicion') ? 'Disposición' : 'Sanción';
+                accionRealizada = (this.accion === 'agregar') ? 'creada' : 'editada';
+                this.plex.toast('success', `${mensaje} ${accionRealizada} exitosamente`);
+                this.cancelar(tipo);
+            },
+            error: () => {
+                mensaje = (tipo === 'disposicion') ? 'disposición' : 'sanción';
+                accionRealizada = (this.accion === 'agregar') ? 'crear' : 'editar';
+                this.plex.toast('danger', ` Error al ${accionRealizada} ${mensaje}`);
+            }
+        });
+    }
 
+    public tieneDisposicionAsociacion(farmacia) {
+        this.tieneDisposicion = (farmacia.disposiciones.length) ? true : false;
+        this.tieneSancion = (farmacia.sancion.length) ? true : false;
+    }
+
+    formatoCuit(event: any): void {
+        let cuit = event.target.value.replace(/\D/g, '');
+
+        if (cuit.length > 2) {
+            cuit = cuit.slice(0, 2) + '-' + cuit.slice(2);
+        }
+        if (cuit.length > 11) {
+            cuit = cuit.slice(0, 11) + '-' + cuit.slice(11, 12);
+        }
+
+        this.filtros.cuit = cuit;
     }
 }
--- src/app/services/farmacia.service.ts
+++ src/app/services/farmacia.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, combineLatest, EMPTY, Observable } from 'rxjs';
-import { IFarmacia } from '../interfaces/IFarmacia';
 import { Server, ResourceBaseHttp } from '@andes/shared';
 import { map, switchMap, auditTime } from 'rxjs/operators';
 
 @Injectable()
 export class FarmaciaService extends ResourceBaseHttp {
     protected url = '/core/tm/farmacias';
-    public farmaciasFiltradas$: Observable<any[]>
+    public farmaciasFiltradas$: Observable<any[]>;
     public denominacion = new BehaviorSubject<string>(null);
     public razonSocial = new BehaviorSubject<string>(null);
     public cuit = new BehaviorSubject<string>(null);
     public DTResponsable = new BehaviorSubject<string>(null);
+    public asociado = new BehaviorSubject<any>(null);
     public lastResults = new BehaviorSubject<any[]>(null);
     private limit = 20;
     private skip;
@@ -23,10 +23,11 @@ export class FarmaciaService extends ResourceBaseHttp {
             this.razonSocial,
             this.cuit,
             this.DTResponsable,
+            this.asociado,
             this.lastResults
         ]).pipe(
             auditTime(0),
-            switchMap(([denominacion, razonSocial, cuit, DTResponsable, lastResults]) => {
+            switchMap(([denominacion, razonSocial, cuit, DTResponsable, asociado, lastResults]) => {
                 if (!lastResults) {
                     this.skip = 0;
                 }
@@ -41,30 +42,34 @@ export class FarmaciaService extends ResourceBaseHttp {
                 };
 
                 if (denominacion) {
-                    params.denominacion = denominacion;
+                    params.denominacion = '^' + (denominacion as string).toUpperCase();
                 }
 
                 if (razonSocial) {
-                    params.razon = razonSocial;
+
+                    params.razonSocial = '^' + (razonSocial as string).toUpperCase();
                 }
 
                 if (cuit) {
-                    params.cuit = cuit;
+                    params.cuit = '^' + (cuit as string).toUpperCase();
                 }
 
                 if (DTResponsable) {
-                    params.DTResponsable = DTResponsable;
+                    params.DTResponsable = '^' + (DTResponsable as string).toUpperCase();
+                }
+                if (asociado) {
+                    params.asociadoA = asociado.nombre;
                 }
 
                 return this.search(params).pipe(
                     map(resultados => {
                         const listaFarmacias = lastResults ? lastResults.concat(resultados) : resultados;
                         this.skip = listaFarmacias.length;
-                        return listaFarmacias;
+                        return listaFarmacias.filter(farm => farm.activo);
                     })
                 );
             })
-        )
+        );
     }
 
 
