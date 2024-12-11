6baf6139b2ac5ddb7f4bec39435756f7440e7263
silviroa <silviroa@gmail.com>
2024-12-11 09:35:38 -0300

    refactor(HUDS): optimiza codigo  al recibir detalle de un  protocolo

--- src/app/modules/rup/components/ejecucion/laboratorios/vista-laboratorio.component.ts
+++ src/app/modules/rup/components/ejecucion/laboratorios/vista-laboratorio.component.ts
@@ -28,50 +28,10 @@ export class VistaLaboratorioComponent implements OnInit {
 
     ngOnInit(): void {
         const id = this.protocolo.data.idProtocolo;
-
         this.laboratorioService.getByProtocolo(id).subscribe((resultados) => {
-            this.areasLaboratorio = this.agrupar(resultados[0].Data);
+            this.areasLaboratorio = resultados;
 
         });
-    }
-
-    public agrupar(elementos) {
-        const setAreas = new Set(elementos.map(d => d.area));
-        const areasStr = Array.from(setAreas);
-
-        const areas = [];
-        const toItem = (e) => ({
-            nombre: e.item,
-            esTitulo: e.esTitulo === 'True' ? true : false,
-            resultado: e.Resultado || e.resultado,
-            unidadMedida: e.UnidadMedida || e.unidadMedida,
-            metodo: e.Metodo,
-            valorReferencia: e.valorReferencia,
-            firma: e.esTitulo === 'True' ? '' : e.userValida
-        });
-
-        areasStr.forEach(area => {
-            const detallesArea = elementos.filter(d => d.area === area);
-            const setGrupos = new Set(detallesArea.map(d => d.grupo));
-            const grupos = Array.from(setGrupos);
-            const item = {
-                area,
-                grupos: grupos.map(g => {
-                    const detallesAreaGrupo = detallesArea.filter(da => da.grupo === g);
-                    const res: any = {};
-                    res.grupo = g;
-                    if (detallesAreaGrupo.length === 1 && detallesAreaGrupo[0].grupo === g) {
-                        res.items = [toItem(detallesAreaGrupo[0])];
-                    } else {
-                        res.items = detallesAreaGrupo.map(toItem);
-                    }
-
-                    return res;
-                })
-            };
-            areas.push(item);
-        });
-        return areas;
     }
 
     descargarLab() {
