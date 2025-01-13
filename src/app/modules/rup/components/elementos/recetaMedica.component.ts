8f8f42ea8c346e34382ded9192ab5df82ad2f077
aldoEMatamala <123381977+aldoEMatamala@users.noreply.github.com>
2025-01-13 12:04:08 -0300

    RUP - Receta : modificar molécula (#3005)
    * fix(RUP-395): Receta - modificar molécula-2vuelta
    + dosis
    
    * fix(RUP): Se  corrige formato de diagnostico en receta
    
    ---------
    
    Co-authored-by: aldoEMatamala <aldo.emanuel.matamla@gmail.com>
    Co-authored-by: silviroa <silviroa@gmail.com>

--- src/app/modules/rup/components/elementos/recetaMedica.component.ts
+++ src/app/modules/rup/components/elementos/recetaMedica.component.ts
@@ -1,34 +1,53 @@
-import { Component, Output, Input, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, Input, EventEmitter, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { RUPComponent } from '../core/rup.component';
 import { RupElement } from '.';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Observable } from 'rxjs';
 import { Unsubscribe } from '@andes/shared';
+import { NgForm } from '@angular/forms';
 @Component({
     selector: 'rup-recetaMedica',
-    templateUrl: 'recetaMedica.html'
+    templateUrl: 'recetaMedica.html',
+    styleUrls: ['recetaMedica.scss'],
 })
+
+
 @RupElement('RecetaMedicaComponent')
 export class RecetaMedicaComponent extends RUPComponent implements OnInit {
+
+    @ViewChild('formMedicamento') formMedicamento: NgForm;
+    intervalos$: Observable<any>;
     public medicamento: any = {
         generico: null,
         presentacion: null,
-        unidades: null,
         cantidad: null,
-        diagnostico: '',
-        tipoReceta: 'simple',
+        cantEnvases: null,
+        diagnostico: null,
+        tipoReceta: { id: 'simple', label: 'Simple' },
         tratamientoProlongado: false,
+        tiempoTratamiento: null,
         dosisDiaria: {
-            cantidad: null,
-            dias: null
+            dosis: null,
+            frecuencia: null,
+            dias: null,
+            notaMedica: null
         }
     };
+    public horas = [];
+    public collapse = false;
+    public diagnosticos = [];
     public unidades = [];
     public genericos = [];
+    public registros = [];
+    public comprimidosEditados = false;
     public opcionesTipoReceta = [
-        { id: 'simple', label: 'Simple' },
         { id: 'duplicado', label: 'Duplicado' },
         { id: 'triplicado', label: 'Triplicado' }
     ];
+    public tiemposTratamiento = [
+        { id: '3meses', nombre: '3 meses' },
+        { id: '6meses', nombre: '6 meses' }
+    ];
+
     ngOnInit() {
         if (!this.registro.valor) {
             this.registro.valor = {};
@@ -36,10 +55,14 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit {
         if (!this.registro.valor.medicamentos) {
             this.registro.valor.medicamentos = [];
         }
+        this.registros = this.prestacion.ejecucion.registros.filter(reg => reg.id !== this.registro.id).map(reg => reg.concepto);
+        this.intervalos$ = this.constantesService.search({ source: 'plan-indicaciones:frecuencia' });
+
     }
 
     @Unsubscribe()
     loadMedicamentoGenerico(event) {
+        this.comprimidosEditados = false;
         const input = event.query;
         if (input && input.length > 2) {
             const query: any = {
@@ -52,10 +75,29 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit {
         }
     }
 
+    loadRegistros() {
+        this.registros = this.prestacion.ejecucion.registros
+            .filter(reg => reg.id !== this.registro.id)
+            .map(reg => reg.concepto);
+    }
+
+    editarComprimidos() {
+        if (!this.comprimidosEditados) {
+            this.plex.confirm('La cantidad recetada no se encuentra en ninguna presentación comercial ¿Desea continuar?', 'Atención').then(confirmacion => {
+                if (confirmacion) {
+                    this.medicamento.cantidad = 0;
+                    this.comprimidosEditados = true;
+                }
+            });
+        } else {
+            this.comprimidosEditados = false;
+        }
+    }
+
     loadPresentaciones() {
-        this.medicamento.unidades = null;
-        this.medicamento.presentacion = null;
         this.medicamento.cantidad = null;
+        this.medicamento.presentacion = null;
+        this.medicamento.cantEnvases = null;
         this.unidades = [];
         if (this.medicamento.generico) {
             const queryPresentacion: any = {
@@ -67,8 +109,9 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit {
                 search: ''
             };
             forkJoin(
-                [this.snomedService.get(queryPresentacion),
-                 this.snomedService.get(queryUnidades)]
+                [
+                    this.snomedService.get(queryPresentacion),
+                    this.snomedService.get(queryUnidades)]
             ).subscribe(([resultado, presentaciones]) => {
                 this.medicamento.presentacion = resultado[0];
                 this.unidades = presentaciones.map(elto => {
@@ -80,28 +123,30 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit {
 
     agregarMedicamento(form) {
         if (form.formValid) {
-            if (this.registro.valor.medicamentos.length < this.params.limiteMedicamentos) {
-                if (this.medicamento.unidades.valor) {
-                    this.medicamento.unidades = Number(this.medicamento.unidades.valor);
-                }
-                this.registro.valor.medicamentos.push(this.medicamento);
-                this.unidades = [];
-                this.medicamento = {
-                    generico: null,
-                    presentacion: null,
-                    unidades: null,
-                    cantidad: null,
-                    diagnostico: '',
-                    tipoReceta: 'simple',
-                    tratamientoProlongado: false,
-                    dosisDiaria: {
-                        cantidad: null,
-                        dias: null
-                    }
-                };
-            } else {
-                this.plex.toast('warning', `No se permite cargar más de ${this.params.limiteMedicamentos} medicamentos.`);
+            if (this.medicamento.cantidad?.valor) {
+                this.medicamento.cantidad = Number(this.medicamento.cantidad.valor);
             }
+            this.registro.valor.medicamentos.push(this.medicamento);
+            this.unidades = [];
+            this.medicamento = {
+                generico: null,
+                presentacion: null,
+                cantidad: null,
+                cantEnvases: null,
+                diagnostico: null,
+                tipoReceta: { id: 'simple', label: 'Simple' },
+                tratamientoProlongado: false,
+                tiempoTratamiento: null,
+                dosisDiaria: {
+                    frecuencia: null,
+                    dias: null,
+                    notaMedica: null
+                }
+            };
+            this.formMedicamento.reset();
+            this.formMedicamento.form.markAsPristine();
+            this.formMedicamento.form.markAsUntouched();
+
         }
     }
 
@@ -112,5 +157,16 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit {
                 this.registro.valor.medicamentos.splice(index, 1);
             }
         });
+    }
+
+    colapsar() {
+        this.collapse = !this.collapse;
+    }
+
+    truncateDiagnostico(nombre: string): string {
+        if (nombre.length > 20) {
+            return nombre.substring(0, 20) + '...';
+        }
+        return nombre;
     }
 }
