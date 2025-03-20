97105cea4ae6b523a270a1b1d18b4ae24c33a111
MarianoCampetella <marianocampe94@gmail.com>
2025-03-20 09:59:08 -0300

    feat(MPI-445): Obra Social PUCO

--- src/app/components/turnos/gestor-agendas/operaciones-agenda/sobreturno.component.ts
+++ src/app/components/turnos/gestor-agendas/operaciones-agenda/sobreturno.component.ts
@@ -168,7 +168,7 @@ export class AgregarSobreturnoComponent implements OnInit {
         if (!paciente || !paciente.documento) {
             return;
         }
-        this.obraSocialService.getObrasSociales(paciente.documento).subscribe(resultado => {
+        this.obraSocialService.getObrasSociales({ documento: paciente.documento, sexo: paciente.sexo }).subscribe(resultado => {
             if (resultado.length) {
                 this.obraSocialPaciente = resultado.map((os: any) => {
                     let osPaciente;
