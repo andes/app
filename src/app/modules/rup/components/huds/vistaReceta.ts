47c8d98819fc2d4cb025572b31a048f81a89d444
Fabio-Ramirez <fabioadiranr2@gmail.com>
2025-07-28 12:44:40 -0300

    agregado cambios en visualizacion de estado-dispensa

--- src/app/modules/rup/components/huds/vistaReceta.ts
+++ src/app/modules/rup/components/huds/vistaReceta.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { IPaciente } from '../../../../core/mpi/interfaces/IPaciente';
 import { HUDSService } from '../../services/huds.service';
 import { RecetaService } from 'projects/portal/src/app/services/receta.service';
+import { OrganizacionService } from 'src/app/services/organizacion.service';
 
 @Component({
     selector: 'vista-receta',
@@ -60,18 +61,69 @@ export class VistaRecetaComponent implements OnInit {
         'dispensa-parcial': 'warning'
     } as { [key: string]: string };
 
-
+    public listadoDispensas = [];
     public recetas;
     public recetaPrincipal: any;
     public historialRecetas: any[];
 
     constructor(
         public huds: HUDSService,
-        public recetaService: RecetaService
+        public recetaService: RecetaService,
+        public organizacionesService: OrganizacionService
     ) { }
 
     ngOnInit() {
         this.recetaPrincipal = this.recetaService.getUltimaReceta(this.registro.recetas);
+        this.combinarDispensas();
         this.historialRecetas = this.registro.recetas.filter(receta => receta.id !== this.recetaPrincipal.id);
     }
+
+    combinarDispensas() {
+        if (!this.recetaPrincipal.estados || !this.recetaPrincipal.estadosDispensa) {
+            return;
+        }
+
+        let dispensaDuplicada = false;
+        this.listadoDispensas = this.recetaPrincipal.estadosDispensa.map(dispensa => {
+            let organizacionNombre = '';
+            const dispensaCoincidente = this.recetaPrincipal.estados.find(estado => {
+                if (estado.createdBy.organizacion?.nombre) {
+                    organizacionNombre = estado.createdBy.organizacion.nombre;
+                } else if (estado.createdBy.organizacion?.id) {
+                    this.organizacionesService.getById(estado.createdBy.organizacion.id).subscribe(org => {
+
+                        organizacionNombre = organizacionNombre = (org.nombre?.toString() || 'Sin especificar');;
+                    });
+                }
+                return moment(estado.createdAt).isSame(moment(dispensa.fecha), 'day'); // Compara solo el día
+            }
+            );
+            const esDuplicada = dispensaDuplicada;
+            if (dispensa.tipo === 'dispensada') {
+                if (!dispensaDuplicada) {
+                    dispensaDuplicada = true;
+                }
+            }
+
+            if (dispensaCoincidente) {
+                return {
+                    fecha: dispensaCoincidente.createdAt,
+                    tipo: dispensa.tipo,
+                    organizacion: organizacionNombre || 'Sin especificar',
+                    sistema: dispensa.sistema || 'Sin especificar',
+                    dispensaDuplicada: esDuplicada
+                };
+            } else {
+                return {
+                    fecha: dispensa.fecha,
+                    tipo: dispensa.tipo,
+                    organizacion: organizacionNombre || 'Sin especificar',
+                    sistema: dispensa.sistema || 'Sin especificar',
+                    dispensaDuplicada: esDuplicada
+                };
+            }
+        });
+        return this.listadoDispensas;
+    }
+
 }
