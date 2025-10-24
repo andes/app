20f77b737a493f8d671867e245e5c3d9c5aecc4b
silviroa <silviroa@gmail.com>
2025-10-24 16:04:19 -0300

    fix(COM): cambia el filtro de unidades organizativas (#3251)

--- src/app/modules/com/components/estados-cama-provincial.ts
+++ src/app/modules/com/components/estados-cama-provincial.ts
@@ -1,6 +1,6 @@
 import { Auth } from '@andes/auth';
 import { Component, OnInit } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, flatMap, from, mergeMap, shareReplay } from 'rxjs';
 import { IOrganizacion } from '../../../interfaces/IOrganizacion';
 import { OrganizacionService } from '../../../services/organizacion.service';
 import { EstadosCamaProvincialService } from './../services/estados-cama-provincial.service';
@@ -22,7 +22,7 @@ export class EstadosCamaProvincialComponent implements OnInit {
     public camasEstados$: Observable<any[]>;
     public selectedOrganizacion: IOrganizacion;
     public selectedUnidadOrganizativa;
-    public unidadesOrganizativas$: Observable<any[]>;
+    public unidadesOrganizativas = [];
     public organizacionCamas: any[] = [];
     public showSidebar = false;
     public efectorSelected;
@@ -34,7 +34,7 @@ export class EstadosCamaProvincialComponent implements OnInit {
 
     ngOnInit() {
         this.camasEstados$ = this.estadosCamaProvincialService.camasEstados$;
-        this.unidadesOrganizativas$ = this.organizacionService.unidadesOrganizativas(this.auth.organizacion.id);
+        this.cargaUnidadesOrganizativas();
     }
 
     closeSidebar() {
@@ -56,4 +56,25 @@ export class EstadosCamaProvincialComponent implements OnInit {
         this.estadosCamaProvincialService.organizacion.next(this.selectedOrganizacion?.id);
         this.estadosCamaProvincialService.unidadOrganizativa.next(this.selectedUnidadOrganizativa?.conceptId);
     }
+
+    cargaUnidadesOrganizativas() {
+        this.camasEstados$.pipe(
+            map(data => {
+                // Aplanar todas las unidades organizativas en un solo array
+                const todas = data.reduce(
+                    (acc, item) => acc.concat(item.unidadesOrganizativas),
+                    [] as { conceptId: string; term: string }[]
+                );
+                const unicas = [
+                    ...new Map(todas.map(uo => [uo.conceptId, uo])).values()
+                ];
+                unicas.sort((a: any, b: any) => a.term.localeCompare(b.term, 'es', { sensitivity: 'base' }));
+                return unicas;
+            })
+        ).subscribe(unidades => {
+            this.unidadesOrganizativas = unidades;
+        });
+
+    }
 }
+
