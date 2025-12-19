7d47c365b902eb90b13102c09bf8279ed5864afb
nicolasarana <90768149+nicolasarana@users.noreply.github.com>
2025-12-19 11:48:14 -0300

    style(TYP):"Se agrega un bold para diferenciar el origen de la Obra social"

--- src/app/components/turnos/dar-turnos/seleccionar-financiador.component.ts
+++ src/app/components/turnos/dar-turnos/seleccionar-financiador.component.ts
@@ -120,7 +120,7 @@ export class SeleccionarFinanciadorComponent implements OnChanges {
             this.datosFinanciadores = [
                 ...this.financiadoresPaciente.map((os: IObraSocial) => ({
                     id: os.nombre || os.financiador,
-                    label: (os.nombre || os.financiador) + (os.origen ? ` (ORIGEN: ${os.origen})` : '')
+                    label: (os.nombre || os.financiador) + (os.origen ? ` (${this.toBold('ORIGEN: ' + os.origen)})` : '')
                 })),
                 { id: 'otras', label: 'Otras' }
             ];
@@ -248,5 +248,14 @@ export class SeleccionarFinanciadorComponent implements OnChanges {
                 this.numeroAfiliado = this.paciente.documento;
             }
         });
+    }
+
+    private toBold(text: string) {
+        const boldChars = {
+            'A': '𝗔', 'B': '𝗕', 'C': '𝗖', 'D': '𝗗', 'E': '𝗘', 'F': '𝗙', 'G': '𝗚', 'H': '𝗛', 'I': '𝗜', 'J': '𝗝', 'K': '𝗞', 'L': '𝗟', 'M': '𝗠', 'N': '𝗡', 'O': '𝗢', 'P': '𝗣', 'Q': '𝗤', 'R': '𝗥', 'S': '𝗦', 'T': '𝗧', 'U': '𝗨', 'V': '𝗩', 'W': '𝗪', 'X': '𝗫', 'Y': '𝗬', 'Z': '𝗭',
+            'a': '𝗮', 'b': '𝗯', 'c': '𝗰', 'd': '𝗱', 'e': '𝗲', 'f': '𝗳', 'g': '𝗴', 'h': '𝗵', 'i': '𝗶', 'j': '𝗷', 'k': '𝗸', 'l': '𝗹', 'm': '𝗺', 'n': '𝗻', 'o': '𝗼', 'p': '𝗽', 'q': '𝗾', 'r': '𝗿', 's': '𝘀', 't': '𝘁', 'u': '𝘂', 'v': '𝘃', 'w': '𝘄', 'x': '𝘅', 'y': '𝘆', 'z': '𝘇',
+            '0': '𝟬', '1': '𝟭', '2': '𝟮', '3': '𝟯', '4': '𝟰', '5': '𝟱', '6': '𝟲', '7': '𝟳', '8': '𝟴', '9': '𝟵'
+        };
+        return text.split('').map(char => boldChars[char] || char).join('');
     }
 }
