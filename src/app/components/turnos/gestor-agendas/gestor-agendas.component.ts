ba72fd08028850cd6b64b2199f94eb656b444917
Fabio-Ramirez <fabioadiranr2@gmail.com>
2026-01-07 16:30:23 -0300

    Agendas varias pasan a publicadas

--- src/app/components/turnos/gestor-agendas/gestor-agendas.component.ts
+++ src/app/components/turnos/gestor-agendas/gestor-agendas.component.ts
@@ -788,13 +788,15 @@ export class GestorAgendasComponent implements OnInit, OnDestroy {
         let alertCount = 0;
         this.agendasSeleccionadas.forEach((agenda, index) => {
             const esAgendaDelPasado = moment(agenda.horaInicio).isBefore(moment().startOf('day'));
-            if (estado === 'publicada' && esAgendaDelPasado) {
-                estado = 'auditada';
-            }
             const patch = {
                 'op': estado,
                 'estado': estado
             };
+            if (estado === 'publicada' && esAgendaDelPasado) {
+                patch.op = 'auditada';
+                patch.estado = 'auditada';
+            }
+
             this.serviceAgenda.patch(agenda.id, patch).subscribe((resultado: any) => {
                 // Si son múltiples, esperar a que todas se actualicen
                 agenda.estado = resultado.estado;
@@ -815,7 +817,6 @@ export class GestorAgendasComponent implements OnInit, OnDestroy {
                 }
             });
 
-            estado = 'publicada';
         });
     }
 
