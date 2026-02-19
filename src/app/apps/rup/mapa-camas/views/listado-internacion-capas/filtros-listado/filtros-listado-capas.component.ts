f9d6efe9abfcd532ffcc5515fc11e33b3d9b97f8
nicolasarana <90768149+nicolasarana@users.noreply.github.com>
2026-02-19 12:43:13 -0300

    feat(IN-613):"Agregar UO en historial"

--- src/app/apps/rup/mapa-camas/views/listado-internacion-capas/filtros-listado/filtros-listado-capas.component.ts
+++ src/app/apps/rup/mapa-camas/views/listado-internacion-capas/filtros-listado/filtros-listado-capas.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import * as enumerados from '../../../../../../utils/enumerados';
 import { PermisosMapaCamasService } from '../../../services/permisos-mapa-camas.service';
 import { ListadoInternacionCapasService } from '../listado-internacion-capas.service';
+import { Auth } from '@andes/auth';
+import { OrganizacionService } from 'src/app/services/organizacion.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-filtros-listado-capas',
@@ -13,24 +16,30 @@ export class FiltrosListadoCapasComponent implements OnInit {
         fechaIngresoDesde: moment().subtract(1, 'months').toDate(),
         fechaIngresoHasta: moment().toDate(),
         fechaEgresoDesde: null,
-        fechaEgresoHasta: null
+        fechaEgresoHasta: null,
+        unidadOrganizativa: null
     };
+    unidadesOrganizativas$: Observable<any[]>;
     estadosInternacion;
     requestInProgress: boolean;
 
     constructor(
         private listadoInternacionService: ListadoInternacionCapasService,
         public permisosMapaCamasService: PermisosMapaCamasService,
+        private auth: Auth,
+        private organizacionService: OrganizacionService
     ) { }
 
     ngOnInit() {
         this.resetFiltros();
         this.estadosInternacion = enumerados.getObjEstadoInternacion();
+        this.unidadesOrganizativas$ = this.organizacionService.unidadesOrganizativas(this.auth.organizacion.id);
     }
 
     resetFiltros() {
         this.listadoInternacionService.pacienteText.next(null);
         this.listadoInternacionService.estado.next(null);
+        this.listadoInternacionService.unidadOrganizativa.next(null);
         this.filtrarFecha();
     }
 
@@ -41,6 +50,8 @@ export class FiltrosListadoCapasComponent implements OnInit {
         } else {
             this.listadoInternacionService.estado.next(null);
         }
+        const unidadId = this.filtros.unidadOrganizativa?.conceptId || this.filtros.unidadOrganizativa;
+        this.listadoInternacionService.unidadOrganizativa.next(unidadId);
     }
 
     filtrarFecha() {
