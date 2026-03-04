2ecaa87204d3188da7000cb4cd78bc64649ddffa
nicolasarana <90768149+nicolasarana@users.noreply.github.com>
2026-03-04 15:32:49 -0300

    feat(COM-116):"Nueva solicitud - estrategia de atención"

--- src/app/modules/com/com.module.ts
+++ src/app/modules/com/com.module.ts
@@ -13,6 +13,7 @@ import { ElementosRUPModule } from '../rup/elementos-rup.module';
 import { SemaforoModule } from '../semaforo-priorizacion/semaforo-priorizacion.module';
 import { ReglasDerivacionService } from './../../services/com/reglasDerivaciones.service';
 import { TipoTrasladoService } from './../../services/com/tipoTraslados.service';
+import { EstrategiaAtencionService } from './../../services/com/estrategiaAtencion.service';
 import { DispositivoService } from './../../services/dispositivo/dispositivo.service';
 import { DispositivoComponent } from './../dispositivo/dispositivo.component';
 import { COMRouting } from './com.routing';
@@ -42,7 +43,8 @@ export const COM_PROVIDERS = [
     ReglasDerivacionService,
     TipoTrasladoService,
     PuntoInicioService,
-    DispositivoService
+    DispositivoService,
+    EstrategiaAtencionService
 ];
 
 @NgModule({
