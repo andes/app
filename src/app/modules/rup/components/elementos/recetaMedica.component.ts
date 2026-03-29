cc0d47560362944fefa9570b7cb1631a59b63950
aldoEMatamala <aldo.emanuel.matamala@gmail.com>
2026-03-29 19:29:14 -0300

    feat(REC-215) Andes: ver nota médica y agregar indicaciones a farmacia

--- src/app/modules/rup/components/elementos/recetaMedica.component.ts
+++ src/app/modules/rup/components/elementos/recetaMedica.component.ts
@@ -31,7 +31,8 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit, OnCha
             dosis: null,
             frecuencia: null,
             dias: null,
-            notaMedica: null
+            notaMedica: null,
+            notaFarmacia: null
         }
     };
     public horas = [];
@@ -266,7 +267,8 @@ export class RecetaMedicaComponent extends RUPComponent implements OnInit, OnCha
             dosisDiaria: {
                 frecuencia: null,
                 dias: null,
-                notaMedica: null
+                notaMedica: null,
+                notaFarmacia: null
             }
         };
 
