469cf135a3de860cb93ebc413eb666d94c2a969c
laucharin <negro89_nqn@hotmail.com>
2026-05-19 15:50:53 -0300

    fix packages, dep npm por error y lint

--- src/app/modules/auditoria/component/reporte-errores.component.ts
+++ src/app/modules/auditoria/component/reporte-errores.component.ts
@@ -16,7 +16,7 @@ export class ReporteErroresComponent implements OnInit {
 
     @ViewChild('modalCorreccion') modalCorreccion: ModalCorreccionPacienteComponent;
     @Output() selected = new EventEmitter<any>();
-    @Output() reset = new EventEmitter<any>();
+    @Output() reseted = new EventEmitter<any>();
 
     showSidebar = false;
     filtroPaciente: string;
@@ -158,7 +158,7 @@ export class ReporteErroresComponent implements OnInit {
                         this.plex.toast('success', 'Los datos se actualizaron correctamente!');
                         // recargamos la lista para reflejar el cambio
                         this.cargar();
-                        this.reset.emit();
+                        this.reseted.emit();
                     }
 
                 } else {
--- src/app/services/turnos/turno.service.ts
+++ src/app/services/turnos/turno.service.ts
@@ -1,5 +1,5 @@
 import { Observable } from 'rxjs';
-import * as moment from 'moment';
+import moment from 'moment';
 import { Injectable } from '@angular/core';
 import { Server } from '@andes/shared';
 import { IAgenda } from '../../interfaces/turnos/IAgenda';
