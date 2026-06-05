d34d9fdcc3b872a69de80097b56b99b7eeb1ddfb
Lautaro Molina <lautaro.mlagos@gmail.com>
2026-06-05 12:32:20 -0300

    Revert "HUDS - " Se agrega filtro de organización a EXPORTAR HUDS" (#3350)"
    This reverts commit 419a16fbb79f1c6cfb178ccdea152ae7e4e774a1.

--- src/app/modules/visualizacion-informacion/services/export-huds.service.ts
+++ src/app/modules/visualizacion-informacion/services/export-huds.service.ts
@@ -21,10 +21,6 @@ export class ExportHudsService {
         return this.server.post(this.exportHudsUrl, data, { responseType: 'blob' } as any);
     }
 
-    checkHistory(data): Observable<any> {
-        return this.server.post(this.exportHudsUrl + '/check-history', data);
-    }
-
     descargaHuds(params): Observable<any> {
         return this.server.post(this.exportHudsUrl + '/' + params.id, params, { responseType: 'blob' } as any).pipe(
             saveAs(params.name, 'zip')
