a3e4826151ffee900c23e1eadf12563302d3762c
AgosLizzi <agostinalizzi@gmail.com>
2026-09-17 08:39:57 -0300

    Correciones en los componentes y versión 1.1.30 de @andesicon para visualizar menú

--- src/app/components/formularioTerapeutico/formTerapeutico.component.ts
+++ src/app/components/formularioTerapeutico/formTerapeutico.component.ts
@@ -5,58 +5,67 @@ import { Router } from '@angular/router';
 import { FormTerapeuticoService } from './../../services/formTerapeutico/formTerapeutico.service';
 
 @Component({
-selector: 'app-formTerapeutico',
-templateUrl: './formTerapeutico.html',
-styleUrls: ['./formTerapeutico.scss']
+    selector: 'app-formTerapeutico',
+    templateUrl: './formTerapeutico.html',
+    styleUrls: ['./formTerapeutico.scss']
 })
 export class FormTerapeuticoComponent implements OnInit {
 
     public columnas = [
-{
-key: 'Sistema',
-label: 'Sistema'},
-{
-key: 'funcion',
-label: 'Función'},
-{
-key: 'grupoFarmacologico',
-label: 'Grupo Farmacológico'},
-{
-key: 'nombre',
-label: 'Nombre'},
-{
-key: 'conceptoSnomed',
-label: 'Concepto SNOMED'}
-];
+        {
+            key: 'Sistema',
+            label: 'Sistema'
+        },
+        {
+            key: 'funcion',
+            label: 'Función'
+        },
+        {
+            key: 'grupoFarmacologico',
+            label: 'Grupo Farmacológico'
+        },
+        {
+            key: 'nombre',
+            label: 'Nombre'
+        },
+        {
+            key: 'conceptoSnomed',
+            label: 'Concepto SNOMED'
+        }
+    ];
 
     public sistemas = [
         { id: 'respiratorio', icon: 'pulmones-outline' },
-        { id: 'vacunas', icon: 'vacuna', color: '#107CE6' },
-        { id: 'gastrointestinal', icon: 'gastrointestinal-outline', color: '#0443BF' },
-        { id: 'cardiovascular', icon: 'corazon-outline', color: '#5700FF' }, 
-        { id: 'genitourinario', icon: 'riñones-outline', color: '#BA00FF' },
-        { id: 'Antineoplasicos-einmunosupresores', icon: 'antineoplasicos', color: '#E6104E' },
-        { id: 'endocrino', icon: 'garganta', color: '#FF00A4' },
-        { id: 'anticonceptivos', icon: 'aco-outline', color: '#C54B8C' },
-        { id: 'anestesicos', icon: 'anestesia', color: '#F4A47E' },
-        { id: 'emergencias-toxicologicas', icon: 'toxi-emer', color: '#FFD400' }, 
-        { id: 'analgesicos-puros', icon: 'frasco-outline', color: '#FF9D00' },
-        { id: 'nervioso-central', icon: 'sist-nervioso', color: '#EE5A24' },
-        { id: 'sanguineo', icon: 'uni-gota', color: '#FF0000' },
-        { id: 'contraste-radiologicos', icon: 'rayos', color: '#96001C' },
-        { id: 'antinflamatorios', icon: 'pildora', color: '#96001C' },
-        { id: 'bloqueantes-neuromusculares', icon: 'bloqueante', color: '#897150' },
-        { id: 'oftalmologico', icon: 'oftalmologico', color: '#4A5E18' },
+        { id: 'vacunas', icon: 'vacuna' },
+        { id: 'gastrointestinal', icon: 'gastrointestinal-outline', },
+        { id: 'cardiovascular', icon: 'corazon-outline', },
+        { id: 'genitourinario', icon: 'riñones-outline' },
+        { id: 'antineoplasicos-e-inmunosupresores', icon: 'antineoplasicos' },
+        { id: 'endocrino', icon: 'garganta' },
+        { id: 'anticonceptivos', icon: 'aco-outline' },
+        { id: 'anestesicos', icon: 'anestesia' },
+        { id: 'emergencias-toxicologicas', icon: 'toxi-emer' },
+        { id: 'analgesicos-puros', icon: 'frasco-outline' },
+        { id: 'nervioso-central', icon: 'sist-nervioso' },
+        { id: 'sanguineo', icon: 'uni-gota' },
+        { id: 'contraste-radiologicos', icon: 'rayos' },
+        { id: 'antinflamatorios', icon: 'pildora' },
+        { id: 'bloqueantes-neuromusculares', icon: 'bloqueante' },
+        { id: 'oftalmologico', icon: 'oftalmologico' },
+        { id: 'antiinfecciosos', icon: 'infeccioso' },
+        { id: 'metabolismo', icon: 'metabolismo' },
+        { id: 'antisepticos-y-desinfectantes', icon: 'otro-frasco' },
+
     ];
 
-    public sistemaSeleccionado: string = null;
+    public sistemaSeleccionado = '';
 
-  public funcion = [];
-  public grupoFarmacologico = [];
-  public medicamento = "";
-  public conceptoSnomed = "";
+    public funcion = [];
+    public grupoFarmacologico = [];
+    public medicamento = '';
+    public conceptoSnomed = '';
 
-   public opciones = [
+    public opciones = [
         { id: 'a', nombre: 'alteradores de la motilidad intestinal' },
         { id: 'b', nombre: 'ablandadores fecales' },
         { id: 'c', nombre: 'antiulcerosos' },
@@ -66,7 +75,7 @@ label: 'Concepto SNOMED'}
         { id: 'g', nombre: 'tratamiento colestasis' }
     ];
 
-public grupos = [
+    public grupos = [
         { id: 'a', nombre: 'anticolinérgicos' },
         { id: 'b', nombre: 'procinéticos' },
         { id: 'c', nombre: 'laxantes' },
@@ -74,17 +83,15 @@ public grupos = [
         { id: 'e', nombre: 'antagonistas h2' },
         { id: 'f', nombre: 'inhibidores de la bomba de protones' },
         { id: 'g', nombre: 'antiácidos no absorbibles' },
-]
+    ];
 
+    constructor(
+        private router: Router,
+        private plex: Plex,
+        public auth: Auth,
+        public servicioFormTerapeutico: FormTerapeuticoService
+    ) { }
 
-    constructor(private router: Router,
-        private plex: Plex, public auth: Auth,
-        public servicioFormTerapeutico: FormTerapeuticoService) { }
-
-
-
-    ngOnInit() {}
-
-
+    ngOnInit() { }
 
 }
